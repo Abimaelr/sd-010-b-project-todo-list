12ed364efc348645f7410de488d207da82a7d948
Arthur Menezes Correa <arthurmcorrea282@gmail.com>
2021-03-10 17:40:24 -0300

    resolvendo a 4

--- script.js
+++ script.js
@@ -3,10 +3,15 @@ function criarTarefa() {
     let pegaLista = document.getElementById("lista-tarefas");
     let criarItemDaLista = document.createElement("li");
     criarItemDaLista.innerText = pegaTextoTarefa;
-
-    pegaLista.appendChild(criarItemDaLista);
+    criarItemDaLista.classList.add("item-lista")
     let pegaTarefa = document.getElementById("texto-tarefa");
     pegaTarefa.value = ""
-    console.log(pegaTarefa);
+    pegaLista.appendChild(criarItemDaLista);
     
+}
+function apagarTudo() {
+    let pegarLi = document.querySelectorAll('.item-lista');
+    for (let i = 0;i <= pegarLi.length;i++) {
+        pegarLi[i].remove();
+    }
 }
