2b7d72f69fa1fa0ff4344cda1886e125322f250c
Thiago Kachinsky <tjk091@gmail.com>
2021-03-10 18:25:19 -0300

    Requisitos 7, 8 e 9

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+function completedTask(event) {
+  const compTask = event.target;
+  if (compTask.classList.contains('completed')) {
+    compTask.classList.remove('completed');
+  } else {
+    compTask.classList.add('completed');
+  }
+}
+
+// eslint-disable-next-line max-lines-per-function
 function createTask() {
   const button = document.getElementById('criar-tarefa');
   const taskList = document.getElementById('lista-tarefas');
@@ -9,7 +19,17 @@ function createTask() {
     taskList.appendChild(item);
 
     input.value = '';
+
+    item.addEventListener('click', (event) => {
+      const allLis = document.getElementsByTagName('li');
+      for (let i = 0; i < allLis.length; i += 1) {
+        allLis[i].style.backgroundColor = 'rgba(0, 0, 0, 0.01)';
+      }
+      // eslint-disable-next-line no-param-reassign
+      event.target.style.backgroundColor = 'rgb(128, 128, 128)';
+    });
+
+    item.addEventListener('dblclick', completedTask);
   });
 }
-
 createTask();
