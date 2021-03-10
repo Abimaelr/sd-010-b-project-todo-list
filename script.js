6011e290c5253338091ce2a6e1253546727aedbe
joao966 <joaooliveira966@gmail.com>
2021-03-10 15:02:49 -0300

    adiciona requisito 2

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
 let maeTitulo = document.getElementById("corpo");
 let header = document.createElement("header");
+header.id = "titulo";
 header.innerText = "Minha Lista de Tarefas";
 maeTitulo.appendChild(header);
+///////////////////////////////////////////////////////////////////
+
+let maeParagrafo = document.getElementById("titulo");
+let paragrafo = document.createElement("p");
+paragrafo.id = "funcionamento";
+paragrafo.innerText = "Clique duas vezes em um item para marcá-lo como completo";
+maeParagrafo.appendChild(paragrafo);
