a097c3747c89c8a10727b5e98f36b7684288b3e4
Emerson Saturnino <emersonsaturnino@pop-os.localdomain>
2021-03-10 15:05:47 -0300

    Etapa 2

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
 window.onload = function () {
-
-
   startAplication();
 
-
   function createHeader() {
+    // Etapa 01 - Adicione à sua lista o título "Minha Lista de Tarefas" em uma tag
     // Criando o Elemento Header
     const element = document.createElement('header');
     document.body.appendChild(element);
@@ -16,7 +14,21 @@ window.onload = function () {
     element.appendChild(title);
   }
 
+  function createInstructions() {
+    // Etapa 02 - Adicione abaixo do título um pequeno e discreto parágrafo com id="funcionamento" e com o texto "Clique duas vezes em um item para marcá-lo como completo"
+    // Criando uma section para guardar o titulo
+    const element = document.createElement('section');
+    document.body.appendChild(element);
+
+    // Criando uma paragrafo elemento <p> dentro da section
+    const elementP = document.createElement('p');
+    elementP.id = "funcionamento";
+    elementP.innerText = "Clique duas vezes em um item para marcá-lo como completo";
+    element.appendChild(elementP);
+  }
+
   function startAplication() {
-    createHeader()
+    createHeader();
+    createInstructions();
   }
 };
