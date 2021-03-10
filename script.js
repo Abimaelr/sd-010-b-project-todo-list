d014e836e9ece87d1876b4e005e955b72e640f14
Misael Bueno <misa.hbueno@gmail.com>
2021-03-10 17:25:54 -0300

    requisito 7

--- script.js
+++ script.js
@@ -6,16 +6,39 @@ btn.addEventListener("click", addTask);
 function addTask() {
   let ol = document.querySelector("#lista-tarefas");
   let li = document.createElement("li");
+  li.classList.add("tasks")
   li.innerHTML = input.value;
-  ol.appendChild(li);  
-  clearInput()
+  //console.log(li)
+  ol.appendChild(li);
+  tasks(); 
+  clearInput();
+  // colorTasks();
+  
 }
 
 function clearInput() {
   input.value = '';
 }
 
+/* function colorTasks() {
+  let tasks = document.querySelector(".tasks");
+  tasks.addEventListener("click", function() {
+    tasks.style.backgroundColor = "rgb(128,128,128)";
+    console.log(tasks)
+  })  
+} */
 
+
+function tasks(event) {
+  let tasks = document.querySelectorAll(".tasks");
+  for (index = 0; index < tasks.length; index+=1) {
+    tasks[index].addEventListener("click", colorTask);  
+  }  
+}
+
+function colorTask(event) {
+  event.target.style.backgroundColor = "rgb(128,128,128)"
+}
 
 
 
