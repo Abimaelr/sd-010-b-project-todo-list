d84dfdf30e29692fc08b661847fdddfabc525cec
Paulo Xavier <pxavierf@gmail.com>
2021-03-10 17:34:50 -0300

    selecionando itens completos

--- script.js
+++ script.js
@@ -1,13 +1,45 @@
-function saveTask() {
-  const task = document.getElementById('texto-tarefa');
-  const saveButton = document.getElementById('criar-tarefa');
-  const list = document.getElementById('lista-tarefas');
-  
-  saveButton.addEventListener('click', () => {
-    const createListItem = document.createElement('li');
-    createListItem.innerText = task.value;
-    list.appendChild(createListItem);
-    task.value = '';
-  });
+const task = document.getElementById('texto-tarefa');
+const saveButton = document.getElementById('criar-tarefa');
+const orderedlist = document.getElementById('lista-tarefas'); 
+const deleteAll = document.getElementById('apaga-tudo');
+const deleteComplete = document.getElementById('remover-finalizados')
+const listItem = document.getElementsByTagName('li');
+
+saveButton.addEventListener('click', save);
+
+function save() {
+  const createListItem = document.createElement('li');
+  createListItem.className = 'list'
+  createListItem.innerText = task.value;
+  orderedlist.appendChild(createListItem);
+  task.value = '';  
+  select(); 
 }
-saveTask();
+
+function select() {
+  for (let i = 0; i < listItem.length; i += 1) {
+    listItem[i].addEventListener('click', () => {  
+      for (let j = 0; j < listItem.length; j += 1) {        
+        listItem[j].classList.remove('selected');
+      }
+      listItem[i].classList.add('selected');      
+    });
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
