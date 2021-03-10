49deaaa217a4e27da09f6bf41bc0de4043d13ed1
Camila Arruda <camilaam1802@gmail.com>
2021-03-10 19:13:59 -0300

    Adicionando requisito 8

--- script.js
+++ script.js
@@ -18,12 +18,41 @@ function limpaInput(){
     document.getElementById('texto-tarefa').value = ''; 
 }
 
-function colorList (){
-    let addlist = document.getElementById('lista-tarefas');
-    addlist.addEventListener("click", function(event){
-        event.target.style.background = 'rgb(128, 128, 128)';
+ 
+
+function changeColor(){
+    let addList = document.querySelector('#lista-tarefas');
+    addList.addEventListener("click", function(event){
+        let elementCinza = document.querySelector(".cinza");
+        if(elementCinza){
+            elementCinza.classList.remove('cinza');
+        }
+            event.target.classList.add('cinza');
     })
 }
+changeColor();
 
-colorList(); 
 
+/* function colorList (){
+    let addlist = document.querySelector('#lista-tarefas');
+    addlist.addEventListener("click", function(event){
+        event.target.classList.add(".cinza");
+    })
+
+}
+colorList();   */
+
+//Função remover background
+/* function removeBrackground(){
+    let addlist = document.getElementById('lista-tarefas');
+    addlist.addEventListener("click", function(event){
+        if(addlist.hasAttribute('rgb(128, 128, 128)')){
+            addlist.removeAttribute('rgb(128, 128, 128)');
+        } else {
+            
+        }
+    })
+    
+}
+
+removeBrackground();  */
