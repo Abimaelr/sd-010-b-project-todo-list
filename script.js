061ea5ad3bd5228fd38f868651e4b264df1da989
fredmartins <fredweb22@gmail.com>
2021-03-10 17:06:25 -0300

    adicionando LI com botão adicionar

---
+++ script.js
@@ -0,0 +1,9 @@
+let toAdd = () => {
+    let addLi = document.createElement('li');
+    addLi.innerText = inputText.value;
+    document.getElementById('lista-tarefas').appendChild(addLi);
+    inputText.value = '';
+}
+let createTask = document.getElementById('criar-tarefa');
+let inputText = document.getElementById('texto-tarefa');
+createTask.addEventListener('click', toAdd);
