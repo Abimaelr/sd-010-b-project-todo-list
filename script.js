460040d6eb8fe46261c0475311667d990ec47082
Vinicius <vini_bodra@hotmail.com>
2021-03-10 14:37:57 -0300

    Requisito 7 e 8

--- script.js
+++ script.js
@@ -1,15 +1,35 @@
 let botoncio = document.getElementById("criar-tarefa");
 let textoDigitado = document.getElementById("texto-tarefa");
-let listaTarefas = document.getElementById("lista-tarefas");
 
 
 botoncio.addEventListener("click", criaItemLista);
 
 
 function criaItemLista(){
-    
-        let itemLista = document.createElement("li");
-        itemLista.innerHTML = textoDigitado.value;
-        listaTarefas.appendChild(itemLista);
+    if(textoDigitado.value !== ""){
+        let criaItemLista = document.createElement("li");
+        criaItemLista.innerHTML = textoDigitado.value;
+        listaTarefas.appendChild(criaItemLista);
         textoDigitado.value = "";
+        
+        
+    }
 }
+let listaTarefas = document.getElementById("lista-tarefas");
+let listaAfazeres = document.getElementsByTagName("li")
+
+listaTarefas.addEventListener("click", pintaFundoLi)
+
+function pintaFundoLi (elemento){
+   
+    
+    let numeroDeLi = listaTarefas.childNodes.length;
+    
+    for(i=0;i<numeroDeLi;i++){
+        listaAfazeres[i].classList.remove("selected")
+    }
+    if (elemento.target.tagName == "LI"){
+        elemento.target.className = "selected"
+    }
+}
+
