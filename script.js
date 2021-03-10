b03193364f11e05f5b9b667b4eb5e76954f268ca
André De Bem <andrebemferraz@gmail.com>
2021-03-10 16:15:07 -0300

    Request IV

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //Starting
+
+createOl()
+
+function createOl () {
+  let addOl = document.createElement("OL");
+  addOl.setAttribute("id", "lista-tarefas")
+  document.getElementById("center-input").appendChild(addOl) }
