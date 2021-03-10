02bd7ce5e25d6cb4a0b7c1d15ef2a3a55d33aa7c
Paulo Xavier <pxavierf@gmail.com>
2021-03-10 15:35:18 -0300

    adicionando item a lista

---
+++ script.js
@@ -0,0 +1,12 @@
+function saveTask() {
+  const task = document.getElementById('texto-tarefa');
+  const saveButton = document.getElementById('criar-tarefa');
+  const list = document.getElementById('lista-tarefas');
+  
+  saveButton.addEventListener('click', () => {
+    const createListItem = document.createElement('li');
+    createListItem.innerText = task.value;
+    list.appendChild(createListItem)          
+  })
+}
+saveTask();
