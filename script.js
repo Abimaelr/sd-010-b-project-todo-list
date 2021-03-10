4300ae9a22b59eab6ccbcac05c6f8dca452fb5a9
Matheus H. Gois <matheus.hengo@gmail.com>
2021-03-10 16:41:42 -0300

    (5th n 6th) Function to add inputed text to the list done

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
+function cleanInput() {
+  const input = document.getElementById('texto-tarefa');
+  input.value = '';
+}
 
+function addItemToList() {
+  const list = document.getElementById('lista-tarefas');
+  const item = document.createElement('li');
+  const input = document.getElementById('texto-tarefa');
+  item.innerText = input.value;
+  list.appendChild(item);
+}
+
+function addButtonClick() {
+  const button = document.getElementById('criar-tarefa');
+  button.addEventListener('click', () => {
+    addItemToList();
+    cleanInput();
+  });
+}
+
+addButtonClick();
