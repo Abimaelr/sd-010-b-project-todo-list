a0a89c0226b54eec321e796295fc178969470e9a
lucasportella <lucasportellaagu@gmail.com>
2021-03-10 20:22:28 -0300

    started requirement 12

--- script.js
+++ script.js
@@ -9,48 +9,53 @@ buttonCreateTask.addEventListener('click', function () {
   task.innerHTML = inputTask.value;
   inputTask.value = '';
   orderedList.appendChild(task);
+  SetLocalStorage(orderedList, task);
 })
 
 function changeListItemColor() {
   orderedList.addEventListener('click', function (event) {
-      for (i = 0; i < orderedList.children.length; i += 1) {
-        orderedList.children[i].style.backgroundColor = '';
-      }
+    for (i = 0; i < orderedList.children.length; i += 1) {
+      orderedList.children[i].style.backgroundColor = '';
+    }
     event.target.style.backgroundColor = 'rgb(128, 128, 128)';
   })
 }
 changeListItemColor();
 
 function scratchTask() {
-
-    orderedList.addEventListener('dblclick', function (event){
-        if (event.target.className === 'completed'){
-            event.target.className = ""
-        } else {
-            event.target.className = "completed"
-        }
-
-    })
+  orderedList.addEventListener('dblclick', function (event) {
+    if (event.target.className === 'completed') {
+      event.target.className = ""
+    } else {
+      event.target.className = "completed"
+    }
+  })
 }
 scratchTask();
 
 
-function cleanList(){
-    buttonCleanList.addEventListener('click', function(){
-        orderedList.textContent = '';
-    })
+function cleanList() {
+  buttonCleanList.addEventListener('click', function () {
+    orderedList.textContent = '';
+  })
 }
 cleanList();
 
-function removeFinishedTasks(){
-    buttonRemoveFinishedTasks.addEventListener('click', function(event){
-        let CompletedTasks = document.getElementById('lista-tarefas');
-        for (i = CompletedTasks.children.length - 1; i >= 0; i -= 1){
-            let task = CompletedTasks.children[i]
-            if (task.className === "completed") {
-                CompletedTasks.removeChild(CompletedTasks.children[i])
-            }
-        }
-    })
+function removeFinishedTasks() {
+  buttonRemoveFinishedTasks.addEventListener('click', function (event) {
+    let CompletedTasks = document.getElementById('lista-tarefas');
+    for (i = CompletedTasks.children.length - 1; i >= 0; i -= 1) {
+      let task = CompletedTasks.children[i]
+      if (task.className === "completed") {
+        CompletedTasks.removeChild(CompletedTasks.children[i])
+      }
+    }
+  })
 }
 removeFinishedTasks();
+
+function SetLocalStorage(a, b) {
+    buttonCreateTask.addEventListener('click', function() {
+        localStorage.setItem(a, b)
+    })
+}
