22411b140b2f0583cbf5dc03ff606a6c93db2b00
legalvao1 <legalvao1@gmail.com>
2021-03-10 17:33:11 -0300

    Mudando cor de fundo do elemento clicado

--- script.js
+++ script.js
@@ -1,15 +1,23 @@
 const buttom = document.querySelector('#criar-tarefa');
 buttom.innerHTML = 'Adicionar';
 
-const caixaTexto = document.querySelector('#texto-tarefa')
-const todoList = document.querySelector('#lista-tarefas')
+const caixaTexto = document.querySelector('#texto-tarefa');
+const todoList = document.querySelector('#lista-tarefas');
 
-function listaDeTarefas(){
+function listaDeTarefas() {
   buttom.addEventListener('click', function(){
-    const listItem = document.createElement('li')
+    const listItem = document.createElement('li');
     listItem.innerHTML = caixaTexto.value;
     todoList.appendChild(listItem);
     caixaTexto.value = ''
   })
 }
 listaDeTarefas();
+
+function coloreFundo(){
+  const tag = document.querySelector('ol');
+  tag.addEventListener('click', function(evento){
+    evento.target.style.backgroundColor = 'rgb(128,128,128)' 
+  })
+}
+coloreFundo();
