2979cabff3520df2622b239841feb3f4d0c18dc3
Matheus H. Gois <matheus.hengo@gmail.com>
2021-03-10 17:33:48 -0300

    (7th) Change the color of the item when clicked

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
+function addClass(item, classes) {
+  if (Array.isArray(classes)) {
+    item.classList.add(...classes);
+  }
+  if (typeof classes === 'string') {
+    item.classList.add(classes);
+  }
+}
+
+function addClickEvent(item, addFunction) {
+  item.addEventListener('click', addFunction);
+}
+
 function cleanInput() {
   const input = document.getElementById('texto-tarefa');
   input.value = '';
@@ -8,6 +21,9 @@ function addItemToList() {
   const item = document.createElement('li');
   const input = document.getElementById('texto-tarefa');
   item.innerText = input.value;
+  addClickEvent(item, () => {
+    addClass(item, 'selected');
+  });
   list.appendChild(item);
 }
 
