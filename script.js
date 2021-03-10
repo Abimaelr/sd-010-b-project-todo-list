831fc143cad5e217f38a1ef45e999dbe934f74da
Junior030 <juniorhenrique030@gmail.com>
2021-03-10 15:52:56 -0300

    Requesitos 5

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 function createTask() {
-    const button = document.getElementById('criar-tarefa');
-    const ol = document.getElementById('lista-tarefas');
-    const input = document.getElementById('texto-tarefa');
-    button.addEventListener('click', function () {
-        const li = document.createElement('li');
-        li.innerText = input.value;
-        ol.appendChild(li);
-    })
+  const button = document.getElementById('criar-tarefa');
+  const ol = document.getElementById('lista-tarefas');
+  const input = document.getElementById('texto-tarefa');
+  button.addEventListener('click', function creatLi() {
+    const li = document.createElement('li');
+    li.innerText = input.value;
+    ol.appendChild(li);
+  })
 }
 createTask();
