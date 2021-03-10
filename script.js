7d4f72482f633c35addcfc4c8850bc0aa277ce4e
Paulo Xavier <pxavierf@gmail.com>
2021-03-10 18:01:24 -0300

    corrigindo lint

--- script.js
+++ script.js
@@ -5,43 +5,43 @@ const deleteAll = document.getElementById('apaga-tudo');
 const deleteComplete = document.getElementById('remover-finalizados')
 const listItem = document.getElementsByTagName('li');
 
-saveButton.addEventListener('click', save);
 
 function save() {
   const createListItem = document.createElement('li');
   createListItem.className = 'list'
   createListItem.innerText = task.value;
   orderedlist.appendChild(createListItem);
-  task.value = '';  
+  task.value = '';
   select(); 
   completed(); 
 }
 
 function select() {
   for (let i = 0; i < listItem.length; i += 1) {
-    listItem[i].addEventListener('click', () => {  
-      for (let j = 0; j < listItem.length; j += 1) {        
+    listItem[i].addEventListener('click', () => {
+      for (let j = 0; j < listItem.length; j += 1) {
         listItem[j].classList.remove('selected');
       }
-      listItem[i].classList.add('selected');      
+      listItem[i].classList.add('selected');
     });
   }
 }
 
 function completed(){
   for (let i = 0; i < listItem.length; i += 1) {
-    listItem[i].addEventListener('dblclick', () => {  
+    listItem[i].addEventListener('dblclick', () => {
       if (listItem[i].classList.contains('completed') == true) {
-        listItem[i].classList.remove('completed'); 
-        console.log('oi')     
+        listItem[i].classList.remove('completed');
+        console.log('oi')
       } else {
-        listItem[i].classList.add('completed');      
-        console.log('ola')     
+        listItem[i].classList.add('completed');
+        console.log('ola')
       }
     });
   }
 }
 
+saveButton.addEventListener('click', save);
 
 
 
