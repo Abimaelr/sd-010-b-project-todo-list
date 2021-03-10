70c91d99b64c81aa05921490796eab21f3daf122
Fabio Marturano <fabiocmjor@gmail.com>
2021-03-10 18:44:33 -0300

    Requisito 12

--- script.js
+++ script.js
@@ -4,14 +4,18 @@ const taskText = document.querySelector("#texto-tarefa");
 
 function addTaskToDo() {
   buttonAdd.addEventListener("click", function () {
-    const taskToDo =  document.createElement("li");
-    taskToDo.className = "taskToDo";
-    taskToDo.innerText = taskText.value;
-    selectableTasks(taskToDo);
-    doubleClick(taskToDo);
-    ThetoDoList.appendChild(taskToDo);
+    mkTask(taskText.value);
     taskText.value = "";
   });
+}
+
+function mkTask(text) {
+  const taskToDo =  document.createElement("li");
+  taskToDo.className = "taskToDo";
+  taskToDo.innerText = text;
+  selectableTasks(taskToDo);
+  doubleClick(taskToDo);
+  ThetoDoList.appendChild(taskToDo);
 }
 
 addTaskToDo();
@@ -19,22 +23,22 @@ addTaskToDo();
 function selectableTasks(taskSelec) {
   taskSelec.addEventListener("click", function () {
     let lastSelected = document.querySelector(".selected");
-    if(lastSelected != null){
+    if (lastSelected != null) {
       lastSelected.style.backgroundColor = "#dfd9d7";
       lastSelected.className = lastSelected.className.replace(" selected", "");
     }
     taskSelec.style.backgroundColor = "rgb(128, 128, 128)";
     taskSelec.className += " selected";
-  });      
+  });
 }
 
 function doubleClick(taskDb) {
   taskDb.addEventListener("dblclick", function () {
-    if(taskDb.className.match(/completed/)) {
+    if (taskDb.className.match(/completed/)) {
       taskDb.className = taskDb.className.replace(" completed", "");
     } else {
       taskDb.className += " completed";
-    }    
+    }
   });
 }
 
@@ -53,10 +57,44 @@ function dellDone() {
     let tasksDone = document.querySelectorAll(".completed");
     const totalDone = tasksDone.length;
     for (let task = 0; task < totalDone; task++) {
-      ThetoDoList.removeChild(tasksDone[task]);    
+      ThetoDoList.removeChild(tasksDone[task]);
     }
   });
-
 }
 
 dellDone();
+
+let tasksToSave = [];
+
+function saveTasks() {  
+  const buttonSave = document.querySelector("#salvar-tarefas");
+  buttonSave.addEventListener("click", function () {
+    let allTasksNow = document.querySelectorAll(".taskToDo");    
+    tasksToSave = [];
+    localStorage.setItem("tasksToSave", JSON.stringify(tasksToSave));
+    for (let toSave = 0; toSave < allTasksNow.length; toSave++) {
+      tasksToSave.push(allTasksNow[toSave].innerText);
+    }
+    localStorage.setItem("tasksToSave", JSON.stringify(tasksToSave));
+    alert("Lista Salva com Sucesso");
+  });
+}
+
+saveTasks();
+
+function loadTasks() {
+  let toLoadTasks = JSON.parse(localStorage.getItem("tasksToSave"));
+  let sizeArrayTask;
+  if(toLoadTasks != undefined){
+    sizeArrayTask = toLoadTasks.length;
+  }
+  if((toLoadTasks != undefined) && (sizeArrayTask != 0)){
+    for (let toLoad = 0; toLoad < toLoadTasks.length; toLoad++) {
+      mkTask(toLoadTasks[toLoad]);
+    } 
+  } else {
+    console.log("Vasio")
+  }
+}
+
+loadTasks();
