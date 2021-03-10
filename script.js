4baaf949ff762c0085c2b2756db78329415eaee9
Hugo Simoes <hugolfsimoes@gmail.com>
2021-03-10 16:33:43 -0300

    feito a tarefa 7 adicionando backgroudColor

--- script.js
+++ script.js
@@ -3,8 +3,15 @@ function criaItemLista() {
   const lista = document.querySelector('#lista-tarefas');
   const criaItem = document.createElement('li');
   criaItem.innerText = texto.value;
+  criaItem.classList.add('itens');
   lista.appendChild(criaItem);
   texto.value = '';
 }
 const botaoAdicionar = document.querySelector('#criar-tarefa');
 botaoAdicionar.addEventListener('click', criaItemLista);
+
+const lista = document.getElementById('lista-tarefas');
+lista.addEventListener('click', (event) => {
+  const itemSelecionado = event;
+  itemSelecionado.target.style.backgroundColor = 'rgb(128, 128, 128)';
+});
