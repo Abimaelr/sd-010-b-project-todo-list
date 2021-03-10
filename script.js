0f8f9af2c6fff652d7d239cce49e48372ac841c5
Junior030 <juniorhenrique030@gmail.com>
2021-03-10 17:23:43 -0300

    Requesitos 7

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+const listaTarefas = document.getElementById('lista-tarefas').getElementsByTagName('li');
+
 function createTask() {
   const button = document.getElementById('criar-tarefa');
   const ol = document.getElementById('lista-tarefas');
@@ -6,6 +8,21 @@ function createTask() {
     const li = document.createElement('li');
     li.innerText = input.value;
     ol.appendChild(li);
+    input.value = '';
+    li.addEventListener('click', clickItem())
   })
 }
 createTask();
+
+function clickItem() {
+  for (let i = 0; i < listaTarefas.length; i +=1) {
+    listaTarefas[i].addEventListener('click', function (){
+      for (let j = 0; j < listaTarefas.length; j += 1) {
+        if (listaTarefas[j].style.backgroundColor === 'rgb(128, 128, 128)') {
+          listaTarefas[j].style.backgroundColor = 'white';
+        }
+      }
+      listaTarefas[i].style.backgroundColor = 'rgb(128, 128, 128)';
+    })    
+  }
+}
