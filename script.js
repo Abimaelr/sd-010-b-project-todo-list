0360992f9ec773c76794eeee4b02d76fcda4b46d
AndersonPedrosa35 <anderson.loiro222@gmail.com>
2021-03-10 19:10:18 -0300

    Corrigindo requisito 7 e 8

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const createBtn = document.getElementById('criar-tarefa');
 const taskList = document.getElementById('lista-tarefas');
 const caixaTexto = document.getElementById('texto-tarefa');
+const listItem = document.querySelectorAll(".listItem");
 createBtn.innerHTML = 'Criar tarefa';
 function clickCreateList () {
   const listItem = document.createElement('li');
@@ -10,3 +11,28 @@ function clickCreateList () {
   caixaTexto.value = ''; 
 }
 createBtn.addEventListener('click', clickCreateList);
+ 
+document.addEventListener('click', function (event) {
+  const select = document.querySelectorAll('.selecionada');
+  if (event.target.classList.contains('listItem')) {
+    if (select.length !== 0) {
+      select[0].className = "listItem";
+      event.target.classList.add('selecionada');
+    } 
+    if (select.length === 0 ) {
+      event.target.classList.add('selecionada');
+    }
+  }
+})
+
+document.addEventListener('dblclick', function (event) {
+  if (event.target.classList.contains('listItem')) {
+    if (event.target.classList.contains('completed')) {
+    event.target.classList.add('completed');
+    }
+    if (event.target.classList.contains('completed')) {
+      event.target.classList.remove('completed');
+    }
+  }
+})
+  
