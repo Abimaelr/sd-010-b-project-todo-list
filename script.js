258c648cf4aeb475991ef88d9f0d5359a6ddebf7
Matheus Bodra <matheusbodra@gmail.com>
2021-03-10 18:41:04 -0300

    requisitos 7 e 8

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 //Crio a função do botão "Enviar", apagando o conteúdo da caixa de input e criando um item na lista de tarefas com o valor que o usuario digitou
 let tarefa = document.getElementById("texto-tarefa");
-let submit = document.getElementById("criar-tarefa");
+let enviar = document.getElementById("criar-tarefa");
 
-submit.addEventListener("click", adicionaNaListaEApaga);
+enviar.addEventListener("click", adicionaNaListaEApaga);
 
 function adicionaNaListaEApaga() {
     let li = document.createElement("li");
@@ -11,11 +11,29 @@ function adicionaNaListaEApaga() {
     tarefa.value = "";
 }
 
-let item = document.getElementsByTagName("li");
-for (i=0; i<item.length; i++) {
-    item[i].addEventListener("click", mudaCorDeFundo);
+/*   
+    Acessei o repositório de Ana Luiza Machado Salgado, onde compreendi a ideia de adicionar uma classe ao invés do backgroundColor direto.
+    Source: https://github.com/tryber/sd-09-project-todo-list/tree/analuizams-todo-list-project.
+
+    Acessei o blog de David Walsh para compreender melhor o uso da delegação de um evento ao elemento que o causou utilizando a propagação bubbling. 
+    Source: https://davidwalsh.name/event-delegate.
+*/
+
+//Seleciono o elemento da minha lista (não dos itens) e determino um event listener para que ao ser clicada, ela mude a classe do elemento que foi o gatilho da ativação do evento.
+var lista = document.getElementById("lista-tarefas");
+lista.addEventListener("click", atribuiClasseSelected);
+
+function atribuiClasseSelected(element) {
+    apagaClasse();
+    if (element.target.tagName == "LI") {
+        element.target.className = "selected";
+    }
 }
 
-function mudaCorDeFundo() {
-    this.style.backgroundColor = "rgb(128, 128, 128)";
+//Apago a classe selected dos demais elementos para garantir que apenas o atual elemento clicado estará em highlight.
+function apagaClasse() {
+    let itens = lista.childNodes;
+    for (i=0; i<itens.length; i++) {
+        itens[i].classList.remove("selected");
+    } 
 }
