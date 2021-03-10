9ed50c00af7159ae17b5256b0cdeaac280ac1b10
Fernando <fhomaia@gmail.com>
2021-03-10 15:38:32 -0300

    Adiciona resquisito 7

--- script.js
+++ script.js
@@ -1,13 +1,22 @@
 function addTask() {
-    const button = document.getElementById('criar-tarefa');
-    const input = document.getElementById('texto-tarefa');
-    const lista = document.getElementById('lista-tarefas');
-    button.addEventListener('click',function createTask() {
-        const newTask = document.createElement('li');
-        newTask.innerText = input.value;
-        lista.appendChild(newTask);
-        input.value = "";
-    });
+  const button = document.getElementById('criar-tarefa');
+  const input = document.getElementById('texto-tarefa');
+  const lista = document.getElementById('lista-tarefas');
+  button.addEventListener('click',function createTask() {
+    const newTask = document.createElement('li');
+    newTask.innerText = input.value;
+    lista.appendChild(newTask);
+    input.value = "";
+  });
 }
 
 addTask();
+
+function changeItem () {
+  const list = document.getElementById('lista-tarefas');
+  list.addEventListener('click',function backTask(event) {
+    event.target.style.backgroundColor = 'rgb(128,128,128)';
+  });
+}
+
+changeItem();
