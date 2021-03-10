7238909a05453ad433ac720982a06ba9732e6791
Alessandra Rezende <alessandra.rosarezende@gmail.com>
2021-03-10 16:22:57 -0300

    Requisito 9

--- script.js
+++ script.js
@@ -6,28 +6,27 @@ function adicionaTarefa() {
     if (getInputField.value.length > 0) {
       const newLi = document.createElement('li');
       newLi.innerText = getInputField.value;
-      getTaskList.appendChild(newLi);     
-      getInputField.value = '';
-    } 
-  });
-  getInputField.addEventListener('keyup', function (event) {
-    evento = event;
-    if (evento.keyCode === 13 && getInputField.value.length > 0) {
-      const newLi = document.createElement('li');
-      newLi.innerText = getInputField.value;
       getTaskList.appendChild(newLi);
       getInputField.value = '';
-    } 
-  })
+    } else {
+      alert('ERROR: Informe a tarefa a ser adicionada!');
+    }
+  });
 };
 adicionaTarefa();
 
 const selecionaItem = document.querySelector('#lista-tarefas');
 selecionaItem.addEventListener("click", function (event) {
-  let evento = event.target;
-  let tarefas = document.getElementsByTagName('li');
-  for (index = 0; index < tarefas.length; index += 1) {
+  const evento = event.target;
+  const tarefas = document.getElementsByTagName('li');
+  for (let index = 0; index < tarefas.length; index += 1) {
     tarefas[index].style.backgroundColor = '';
-  }
+  };
   evento.style.backgroundColor = 'rgb(128, 128, 128)';
+})
+
+const tarefas = document.getElementsByTagName('li');
+selecionaItem.addEventListener("dblclick", function (event) {
+  const evento = event.target;
+  evento.classList.toggle ('completed');  
 })
