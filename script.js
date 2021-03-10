8464247272d809501ecd79165af6bc3fef4d50c1
Duda Wiltiner <eduardawiltiner@gmail.com>
2021-03-10 16:53:06 -0300

    Aplica remover-finalizados button

--- script.js
+++ script.js
@@ -6,14 +6,75 @@ listTask = document.querySelector("#lista-tarefas");
 
 
 function changeBackgroundColor(){
-    for(let index = 0; index < document.querySelectorAll(".list-task").length; index++){
-        console.log(document.querySelectorAll(".list-task")[index])
-        document.querySelectorAll(".list-task")[index].addEventListener('click', function(event){
-            console.log("aqui")
-            event.target.style.backgroundColor = 'rgb(128, 128, 128)';
+
+    let listArray = document.querySelector("ol").childNodes;
+
+    for(let index = 0; index < listArray.length; index++){
+        // console.log(document.querySelectorAll(".list-task")[index])
+
+        listArray[index].addEventListener('click', function(event){
+            if(event.target.style.backgroundColor !== 'rgb(128, 128, 128)'){
+                event.target.style.backgroundColor = 'rgb(128, 128, 128)';
+            }
+            else{ 
+            }
+            for(let index1 = 0; index1 < listArray.length; index1++){
+                console.log("aqui")
+                if(listArray[index1] !== event.target){
+                    listArray[index1].style.backgroundColor = 'white';
+                }
+            }
         })
     }    
 }
+
+function buttonFinalizados(){
+    
+    let buttonApaga = document.querySelector("#remover-finalizados");
+    let listArray = document.querySelectorAll(".completed");
+    console.log(listArray);
+
+    buttonApaga.addEventListener('click', function(event){
+        for(let index = 0; index < listArray.length; index++){
+            listArray[index].remove();
+                
+        }
+     })     
+}
+
+function riscaTask(){
+    
+    let listArray = document.querySelector("ol").childNodes;
+
+    for(let index = 0; index < listArray.length; index++){
+        listArray[index].addEventListener('dblclick', function(event){
+            console.log(event.target);
+            console.log(event.target.className);
+            if(event.target.className !== 'completed'){
+                event.target.className = 'completed';
+                buttonFinalizados(); 
+            }
+            else{ 
+                event.target.classList.remove('completed');
+            }
+        })
+    }  
+}
+
+function buttonApaga(){
+    
+    let buttonrApaga = document.querySelector("#apaga-tudo");
+    let listArray = document.querySelector("ol").childNodes;
+
+    buttonrApaga.addEventListener('click', function(event){
+        for(let index = 0; index < listArray.length; index++){
+            listArray[index].remove();
+                
+        }
+     })     
+}
+
+
 
 createTaskButton.addEventListener("click", function(){
     if(inputTask.value == ''){
@@ -22,10 +83,12 @@ createTaskButton.addEventListener("click", function(){
     else{
         let li = document.createElement('li');
         li.innerHTML = inputTask.value;
-        li.className = 'list-task';
         listTask.appendChild(li);
         inputTask.value = '';
         changeBackgroundColor();
+        riscaTask();
+        buttonApaga();
+        buttonFinalizados();  
     }
     
 })
