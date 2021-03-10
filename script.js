e583bea9face75f6587a972e78c28b730addaa4b
Hugo Simoes <hugolfsimoes@gmail.com>
2021-03-10 18:02:58 -0300

    efetuado a tarefa 8

--- script.js
+++ script.js
@@ -10,8 +10,19 @@ function criaItemLista() {
 const botaoAdicionar = document.querySelector('#criar-tarefa');
 botaoAdicionar.addEventListener('click', criaItemLista);
 
+function removeSelected(elemento) {
+  let tagItem = elemento;
+  for (let index = 0; index < elemento.length; index += 1) {
+    tagItem[index].style.backgroundColor = 'white';
+    tagItem[index].classList.remove('selected');
+  }
+}
 const lista = document.getElementById('lista-tarefas');
 lista.addEventListener('click', (event) => {
+  const itens = document.querySelector('ol').children;
+  removeSelected(itens);
   const itemSelecionado = event;
   itemSelecionado.target.style.backgroundColor = 'rgb(128, 128, 128)';
+  itemSelecionado.target.classList.add('selected');
+  console.log(itemSelecionado);
 });
