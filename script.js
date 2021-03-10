fe1a3b73f8d99a1939847568082900f58db496f1
Jefferson_Andrade <jffrsncrls@gmail.com>
2021-03-10 15:56:07 -0300

    15:56

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
 const button = document.getElementById('criar-tarefa');
 const ol = document.getElementById('lista-tarefas');
-const textos = document.getElementById('texto-tarefa');
+let textos = document.getElementById('texto-tarefa');
 console.log(textos)
 function creatElement() {
   button.addEventListener('click', () => {
     const newElement = document.createElement('li');
     newElement.innerText = textos.value;
+    newElement.className = 'tasks';
     ol.appendChild(newElement);
+    textos.value = ''
+    textos.focus()
   });
 }
 creatElement();
+
+const li = document.getElementById
