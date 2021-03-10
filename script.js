8d0b2b735bc0ccf9c841821bbce056b8c75e4344
lucasportella <lucasportellaagu@gmail.com>
2021-03-10 15:31:47 -0300

    requirements 7,8

--- script.js
+++ script.js
@@ -3,8 +3,19 @@ let orderedList = document.getElementById('lista-tarefas');
 let buttonCreateTask = document.getElementById('criar-tarefa');
 
 buttonCreateTask.addEventListener('click', function () {
-    let task = document.createElement('li')
-    task.innerHTML = inputTask.value;
-    inputTask.value = '';
-    orderedList.appendChild(task);
+  let task = document.createElement('li')
+  task.innerHTML = inputTask.value;
+  inputTask.value = '';
+  orderedList.appendChild(task);
 })
+
+function changeListItemColor() {
+  orderedList.addEventListener('click', function (event) {
+      console.log(orderedList.children.length)
+      for (i = 0; i < orderedList.children.length; i += 1) {
+        orderedList.children[i].style.backgroundColor = '';
+      }
+    event.target.style.backgroundColor = 'rgb(128, 128, 128)';
+  })
+}
+changeListItemColor();
