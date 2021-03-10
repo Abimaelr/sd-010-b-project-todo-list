e47403dd47880c7a328b67b8cbd163a5de00c8ef
Jessie <miss_sc_jessi@hotmail.com>
2021-03-10 16:57:37 -0300

    requisito 7 e 8

--- script.js
+++ script.js
@@ -1,14 +1,26 @@
 const button = document.querySelector('#criar-tarefa');
+const lista = document.querySelector('#lista-tarefas');
 
 function criarLista() {
     const caixaTexto = document.querySelector('#texto-tarefa');
-    const lista = document.querySelector('#lista-tarefas');
     button.addEventListener('click', function (){
         const li = document.createElement('li');
         lista.appendChild(li);
         li.innerHTML = caixaTexto.value;
-        caixaTexto.value = '';
+        caixaTexto.value = null;
     })
 }
 
-criarLista()
+criarLista();
+
+function clicarLista() {
+  const tarefa = document.getElementsByTagName('li');
+  lista.addEventListener('click', function (event){
+      for (let index = 0; index < tarefa.length; index += 1){
+      tarefa[index].classList.remove('listaClicada');
+      event.target.classList.add('listaClicada');
+    }
+  })
+}
+
+clicarLista();
