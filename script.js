c5edce817a7780f9ac9afa6487d86e0c01feaa53
Abimael <abimaelr.albu@gmail.com>
2021-03-10 15:24:10 -0300

    7 8 requisito

--- script.js
+++ script.js
@@ -5,6 +5,21 @@ const btn = document.getElementById('criar-tarefa');
 btn.addEventListener ('click',function(){
     const listItem = document.createElement('li');
     listItem.innerText = inputField.value;
+    listItem.className = 'li'
     inputField.value = "";
     list.appendChild(listItem);
 })
+
+function itemSelection(evt){
+    //limpar os outros Itens
+    const listItem = document.querySelectorAll('.li')
+
+    for (let index = 0; index < listItem.length; index += 1){
+       listItem[index].className = 'li'
+    }
+
+
+    evt.target.className = 'li selected'
+}
+
+list.addEventListener('click',itemSelection,false)
