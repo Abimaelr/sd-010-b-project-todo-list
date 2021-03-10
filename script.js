dd8d998a546f7b2fae0108e75a903c81f966e714
Hugo Simoes <hugolfsimoes@gmail.com>
2021-03-10 14:50:17 -0300

    efetuada t rarefa 5, criando o evento de click do botao

---
+++ script.js
@@ -0,0 +1,11 @@
+function criaItemLista() {
+  const texto = document.querySelector('#texto-tarefa');
+  const lista = document.querySelector('#lista-tarefas');
+  const criaItem = document.createElement('li');
+  criaItem.innerText = texto.value;
+  lista.appendChild(criaItem);
+  texto.value = '';
+
+}
+const botaoAdicionar = document.querySelector('#criar-tarefa');
+botaoAdicionar.addEventListener('click', criaItemLista);
