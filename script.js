7b5502e538d0f773784794b061a1721843165eb1
AndersonPedrosa35 <anderson.loiro222@gmail.com>
2021-03-10 16:39:37 -0300

    Alinhando com o linter

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
-const createBtn = document.getElementById("criar-tarefa");
-const taskList = document.getElementById("lista-tarefa");
-const caixaTexto = document.getElementById("texto-tarefa");
-
+const createBtn = document.getElementById('criar-tarefa');
+const taskList = document.getElementById('lista-tarefa');
+const caixaTexto = document.getElementById('texto-tarefa');
+createBtn.innerHTML = 'Criar tarefa';
 function clickCreateList () {
-  const listItem = document.createElement("li");
-  listItem.className = "listItem";
+  const listItem = document.createElement('li');
+  listItem.className = 'listItem';
   listItem.innerHTML = caixaTexto.value;
   taskList.appendChild(listItem);
-  caixaTexto.value = ""; 
-} 
-
-createBtn.addEventListener("click", clickCreateList);
+  caixaTexto.value = ''; 
+}
+createBtn.addEventListener('click', clickCreateList);
