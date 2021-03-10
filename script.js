ec7aade6addcd37133bfa52768bea53eb6f7453f
Duda Wiltiner <eduardawiltiner@gmail.com>
2021-03-10 15:14:07 -0300

    Não deve ser possível selecionar mais de um elemento da lista ao mesmo tempo

--- script.js
+++ script.js
@@ -4,17 +4,28 @@ createTaskButton = document.querySelector("#criar-tarefa");
 inputTask = document.querySelector("#texto-tarefa");
 listTask = document.querySelector("#lista-tarefas");
 
-console.log(createTaskButton );
 
+function changeBackgroundColor(){
+    for(let index = 0; index < document.querySelectorAll(".list-task").length; index++){
+        console.log(document.querySelectorAll(".list-task")[index])
+        document.querySelectorAll(".list-task")[index].addEventListener('click', function(event){
+            console.log("aqui")
+            event.target.style.backgroundColor = 'rgb(128, 128, 128)';
+        })
+    }    
+}
 
 createTaskButton.addEventListener("click", function(){
     if(inputTask.value == ''){
         alert("Invalid Task");
     }
     else{
-        li = document.createElement('li');
+        let li = document.createElement('li');
         li.innerHTML = inputTask.value;
+        li.className = 'list-task';
         listTask.appendChild(li);
         inputTask.value = '';
+        changeBackgroundColor();
     }
+    
 })
