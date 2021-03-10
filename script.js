6a538a4e3fa129037b3463a0a367af29d7dc9b9d
Jefferson_Andrade <jffrsncrls@gmail.com>
2021-03-10 18:23:34 -0300

    18:23

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const button = document.getElementById('criar-tarefa');
 const ol = document.getElementById('lista-tarefas');
 const textos = document.getElementById('texto-tarefa');
 console.log(textos);
+
 function creatElement() {
   button.addEventListener('click', () => {
     const newElement = document.createElement('li');
@@ -13,14 +14,27 @@ function creatElement() {
   });
 }
 creatElement();
+const li = document.getElementsByClassName('tasks');
 
 function changeLi() {
-  const li = document.getElementsByClassName('tasks');
   for (let index = 0; index < li.length; index += 1) {
     li[index].addEventListener('click', (event) => {
       event.target.style.backgroundColor = 'rgb(128,128,128)';
-
     });
   }
 }
-button.addEventListener('click',changeLi)
+
+button.addEventListener('click', changeLi);
+
+
+const reset = document.getElementById('apaga-tudo');
+
+let listaTarefas = document.getElementById('lista-tarefas')
+function removeTask() {
+
+  for (let index = li.length; index >=  0; index -= 1) {
+    listaTarefas.lastChild.remove();
+  }
+
+}
+reset.addEventListener('click', removeTask)
