948876cc50fe93e9e0baae7df6d16f2bb61f9a4b
André De Bem <andrebemferraz@gmail.com>
2021-03-10 16:52:55 -0300

    Request V

--- script.js
+++ script.js
@@ -6,3 +6,14 @@ function createOl () {
   let addOl = document.createElement("OL");
   addOl.setAttribute("id", "lista-tarefas")
   document.getElementById("center-input").appendChild(addOl) }
+
+document.addEventListener("click", function (event) {
+  if (event.target.id === "criar-tarefa") {
+    let newLi = document.createElement("LI");
+    let textInput = document.getElementById("texto-tarefa").value;
+    let textLi = document.createTextNode(textInput);
+    newLi.appendChild(textLi);
+    document.getElementById("lista-tarefas").appendChild(newLi);
+    document.getElementById("texto-tarefa").value = ""
+  }
+})
