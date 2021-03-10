16b85f5920b14ec1cce86f4d14815d70db76a1ad
Mariana Mohr <marianamohr@gmail.com>
2021-03-10 15:46:32 -0300

    Requisito 7 - mudar cvor da li quando clicada

--- script.js
+++ script.js
@@ -1,12 +1,22 @@
-function criarTarefa(){
-const pai = document.getElementById('lista-tarefas');
-const tarefa = document.getElementById('texto-tarefa').value;
-console.log(pai);
-console.log(tarefa);
+function criarTarefa() {
+  const pai = document.getElementById('lista-tarefas');
+  const tarefa = document.getElementById('texto-tarefa').value;
+  if (tarefa === '') {
 
-const li = document.createElement('li');
-li.innerHTML = tarefa;
-pai.appendChild(li);
+    alert('Você deve incluir uma tarefa!')
+  } else {
 
-document.getElementById('texto-tarefa').value='';
+    const li = document.createElement('li');
+    li.className = 'tarefa';
+    li.innerHTML = tarefa;
+    pai.appendChild(li);
+    li.addEventListener('click', setColor);
+    document.getElementById('texto-tarefa').value = '';
+
+  }
+}
+
+function setColor(event) {
+  const tarefa = event.target;
+  tarefa.style.backgroundColor = 'rgb(128, 128, 128)';
 }
