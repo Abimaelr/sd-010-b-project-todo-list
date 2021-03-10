9e4642462059f3345f8bc19943f6cd9baa293bb5
Alessandra Rezende <alessandra.rosarezende@gmail.com>
2021-03-10 18:49:58 -0300

    Tentativa Requisito 12

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function adicionaTarefa() {
   const getInputField = document.querySelector('#texto-tarefa');
   const addInputButton = document.querySelector('#criar-tarefa');
   const getTaskList = document.querySelector('#lista-tarefas');
-  addInputButton.addEventListener('click',function () {
+  addInputButton.addEventListener('click', function () {
     if (getInputField.value.length > 0) {
       const newLi = document.createElement('li');
       newLi.innerText = getInputField.value;
@@ -17,26 +17,26 @@ adicionaTarefa();
 
 const selecionaItem = document.querySelector('#lista-tarefas');
 const tarefas = document.getElementsByTagName('li');
-selecionaItem.addEventListener('click',function (event) {
-  const evento = event.target;  
+selecionaItem.addEventListener('click', function (event) {
+  const evento = event.target;
   for (let index = 0; index < tarefas.length; index += 1) {
     tarefas[index].style.backgroundColor = '';
   }
   evento.style.backgroundColor = 'rgb(128, 128, 128)';
 });
 
-selecionaItem.addEventListener('dblclick',function (event) {
+selecionaItem.addEventListener('dblclick', function (event) {
   const evento = event.target;
   evento.classList.toggle('completed');
 });
 
 const botaoApaga = document.querySelector('#apaga-tudo');
-botaoApaga.addEventListener('click',function () {
+botaoApaga.addEventListener('click', function () {
   selecionaItem.innerText = '';
 });
 
 const botaoRemoveFinalizados = document.querySelector('#remover-finalizados');
-botaoRemoveFinalizados.addEventListener('click',function () {
+botaoRemoveFinalizados.addEventListener('click', function () {
   for (let index = 0; index < tarefas.length; index += 1) {
     if (tarefas[index].classList.contains('completed')) {
       selecionaItem.removeChild(tarefas[index]);
@@ -45,11 +45,35 @@ botaoRemoveFinalizados.addEventListener('click',function () {
   }
 });
 
+const botaoSalvar = document.querySelector('#salvar-tarefas');
+botaoSalvar.addEventListener('click', function () {  
+  const itens = [];
+  if (tarefas !== null) {
+    for (let index = 0; index < tarefas.length; index += 1) {
+      itens.push(tarefas[index].innerHTML);
+    }
+    localStorage.setItem('todoList', JSON.stringify(itens));
+  }
+})
+
+function recuperaItens() {
+  const recupera = JSON.parse(localStorage.getItem('todoList'));
+  if (recupera !== null) {
+    for (let index = 0; index < recupera.length; index += 1) {
+      let itemRecupera = document.createElement('li');
+      itemRecupera.innerHTML = recupera[index];
+      selecionaItem.appendChild(itemRecupera);
+    }    
+  }
+}
+
 const botaoRemoveSelecionado = document.querySelector('#remover-selecionado');
-botaoRemoveSelecionado.addEventListener('click',function () {
+botaoRemoveSelecionado.addEventListener('click', function () {
   for (let index = 0; index < tarefas.length; index += 1) {
     if (tarefas[index].style.backgroundColor === 'rgb(128, 128, 128)') {
       selecionaItem.removeChild(tarefas[index]);
     }
   }
 });
+
+recuperaItens();
