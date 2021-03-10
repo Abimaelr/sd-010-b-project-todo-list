cd3f388117c949798bb35d11991ef715107377b8
Malbathan Lima <malbathanlima@gmail.com>
2021-03-10 15:16:43 -0300

    paragrafo criado

--- script.js
+++ script.js
@@ -4,3 +4,9 @@ document.body.append(header);
 const title = document.createElement('h1');
 title.innerHTML = 'Minha Lista de Tarefas';
 header.append(title);
+
+// Criando uma paragrafo com descrição e id 'funcionamento'
+const textSection = document.createElement('p');
+textSection.id = 'funcionamento';
+textSection.innerHTML = 'Clique duas vezes em um item para marcá-lo como completo';
+document.body.append(textSection);
