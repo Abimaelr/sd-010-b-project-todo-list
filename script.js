59200cf9c5d6bc5fc12f095075590ad9db2c72b8
Gabriel Miranda <gabriel.mirandab92@gmail.com>
2021-03-10 16:42:24 -0300

    add exercicio 7 - altera cor de fundo do item clicado

--- script.js
+++ script.js
@@ -12,3 +12,27 @@ function addActivity() {
   });
 }
 addActivity();
+
+window.onload = function changeBackgroundListItem() {
+  const backgroundColorList = list.style.backgroundColor = 'white';
+  sessionStorage.setItem('colorList', backgroundColorList);
+};
+
+list.addEventListener('click', function(event){
+  event.target.style.backgroundColor = 'rgb(128, 128, 128)';
+})
+
+/* const listAll = document.querySelectorAll('#lista-tarefas');
+
+for (let index = 0; index < listAll.length; index += 1) {
+
+  listAll[index].addEventListener('click', (event) => {
+    const element = event;
+    if (element.target.style.backgroundColor !== 'rgb(128, 128, 128)') {
+      element.target.className += ' selected';
+      element.target.style.backgroundColor = 'rgb(128, 128, 128)';
+    } else {
+
+  });
+}
+ */
