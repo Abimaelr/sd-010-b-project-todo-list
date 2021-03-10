68d1e9aa07bf1f79d5cf9a2167c0a53ebbd31e9d
Jessie <miss_sc_jessi@hotmail.com>
2021-03-10 17:38:09 -0300

    requisito 9

--- script.js
+++ script.js
@@ -2,25 +2,36 @@ const button = document.querySelector('#criar-tarefa');
 const lista = document.querySelector('#lista-tarefas');
 
 function criarLista() {
-    const caixaTexto = document.querySelector('#texto-tarefa');
-    button.addEventListener('click', function (){
-        const li = document.createElement('li');
-        lista.appendChild(li);
-        li.innerHTML = caixaTexto.value;
-        caixaTexto.value = null;
-    })
+  const caixaTexto = document.querySelector('#texto-tarefa');
+  button.addEventListener('click', function () {
+    const li = document.createElement('li');
+    lista.appendChild(li);
+    li.innerHTML = caixaTexto.value;
+    caixaTexto.value = null;
+  });
 }
 
 criarLista();
 
 function clicarLista() {
   const tarefa = document.getElementsByTagName('li');
-  lista.addEventListener('click', function (event){
-      for (let index = 0; index < tarefa.length; index += 1){
+  lista.addEventListener('click', function (event) {
+    for (let index = 0; index < tarefa.length; index += 1) {
       tarefa[index].classList.remove('listaClicada');
       event.target.classList.add('listaClicada');
     }
-  })
+  });
 }
 
 clicarLista();
+
+function tarefaCompleta() {
+  const tarefa = document.getElementsByTagName('li');
+  lista.addEventListener('dblclick', function (event) {
+    for (let index = 0; index < tarefa.length; index += 1) {
+      event.target.classList.toggle('completed');
+    }
+  });
+}
+
+tarefaCompleta();
