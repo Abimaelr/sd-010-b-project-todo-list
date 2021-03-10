494ca23453949678b37a7c2304fb3db230117750
Matheus Bodra <matheusbodra@gmail.com>
2021-03-10 10:07:51 -0300

    primeira tentativa requisito 7

--- script.js
+++ script.js
@@ -1,11 +1,21 @@
+//Crio a função do botão "Enviar", apagando o conteúdo da caixa de input e criando um item na lista de tarefas com o valor que o usuario digitou
 let tarefa = document.getElementById("texto-tarefa");
-
 let submit = document.getElementById("criar-tarefa");
-submit.addEventListener("click" ,adicionaNaListaEApaga);
+
+submit.addEventListener("click", adicionaNaListaEApaga);
 
 function adicionaNaListaEApaga() {
     let li = document.createElement("li");
     li.innerHTML = tarefa.value;
     document.getElementById("lista-tarefas").appendChild(li);
     tarefa.value = "";
+}
+
+let item = document.getElementsByTagName("li");
+for (i=0; i<item.length; i++) {
+    item[i].addEventListener("click", mudaCorDeFundo);
+}
+
+function mudaCorDeFundo() {
+    this.style.backgroundColor = "rgb(128, 128, 128)";
 }
