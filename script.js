50a612e21b1ce16eadece1631c630c8b6d61010a
denis-rossati <denis.rossati@hotmail.com>
2021-03-10 14:47:37 -0300

    requisito 3 - tentativa

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
-const bodyChilds = document.body
-
+const bodyChilds = document.body;
 // requisito 1
 bodyChilds.appendChild(document.createElement('header'));
 document.querySelector('header').innerText = 'Minha Lista de Tarefas';
 
 // requisito 2
-const headerP = document.createElement('p')
+const headerP = document.createElement('p');
 bodyChilds.appendChild(headerP);
 headerP.id = 'funcionamento';
 headerP.innerText = 'Clique duas vezes em um item para marcá-lo como completo';
+
+// requisito 3
+const input = document.createElement('input');
+bodyChilds.appendChild(input);
+input.id = 'texto-tarefa'
