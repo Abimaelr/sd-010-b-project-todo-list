832e9d2bee3a55bc1731d5172db49c004f28dfb7
Alessandra Rezende <alessandra.rosarezende@gmail.com>
2021-03-10 16:07:24 -0300

    Requisitos 7 e 8

--- script.js
+++ script.js
@@ -1,28 +1,33 @@
 function adicionaTarefa() {
-  let getInputField = document.querySelector('#texto-tarefa');
-  let addInputButton = document.querySelector('#criar-tarefa');
-  let getTaskList = document.querySelector('#lista-tarefas');
-
-  addInputButton.addEventListener('click', function() {
+  const getInputField = document.querySelector('#texto-tarefa');
+  const addInputButton = document.querySelector('#criar-tarefa');
+  const getTaskList = document.querySelector('#lista-tarefas');
+  addInputButton.addEventListener('click', function () {
     if (getInputField.value.length > 0) {
-      let newLi = document.createElement('li');
+      const newLi = document.createElement('li');
       newLi.innerText = getInputField.value;
-
-      getTaskList.appendChild(newLi);
+      getTaskList.appendChild(newLi);     
       getInputField.value = '';
-    } else if (getInputField.value.length = 0) {
-      alert('Error: Digite ao menos 1 caractere.');
-    }
-  })
-
-  getInputField.addEventListener('keyup', function() {
-    if (event.keyCode === 13 && getInputField.value.length > 0) {
-      let newLi = document.createElement('li');
-      newLi.innerText = getInputField.value;
-
-      getTaskList.appendChild(newLi);
-      getInputField.value = '';
-    }
+    } 
   });
+  getInputField.addEventListener('keyup', function (event) {
+    evento = event;
+    if (evento.keyCode === 13 && getInputField.value.length > 0) {
+      const newLi = document.createElement('li');
+      newLi.innerText = getInputField.value;
+      getTaskList.appendChild(newLi);
+      getInputField.value = '';
+    } 
+  })
 };
 adicionaTarefa();
+
+const selecionaItem = document.querySelector('#lista-tarefas');
+selecionaItem.addEventListener("click", function (event) {
+  let evento = event.target;
+  let tarefas = document.getElementsByTagName('li');
+  for (index = 0; index < tarefas.length; index += 1) {
+    tarefas[index].style.backgroundColor = '';
+  }
+  evento.style.backgroundColor = 'rgb(128, 128, 128)';
+})
