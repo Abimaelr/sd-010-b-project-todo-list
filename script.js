734132bc40ef88c6f768d9a808f644d1bae4fef6
Lucas Martins da Silva <lucasmartins.dsilva@gmail.com>
2021-03-10 15:59:04 -0300

    Adicionando o 7º requisito

--- script.js
+++ script.js
@@ -9,3 +9,8 @@ function createTasks() {
   taskInput.value = '';
 }
 createTasksBtn.addEventListener('click', createTasks);
+
+toDoList.addEventListener('click', (event) => {
+  const element = event;
+  element.target.style.backgroundColor = 'rgb(128, 128, 128)';
+}, false);
