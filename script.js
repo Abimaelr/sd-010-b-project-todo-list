f2e2d557d00962467b82399e16ec23fa5914e05a
bidaffonso <bidaffondo@gmail.com>
2021-03-10 15:23:51 -0400

    requisitos 3 e 4

--- script.js
+++ script.js
@@ -2,3 +2,11 @@ const func = document.createElement('p');
 func.id = 'funcionamento';
 func.innerHTML = 'Clique duas vezes em um item para marcá-lo como completo';
 document.body.appendChild(func);
+
+const text = document.createElement('input');
+text.id = 'texto-tarefa';
+document.body.appendChild(text);
+
+const taskList = document.createElement('ol');
+taskList.id = 'lista-tarefas';
+document.body.appendChild(taskList);
