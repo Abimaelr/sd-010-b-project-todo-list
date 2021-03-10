8bff4382000e2403959f5c60e0912b8338ccf6a9
esionascimento <esio.392@protonmail.com>
2021-03-10 15:24:18 -0300

    adicionado na lista ordenada

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ function addHeader() {
   aux.innerText = 'Minha Lista de Tarefas';
   leBoby.appendChild(aux);
 }
+
 addHeader();
 function addP() {
   let aux = document.createElement('p');
@@ -11,12 +12,14 @@ function addP() {
   aux.innerText = 'Clique duas vezes em um item para marcá-lo como completo';
   leBoby.appendChild(aux);
 }
+
 addP();
 function addInput() {
   let aux = document.createElement('input');
   aux.id = 'texto-tarefa';
   leBoby.appendChild(aux);
 }
+
 addInput();
 function addButton() {
   let aux = document.createElement('button');
@@ -24,10 +27,22 @@ function addButton() {
   aux.innerText = 'Adicionar';
   leBoby.appendChild(aux);
 }
+
 addButton();
 function addOl() {
   let aux = document.createElement('ol');
   aux.id = 'lista-tarefas';
   leBoby.appendChild(aux);
 }
+
 addOl();
+let auxButton = document.querySelector('#criar-tarefa');
+auxButton.addEventListener('click', addLista);
+function addLista() {
+  let auxOl = document.querySelector('#lista-tarefas');
+  let auxLi = document.createElement('li');
+  let auxInput = document.querySelector('#texto-tarefa');
+  auxLi.innerText = auxInput.value;
+  auxOl.appendChild(auxLi);
+  auxInput.value = '';
+}
