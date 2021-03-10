31dd6cd860281d782633c817cc3deb6f5af13f1e
Gustavo Cerqueira <gmcerqueira93@gmail.com>
2021-03-10 13:41:50 -0300

    Realizando o requisito obrigatorio 11

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const createTaskButton = document.querySelector('#criar-tarefa');
 const taskInput = document.querySelector('#texto-tarefa');
 const taskList = document.querySelector('#lista-tarefas');
 const deleteTasksButton = document.querySelector('#apaga-tudo');
+const removeDoneTasksButton = document.querySelector('#remover-finalizados');
 
 // Adicionando uma tarefa a lista de tarefas
 function addTask() {
@@ -9,40 +10,44 @@ function addTask() {
     const listItem = document.createElement('li');
     listItem.innerHTML = taskInput.value;
     listItem.className = 'tarefa';
+    listItem.addEventListener('click', () => { // Selecionando uma tarefa
+      const taskItems = document.querySelectorAll('.tarefa');
+      for (let i = 0; i < taskItems.length; i += 1) {
+        taskItems[i].classList.remove('selected');
+      }
+
+      listItem.classList.add('selected');
+    });
+    listItem.addEventListener('dblclick', () => { // Marcando uma tarefa que já foi realizada
+      listItem.classList.toggle('completed');
+    });
     taskList.appendChild(listItem);
-    // taskInput.value = '';
+    taskInput.value = '';
   });
 }
 
-// Selecionando uma tarefa da lista
-function selectTask() {
-  taskList.addEventListener('click', (event) => {
-    const taskItems = document.querySelectorAll('.tarefa');
-    for (let i = 0; i < taskItems.length; i += 1) {
-      taskItems[i].classList.remove('selected');
-    }
-
-    event.target.classList.add('selected');
-  });
-}
-
-// Marcando uma tarefa que já foi realizada
-function doneTask() {
-  taskList.addEventListener('dblclick', (event) => {
-    event.target.classList.toggle('completed');
-  });
-}
-
-// Deletando dota a lista de tarefas
-function deleteAllTasks() {
+// Deletando toda a lista de tarefas
+function clearTasks() {
   deleteTasksButton.addEventListener('click', () => {
     taskList.innerHTML = '';
   });
 }
 
+// Deletando as tarefas já realizadas da lista
+function removeDoneTasks() {
+  removeDoneTasksButton.addEventListener('click', () => {
+    const done = [];
+    taskList.childNodes.forEach((child) => {
+      if (child.classList.contains('completed')) done.push(child);
+    });
+    for (let i = 0; i < done.length; i += 1) {
+      taskList.removeChild(done[i]);
+    }
+  });
+}
+
 window.onload = () => {
   addTask();
-  selectTask();
-  doneTask();
-  deleteAllTasks();
+  clearTasks();
+  removeDoneTasks();
 };
