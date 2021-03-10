e62ce43bf3b445eaa3fce3f5c82fe6ed136a25f5
Abimael <abimaelr.albu@gmail.com>
2021-03-10 16:22:05 -0300

    9 requisito melhorei a manipulacao da clearListClass

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const inputField = document.getElementById('texto-tarefa');
 const list = document.getElementById('lista-tarefas');
 const btn = document.getElementById('criar-tarefa');
 
-btn.addEventListener ('click',function(){
+btn.addEventListener ('click', function(){
     const listItem = document.createElement('li');
     listItem.innerText = inputField.value;
     listItem.className = 'li'
@@ -10,16 +10,34 @@ btn.addEventListener ('click',function(){
     list.appendChild(listItem);
 })
 
-function itemSelection(evt){
+function itemSelection (evt){
+    clearListClass ();
+    evt.target.className += ' selected';
+}
+let opa 
+function clearListClass (){
     //limpar os outros Itens
     const listItem = document.querySelectorAll('.li')
-
     for (let index = 0; index < listItem.length; index += 1){
-       listItem[index].className = 'li'
+        let classes = `${listItem[index].className}`;
+        const classe = classes.replace('selected','');
+        listItem[index].className = classe;
     }
-
-
-    evt.target.className = 'li selected'
 }
 
-list.addEventListener('click',itemSelection,false)
+
+
+function itemCheck(evt){
+    const obj = evt.target.className;
+    const classes = obj.split(' ')
+
+  
+    if(classes.includes('completed')){
+        obj.replace('completed','');
+    } 
+    else 
+        evt.target.className += ' completed'
+}
+
+list.addEventListener ('dblclick', itemCheck, false)
+list.addEventListener ('click', itemSelection, false)
