69ff3ebd7e2ff55ac5234b406ea48080c8d4b6d0
Jonnes Bezerra <jonnes.bezerra@gmail.com>
2021-03-11 14:53:40 -0300

    7 - Clicar em um item da lista deve alterar a cor de fundo do item para cinza rgb(128,128,128) DONE

--- script.js
+++ script.js
@@ -2,9 +2,11 @@ const list = document.querySelector('#lista-tarefas');
 const button = document.querySelector('#criar-tarefa');
 const input = document.querySelector('#texto-tarefa');
 
+// add a task to the Task List
 function addTask() {
   button.addEventListener('click', function () {
     const listItem = document.createElement('li');
+    listItem.addEventListener('click', selectItem);
     if (input.value !== '') { // I took this IF idea from Bruno Afonso's PR: https://github.com/tryber/sd-010-b-project-todo-list/blob/brunoAffonso-project-tod-list/script.js
       listItem.innerHTML = input.value; // I took this logic from http://embuscadocodigo.com/codigos/javascript/capturar-valor-digitado-javascript.html
       list.appendChild(listItem);
@@ -14,3 +16,8 @@ function addTask() {
 }
 
 addTask();
+
+function selectItem() {
+  let clickedItem = event.target;
+  clickedItem.style.backgroundColor = 'rgb(128, 128, 128)';
+}
