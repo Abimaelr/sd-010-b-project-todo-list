b7ed6dd103f10e5ea342a35f494fbc7bc170a70e
Carlos Vieira <appencigr@gmail.com>
2021-03-11 11:40:29 -0300

    Requisito 6 e 7 Feito!

--- script.js
+++ script.js
@@ -1,16 +1,25 @@
+const bgColor = 'rgb(128, 128, 128)';
 const selectionOl = document.getElementById('lista-tarefas');
 const inputText = document.getElementById('texto-tarefa');
+inputText.focus();
 
-function criarTarefa() {
+function newTask() {
   if (inputText.value === '') {
     alert('Digite uma Tarefa');
+    inputText.focus();
   } else {
     const newLi = document.createElement('li');
     newLi.innerText = inputText.value;
     selectionOl.appendChild(newLi);
     inputText.value = '';
+    inputText.focus();
   }
 }
+// count.style.backgroundColor = 'rgb (128, 128, 128)';
 // Escutador de Eventos
-const adicionarTarefas = document.getElementById('criar-tarefa');
-adicionarTarefas.addEventListener('click', criarTarefa);
+const addTask = document.getElementById('criar-tarefa');
+addTask.addEventListener('click', newTask);
+const selectTask = document.getElementById('lista-tarefas');
+selectTask.addEventListener('click', (element) => {
+  element.target.style.background = bgColor;
+});
