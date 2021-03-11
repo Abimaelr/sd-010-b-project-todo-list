c4c69bc1d70742ee69952348b36d43d62c853cd3
Paulo Xavier <pxavierf@gmail.com>
2021-03-10 23:48:20 -0300

    craindo elemento da lista

--- script.js
+++ script.js
@@ -5,3 +5,23 @@ const deleteAll = document.getElementById('apaga-tudo');
 const deleteComplete = document.getElementById('remover-finalizados')
 const listItem = document.getElementsByTagName('li');
 
+saveButton.addEventListener('click', () => {
+  addTask();
+});
+
+function addTask(taskTodo) {
+  let taskText = task.value;
+  if (taskTodo) {
+    taskText = taskTodo.text;
+  }
+  if (taskText) {
+    const taskEl = document.createElement('li');
+    taskEl.classList.add('list');
+    taskEl.innerText = taskText;
+    orderedlist.appendChild(taskEl)
+
+   
+
+  }
+  task.value = '';
+}
