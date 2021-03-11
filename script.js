13540915da5b875f02eb110a8a20e38cddaa69d9
Dieghonm <dieghonm@gmail.com>
2021-03-11 03:49:54 -0300

    Quesito 7 tentativa 1

--- script.js
+++ script.js
@@ -1,22 +1,32 @@
-let entradaDeTarefa
+
+let tamanhoDaLista =[]
+
+// **************Armazena o texto****************
+
+let entradaDeTarefa;
 let caixaDeEntrada = document.getElementById('texto-tarefa');
-caixaDeEntrada.addEventListener('keyup',tarefa);
+caixaDeEntrada.addEventListener('keyup', tarefa);
 
-function tarefa () {
- entradaDeTarefa = caixaDeEntrada.value
+function tarefa()  {
+ entradaDeTarefa = caixaDeEntrada.value;
 }
-
-let paiDaLista = document.getElementById('lista-tarefas')
+// **************Adciona o texto na lista****************
+let paiDaLista = document.getElementById('lista-tarefas');
 let botão = document.getElementById('criar-tarefa');
-botão.addEventListener('click',vaiPraLista); 
+botão.addEventListener('click', vaiPraLista); 
 
-function vaiPraLista () {
+function vaiPraLista() {
   let li = document.createElement('li');
-  li.innerText = entradaDeTarefa
-  caixaDeEntrada.value = ''
+  li.innerText = entradaDeTarefa;
+  li.className = 'lista'
+  caixaDeEntrada.value = '';
   paiDaLista.appendChild(li);
-
+  tamanhoDaLista=document.querySelectorAll('.lista')
+  for (let cont = 0; cont < tamanhoDaLista.length; cont += 1) {
+    tamanhoDaLista[cont].addEventListener('click',function () {
+      this.className = 'lista grey';
+   });
+  }
 }
-
-
+// ***********Pinta de cinza o item selecionado***************
 
