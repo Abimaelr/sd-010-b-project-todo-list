c8c34142184ceb41f61adab14dbed4d20af71214
renatogsouza <renatomacedo1995@gmail.com>
2021-03-10 23:02:20 -0300

    requisito 7 em andamento

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
 let textInput = document.getElementById('texto-tarefa');
 let buton = document.getElementById('criar-tarefa');
 let lista = document.getElementById('lista-tarefas');
-
+let clickList = document.querySelectorAll('ol');
 
 buton.addEventListener('click', function(){  
   let novaLista = document.createElement('li');
   lista.appendChild(novaLista).innerText = textInput.value;
   textInput.value = '';  
 });
+
+for (let i = 0; i < clickList.length; i++) {
+clickList[i].addEventListener('click', function() {
+  event.target.style.color = 'rgb(128, 128, 128)';
+  console.log('teste');
+});
+}
