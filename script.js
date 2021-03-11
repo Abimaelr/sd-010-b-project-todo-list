db51157556cd1fc5055e89351490a6c09c352d78
legalvao1 <legalvao1@gmail.com>
2021-03-11 17:34:54 -0300

    salvando os elementos selecionados no storage

--- script.js
+++ script.js
@@ -1,22 +1,20 @@
 const buttom = document.querySelector('#criar-tarefa');
-buttom.innerHTML = 'Adicionar';
 
 const caixaTexto = document.querySelector('#texto-tarefa');
-const todoList = document.querySelector('#lista-tarefas');
+const lista = document.querySelector('#lista-tarefas');
 
 function listaDeTarefas() {
   buttom.addEventListener('click', function() {
     const listItem = document.createElement('li'); // Adicionando um elemento a lista
     listItem.innerHTML = caixaTexto.value; // adicionando o valor da caixa texto 
-    todoList.appendChild(listItem);
+    lista.appendChild(listItem);
     caixaTexto.value = '';
   });
 }
 listaDeTarefas();
 
 function coloreFundo() {
-  const tag = document.querySelector('#lista-tarefas'); // pego todos os elem de ol
-  tag.addEventListener('click', function(evento) { // adiciono o evento para o pai
+  lista.addEventListener('click', function(evento) { // adiciono o evento para o pai
     const selecionado = document.querySelector('.selected'); // pego o elem com a classe selected
     if (selecionado) { // se tiver selecionado
     selecionado.classList.remove('selected'); // removo a classe dele
@@ -27,18 +25,15 @@ function coloreFundo() {
 coloreFundo();
 
 function tarefaCompleta() {
-  const tarefas = document.querySelector('#lista-tarefas'); 
-  tarefas.addEventListener('dblclick', function(evento){
+  lista.addEventListener('dblclick', function(evento){
     evento.target.classList.toggle('completed'); // adiciona e remove a classe quando há o dblclick
   });
 }
 tarefaCompleta();
 
 const botaoApagaTudo = document.querySelector('#apaga-tudo')
-botaoApagaTudo.innerHTML = 'Limpar Lista'
 
 function apagaItensDaLista(){
-    const lista = document.querySelector('#lista-tarefas'); // porem a minha lista aqui ainda é zero
     const itensLista = lista.getElementsByTagName('li')
 
     botaoApagaTudo.addEventListener('click', function() {
@@ -49,20 +44,34 @@ function apagaItensDaLista(){
 }
 apagaItensDaLista();
 
-
 // Requisito 11 - Remove os itens com a classe completed
 const botaoRemoveCompletos = document.querySelector('#remover-finalizados');
-  botaoRemoveCompletos.innerHTML = 'Limpar Completos';
 
 function removeCompletos(){
-  const lista = document.querySelector('#lista-tarefas')
   const tarefaFinalizada = document.getElementsByTagName('li');
   for (let i = 0; i < tarefaFinalizada.length; i += 1) {
-    if(tarefaFinalizada[i].classList.contains('completed')){
-      lista.removeChild(tarefaFinalizada[i]);
+    if(tarefaFinalizada[i].classList.contains('completed')){ 
+      lista.removeChild(tarefaFinalizada[i]); // Para conseguir remover todos usei a mesma ideia da Alessandra Resende
       i -= 1;
     }
   }
 }
 botaoRemoveCompletos.addEventListener('click', removeCompletos);
   
+// Requisito 12 - Salvar tarefas 
+
+const botaoSalvaTarefa = document.querySelector('#salvar-tarefas');
+
+function salvarValoresLista(){
+  localStorage.setItem('listaDeTarefas', lista.innerHTML); // salvo os valores da lista(ol) no storage
+}
+botaoSalvaTarefa.addEventListener('click', salvarValoresLista);
+
+// Pegar as tarefas salvas
+
+function pegaValoresSalvos() {
+  if (localStorage.getItem('listaDeTarefas') !== '') { // se a minha lista de tarefas não for vazia
+    lista.innerHTML = (localStorage.getItem('listaDeTarefas')); // pego os valores dela
+  }
+}
+window.onload = pegaValoresSalvos; // ao carregar a página chamo essa função
