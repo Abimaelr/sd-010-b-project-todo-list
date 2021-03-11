3de679b9dc5433466d59488b779483fcf482aeed
isTorres <ismaeltorres612@gmail.com>
2021-03-11 10:15:35 -0300

    requisito 5

--- script.js
+++ script.js
@@ -8,43 +8,42 @@ function createElement(tag) {
 
 const main = document.getElementById('main');
 
-// const title = createElement('header');
-// title.innerHTML = 'Minha Lista de Tarefas';
-// main.appendChild(title);
-// title.id = 'title';
+const title = createElement('h1');
+title.innerHTML = 'Minha Lista de Tarefas';
+main.appendChild(title);
+title.id = 'title';
 
-// const funcionamento = createElement('p');
-// funcionamento.innerHTML = 'Clique duas vezes em um item para marcá-lo como completo';
-// main.appendChild(funcionamento);
-// funcionamento.id = 'funcionamento';
+const funcionamento = createElement('p');
+funcionamento.innerHTML = 'Clique duas vezes em um item para marcá-lo como completo';
+main.appendChild(funcionamento);
+funcionamento.id = 'funcionamento';
 
-// const input = createElement('input');
-// input.id = 'texto-tarefa';
-// main.appendChild(input);
+const input = createElement('input');
+input.id = 'texto-tarefa';
+main.appendChild(input);
 
-// const ordenedList = createElement('ol');
-// ordenedList.id = 'lista-tarefas';
-// main.appendChild(ordenedList);
+const ordenedList = createElement('ol');
+ordenedList.id = 'lista-tarefas';
+main.appendChild(ordenedList);
 
 const button = createElement('button');
-button.innerHTML = 'Cria tarefa';
-button.id = 'cria-tarefa';
+button.innerHTML = 'Criar tarefa';
+button.id = 'criar-tarefa';
 main.appendChild(button);
 
 function addNewTask() {
   const inputValue = document.getElementById('texto-tarefa');
   // const button = document.getElementById('cria-tarefa');
-  const ordenedList = document.getElementById('lista-tarefas');
+  // const ordenedList = document.getElementById('lista-tarefas');
 
   button.addEventListener('click', () => {
-    console.log(inputValue.value);
-    const newItem = createElement('li');
-    newItem.innerHTML = inputValue.value;
-    ordenedList.appendChild(newItem);
-
-    inputValue.innerHTML = ' ';
-    // if (inputValue.length > 0) {
-    // }
+    // console.log(inputValue.value);
+    if (inputValue.value.length > 0) {
+      const newItem = createElement('li');
+      newItem.innerHTML = inputValue.value;
+      inputValue.value = '';
+      ordenedList.appendChild(newItem);
+    }
   });
 }
 addNewTask();
