546f3002b4bd76969f74f96023206c1ab4f104fb
Aremtior <marcosleandrofl@gmail.com>
2021-03-11 17:17:54 -0300

    requisito 14 - bonus botao remover tarefa selecionada

--- script.js
+++ script.js
@@ -6,10 +6,12 @@ function taskSelect(e) {
     taskSelected.classList.remove('selected');
   }
   e.target.classList.add('selected');
+  saveTasksList();
 }
 
 function taskCompleted(e) {
   e.target.classList.toggle('completed');
+  saveTasksList();
 }
 
 function addTask() {
@@ -25,6 +27,23 @@ function addTask() {
   }
 }
 
+function saveTasksList() {
+  const tasks = document.getElementsByClassName('task');
+  const items = [];
+  const classes = [];
+  if (tasks.length > 0) {
+    
+    for (let i = 0; i < tasks.length; i += 1) {
+      items.push(tasks[i].innerHTML);
+      classes.push(tasks[i].className);
+      localStorage.setItem('tasks', JSON.stringify(items));
+      localStorage.setItem('classes', JSON.stringify(classes)); // https://stackoverflow.com/questions/58422340/trying-to-save-all-generated-li-elements-to-local-storage-using-javascript
+    }
+  } else {
+    localStorage.clear();
+  }
+}
+
 function removeAllTasks() {
   taskList.innerHTML = '';
   localStorage.clear();
@@ -36,21 +55,17 @@ function removeCompletedTasks() {
   for (let i = 0; i < completedTasks.length; i += 1) {
     completedTasks[i].remove();
   }
-  saveTasksList();
+  // saveTasksList();
   if (completedTasks.length > 0) {
     removeCompletedTasks();
   }
 }
 
-function saveTasksList() {
-  const tasks = document.getElementsByClassName('task');
-  const items = [];
-  const classes = [];
-  for (let i = 0; i < tasks.length; i += 1) {
-    items.push(tasks[i].innerHTML);
-    classes.push(tasks[i].className);
-    localStorage.setItem('tasks', JSON.stringify(items));
-    localStorage.setItem('classes', JSON.stringify(classes)); // https://stackoverflow.com/questions/58422340/trying-to-save-all-generated-li-elements-to-local-storage-using-javascript
+function removeSelectedTasks() {
+  const selectedTask = document.querySelector('.selected');
+  if (selectedTask) {
+    selectedTask.remove();
+    saveTasksList();
   }
 }
 
@@ -66,14 +81,18 @@ function addList(storedTask, classTask) {
 // moveUp e moveDown baseado em: https://stackoverflow.com/questions/34913953/move-an-element-one-place-up-or-down-in-the-dom-tree-with-javascript
 function moveUp() {
   const element = document.querySelector('.selected');
-  if(element && element.previousElementSibling)
-  element.parentNode.insertBefore(element, element.previousElementSibling);
+  if (element && element.previousElementSibling) {
+    element.parentNode.insertBefore(element, element.previousElementSibling);
+    saveTasksList();
+  }
 }
 
 function moveDown() {
   const element = document.querySelector('.selected');
-  if(element && element.nextElementSibling)
-  element.parentNode.insertBefore(element.nextElementSibling, element);
+  if (element && element.nextElementSibling) {
+    element.parentNode.insertBefore(element.nextElementSibling, element);
+    saveTasksList();
+  }
 }
 
 function init() {
@@ -95,6 +114,9 @@ btnRmvAll.addEventListener('click', removeAllTasks);
 
 const btnRmvCompleted = document.getElementById('remover-finalizados');
 btnRmvCompleted.addEventListener('click', removeCompletedTasks);
+
+const btnRmvSelected = document.getElementById('remover-selecionado');
+btnRmvSelected.addEventListener('click', removeSelectedTasks);
 
 const btnSave = document.getElementById('salvar-tarefas');
 btnSave.addEventListener('click', saveTasksList);
