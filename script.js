957eebfcbe64bba2f026b40f5d8d21d869f73f27
RobsonCardozo <robsonleaocardozo@hotmail.com>
2021-03-11 13:18:13 -0300

    task 7 1.0

--- script.js
+++ script.js
@@ -1,14 +1,32 @@
-const createListButton = document.getElementById('criar-tarefa');
+const createListButton = document.getElementById('criar-tarefa'); // constante recebe button
 
 function createListItem() {
-  const list = document.getElementById('lista-tarefas');
-  const capture = document.getElementById('texto-tarefa').value;
-  const newListItem = document.createElement('li');
+  const listCreate = document.getElementById('lista-tarefas'); // list recebe ol
+  const capture = document.getElementById('texto-tarefa'); // capture recebe input text
+  const newListItem = document.createElement('li'); // cria novo item de lista
 
-  newListItem.innerText = capture;
-  list.appendChild(newListItem);
-
-  document.getElementById('texto-tarefa').value = ''; // para limpar o input
+  newListItem.innerText = capture.value; // novo item de lista + innerText recebe input value
+  listCreate.appendChild(newListItem); // novo item de lista é acomplado à lista como filho
+  capture.value = ''; // limpa o input com string vazia
 }
 
-createListButton.addEventListener('click', createListItem);
+function createBgColor(cgColor) {
+  const listColor = document.querySelectorAll('ol li');
+
+  for (let index = 0; index < listColor.length; index += 1) {
+    listColor[index].style.backgroundColor = 'rgb(256, 256, 256)';
+  }
+
+  cgColor.target.style.backgroundColor = 'rgb(128, 128, 128)';
+}
+
+function TheListener() {
+  const generalList = document.querySelectorAll('ol');
+
+  for (let index = 0; index < generalList.length; index += 1) {
+    generalList[index].addEventListener('click', createBgColor);
+  }
+}
+
+createListButton.addEventListener('click', createListItem); // adiciona função ao button
+TheListener();
