a665e76fcd0dbe4ce6643be35b7c5f0ff1d6944c
edersonrodara <edersonrodara@gmail.com>
2021-03-11 14:20:23 -0300

    requisito 6 e 7 feito

--- script.js
+++ script.js
@@ -6,7 +6,28 @@ function makeTarefa() {
   if (textoTarefa == '') {} else {
     let makeLi = document.createElement('li');
     document.getElementById('lista-tarefas').appendChild(makeLi).innerHTML = textoTarefa;
-		document.getElementById('texto-tarefa').value = '';
+    document.getElementById('texto-tarefa').value = '';
   }
+}
 
+let listaTarefas = document.getElementById('lista-tarefas');
+listaTarefas.addEventListener('dblclick', changeColor);
+
+function changeColor(event) {  
+	let element = document.getElementById('lista-tarefas').childNodes;		
+	for (let index = 0; index < element.length; index += 1) {
+		let x = document.getElementById('lista-tarefas').childNodes[index];
+		if (event.target[index] != x) {
+			element[index].classList.remove('backColor');
+		}
+		if (event.target == element[index]) {
+			element[index].classList.add('backColor');			
+		}
+	}
+}
+
+listaTarefas.addEventListener('dblclick', riscando);
+
+function riscando(event) {
+	event.target.classList.toggle('through');
 }
