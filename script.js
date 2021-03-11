236f101298048a5f2ae0dcc04360dc83c4539e06
Jonnes Bezerra <jonnes.bezerra@gmail.com>
2021-03-11 18:19:32 -0300

    10 - Adicione um botão com id=apaga-tudo que quando clicado deve apagar todos os itens da lista DONE

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
 const list = document.querySelector('#lista-tarefas');
 const button = document.querySelector('#criar-tarefa');
 const input = document.querySelector('#texto-tarefa');
+const delButton = document.querySelector('#apaga-tudo');
 
 // add a task to the Task List
+// I took this IF idea from Bruno Afonso's PR: https://github.com/tryber/sd-010-b-project-todo-list/blob/brunoAffonso-project-tod-list/script.js
 function addTask() {
   button.addEventListener('click', function () {
     const listItem = document.createElement('li');
     listItem.addEventListener('click', selectItem);
     listItem.addEventListener('dblclick', doneNotDone);
-    if (input.value !== '') { // I took this IF idea from Bruno Afonso's PR: https://github.com/tryber/sd-010-b-project-todo-list/blob/brunoAffonso-project-tod-list/script.js
+    if (input.value !== '') { 
       listItem.innerHTML = input.value;
       list.appendChild(listItem);
       input.value = '';
@@ -40,3 +42,13 @@ function doneNotDone() {
     completed.classList.add('completed');
   }
 }
+
+// I took this innerHTML idea from https://medium.com/front-end-weekly/remove-all-children-of-the-node-in-javascript-968ad8f120eb
+function deleteALL() {
+  delButton.addEventListener('click', function () {
+    console.log(list.innerHTML);
+    list.innerHTML = '';
+  });
+}
+
+deleteALL();
