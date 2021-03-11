f58be9679ec8dd4f7baa6c33b0e15ea93c7944d6
Lucas Rodrigues <lucasrpontesgarcia@gmail.com>
2021-03-11 16:44:53 -0300

    arrumando exercicio 1

--- script.js
+++ script.js
@@ -3,17 +3,21 @@ let linha = document.createElement('li');
 let lista = document.getElementById('lista-tarefas');
 let input = document.getElementById('texto-tarefa');
 let clickList = document.querySelectorAll('ol');
-
+//criação do botão
 button.addEventListener('click', function() {
     let linha = document.createElement('li');
     lista.appendChild(linha).innerText = input.value;
     input.value = ' ';
 })
-
+//colorir a linha que clicar
 
 for (let i = 0; i < clickList.length; i += 1) {
   clickList[i].addEventListener('click', function() {
-    event.target.style.backgroundColor = 'rgb(128, 128, 128)';
+    if (clickList[i].className === 'cor-de-fundo') {
+      clickList[i].classList.remove('cor-de-fundo')
+    } else {
+      event.target.classList.add('cor-de-fundo');
+    }
   })
 }
 
