7c754d898ed19572170695259dfadbceeb605562
DanielTrybe <darkdanielr@gmail.com>
2021-03-11 17:30:51 -0300

    7

--- script.js
+++ script.js
@@ -8,16 +8,19 @@ function inserirTarefa() {
   document.getElementById('texto-tarefa').value = "";
 }
 
-
-
-
 // site que me ajudou https://pt.stackoverflow.com/questions/375441/selecionar-um-select-pelo-valor-digitado-no-input
 
-let listaOrdenada = document.getElementsByClassName('itens');
-for (let index = 0; index < listaOrdenada.length; index += 1) {
-  listaOrdenada[index].addEventListener('click', function (event) {
-    document.getElementsByClassName('selected')[0].classList.remove('selected');
+let cinza = document.getElementById('lista-tarefas');
+cinza.addEventListener('click', function(event){
+    let cinza2 = document.getElementsByClassName('itens');
+    for (let index = 0; index < cinza2.length; index += 1) {
+    document.getElementsByClassName('itens')[index].classList.remove('selected');
     event.target.classList.add('selected');
+    }
+    
+})
 
-  })
-}
+// for (let index = 0; index < palet.length; index += 1) {
+//     palet[index].addEventListener('click', function (event) {
+//       document.getElementsByClassName('selected')[0].classList.remove('selected');
+      
