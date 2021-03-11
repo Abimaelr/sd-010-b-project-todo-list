33aff8fb79a30580bea251110213072bf2175f9d
isTorres <ismaeltorres612@gmail.com>
2021-03-10 23:07:43 -0300

    requisitos 1,2 e 3

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
-window.onload = function() {
-
+window.onload = function () {
 };
+
+function createElement(tag) {
+  const element = document.createElement(tag);
+  return element;
+}
+const main = document.getElementById('main');
+
+const title = createElement('h1');
+title.innerHTML = 'Minha Lista de Tarefas';
+main.appendChild(title);
+title.id = 'title';
+
+const funcionamento = createElement('p');
+funcionamento.innerHTML = 'Clique duas vezes em um item para marcá-lo como completo';
+main.appendChild(funcionamento);
+funcionamento.id = 'funcionamento';
+
+const input = createElement('input');
+input.id = 'texto-area';
+main.appendChild(input);
