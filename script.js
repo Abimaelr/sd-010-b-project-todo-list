75a5590af76290cdf155ddc68a373bb933ff90fd
joao966 <joaooliveira966@gmail.com>
2021-03-11 02:01:45 -0300

    requisito 10

--- script.js
+++ script.js
@@ -1,32 +1,30 @@
+const body = document.getElementById("corpo");
+
 function criaHeader (){
-    // Cria tag header com titulo
-    let maeTitulo = document.getElementById("corpo");
-    let header = document.createElement("header");
-    header.id = "titulo";
-    header.innerText = "Minha Lista de Tarefas";
-    maeTitulo.appendChild(header);
+  // Cria tag header com titulo
+  let header = document.createElement("header");
+  header.id = "titulo";
+  header.innerText = "Minha Lista de Tarefas";
+  body.appendChild(header);
 
-    //Estilo
+  //Estilo
 
-    header.style.marginLeft = "100px";
-    header.style.fontSize = "30px";
+  header.style.marginLeft = "100px";
+  header.style.fontSize = "30px";
 }
 criaHeader();
-
-
 
 ///////////////////////////////////////////////////////////////////
 
 function criaParagrafo (){
-    // Cria paragrafo
-    let maeParagrafo = document.getElementById("corpo");
-    let paragrafo = document.createElement("p");
-    paragrafo.id = "funcionamento";
-    paragrafo.innerText = "Clique duas vezes em um item para marcá-lo como completo";
-    maeParagrafo.appendChild(paragrafo);
+  // Cria paragrafo
+  let paragrafo = document.createElement("p");
+  paragrafo.id = "funcionamento";
+  paragrafo.innerText = "Clique duas vezes em um item para marcá-lo como completo";
+  body.appendChild(paragrafo);
 
-    //Estilo
-    paragrafo.style.marginLeft = "100px";
+  //Estilo
+  paragrafo.style.marginLeft = "100px";
 }
 criaParagrafo();
 
@@ -34,62 +32,52 @@ criaParagrafo();
 
 function criaInput (){
     // Cria input
-    let maeInput = document.getElementById("corpo");
     let label = document.createElement("label");
     label.innerText = "Digite o item";
     let input = document.createElement("input");
     input.id = "texto-tarefa";
-    maeInput.appendChild(label);
-    maeInput.appendChild(input);
+    body.appendChild(label);
+    body.appendChild(input);
 }
 criaInput();
-
 ////////////////////////////////////////////////////////////////////////
-
 function criaListaOrdenada (){
-    // Cria lista ordenada
-    let maeListaOrdenada = document.getElementById("corpo");
-    let listaOrdenada = document.createElement("ol");
-    listaOrdenada.id = "lista-tarefas";
-    maeListaOrdenada.appendChild(listaOrdenada);
+  // Cria lista ordenada
+  let listaOrdenada = document.createElement("ol");
+  listaOrdenada.id = "lista-tarefas";
+  body.appendChild(listaOrdenada);
 }
 criaListaOrdenada();
-
 //////////////////////////////////////////////////////////////////////////
-
 function criaBotao (){
-    // Cria botão
-    let maeBotao = document.getElementById("corpo");
-    let botao = document.createElement("button");
-    botao.innerText = "Click para adicionar tarefa";
-    botao.id = "criar-tarefa";
-    maeBotao.appendChild(botao);
+  // Cria botão
+  let botao = document.createElement("button");
+  botao.innerText = "Click para adicionar tarefa";
+  botao.id = "criar-tarefa";
+  body.appendChild(botao);
 }
 criaBotao();
-
 //////////////////////////////////////////////////////////////////////////////
-
 function valorInput (){
 // Pega valor Input
-    let pegaValorInput = document.getElementById("texto-tarefa");
-    let adicionaOl = document.getElementById("lista-tarefas");
-    let botaoAdiciona = document.getElementById("criar-tarefa");
+  let pegaValorInput = document.getElementById("texto-tarefa");
+  let adicionaOl = document.getElementById("lista-tarefas");
+  let botaoAdiciona = document.getElementById("criar-tarefa");
 
-    function adicionaItem (){
-        let lista = document.createElement("li");
-        lista.id = "item";
-        lista.innerText = pegaValorInput.value;
-        if (pegaValorInput.value == ""){
-        alert("ERRO 404")
-        }
-        pegaValorInput.value = "";
-
-        adicionaOl.appendChild(lista)
+  function adicionaItem (){
+    let lista = document.createElement("li");
+    lista.id = "item";
+    lista.innerText = pegaValorInput.value;
+    if (pegaValorInput.value == ""){
+    alert("ERRO 404")
     }
-    botaoAdiciona.addEventListener("click", adicionaItem);
+    pegaValorInput.value = "";
+
+    adicionaOl.appendChild(lista);
+  }
+  botaoAdiciona.addEventListener("click", adicionaItem);
 }
 valorInput();
-
 ///////////////////////////////////////////////////////////////////////////////////////////////
 
 // muda fundo dos itens
@@ -105,31 +93,36 @@ valorInput();
 //     });
 // }
 let pegaUl = document.querySelector("#lista-tarefas");
+pegaUl.addEventListener("click", mudaFundo );
 
-    pegaUl.addEventListener("click", mudaFundo );
+function mudaFundo (evento){
+  console.log("xablau")
+  let aux = evento.target;
 
-    function mudaFundo (evento){
-        console.log("xablau")
-        let aux = evento.target;
-
-        let cinza = document.getElementsByClassName("cinza");
-        for (let index = 0; index < cinza.length; index += 1){
-            cinza[index].className = "";
-        }
-
-        if (aux.className !== 'cinza'){
-            aux.className = 'cinza'; 
-            
-        } else {
-            aux.className = "";
-        }  
-    
+  let cinza = document.getElementsByClassName("cinza");
+  for (let index = 0; index < cinza.length; index += 1){
+      cinza[index].className = "";
+  }
+  if (aux.className !== 'cinza'){
+      aux.className = 'cinza'; 
       
+  } else {
+      aux.className = "";
+  } 
 }
 ////////////////////////////////////////////////////////////////////////////////
 
 
+let botao2 = document.createElement("button");
+botao2.id = "apaga-tudo";
+botao2.innerHTML = 'Limpar Lista';
+body.appendChild(botao2);
 
+let pegaBotao = document.getElementById("apaga-tudo");
+pegaBotao.addEventListener("click", function () {
+    let pegaLista = document.getElementById("lista-tarefas");
+    pegaLista.innerHTML = "";
+});
 
 /////////////////////////////////////////////////////////////////////////////////////////
 
@@ -149,7 +142,7 @@ let pegaUl = document.querySelector("#lista-tarefas");
 //         itens[i].classList.remove("selected");
 //     } 
 // } 
-
+/* 
 const selectItem = (item) => {
     const selectedElement = document.querySelector('.selected');
     if (selectedElement) {
@@ -160,6 +153,7 @@ const selectItem = (item) => {
     }
   };
   
+  
   const completeItem = (item) => {
     const { classList } = item;
     if (classList.contains('completed')) {
@@ -167,4 +161,4 @@ const selectItem = (item) => {
     } else {
       classList.add('completed');
     }
-  };
+  }; */
