edd20e71089d2257f2f647e034bde1e8ff1c23f4
Arthur Menezes Correa <arthurmcorrea282@gmail.com>
2021-03-11 16:37:45 -0300

    tentando a 7

--- script.js
+++ script.js
@@ -3,12 +3,22 @@ function criarTarefa() {
     let pegaLista = document.getElementById("lista-tarefas");
     let criarItemDaLista = document.createElement("li");
     criarItemDaLista.innerText = pegaTextoTarefa;
-    criarItemDaLista.classList.add("item-lista")
+    criarItemDaLista.classList.add("item-lista");
+
     let pegaTarefa = document.getElementById("texto-tarefa");
     pegaTarefa.value = ""
     pegaLista.appendChild(criarItemDaLista);
-    
+    pintar()
 }
+function pintar() {
+  let encontraFundoItem = document.getElementsByClassName("fundo-Item")[0];
+let encontraItensDaLista = document.querySelectorAll('.item-lista');
+encontraItensDaLista.forEach((element) => {
+        element.addEventListener('click', (event) => {
+        event.target.classList.add("fundo-Item")
+    })
+    })
+  }
 function apagarTudo() {
     let pegarLi = document.querySelectorAll('.item-lista');
     for (let i = 0;i <= pegarLi.length;i++) {
