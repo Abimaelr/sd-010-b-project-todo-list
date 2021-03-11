032b194e0a4dd5a3e1cd8de6a7a95366abe1a142
Flavio Andrade <foa12381@gmail.com>
2021-03-10 23:01:18 -0300

    adiciona a function completTask , com Marcos mantovani

--- script.js
+++ script.js
@@ -19,8 +19,34 @@ const addTaskStyle = (createElementLi) => {
 }
 
 
+const addSelectedTask =() => {
+listOl.addEventListener('click', function(event) {
+let selectedTask = document.querySelector('.selected');
+  if(selectedTask && event.target.classList.contains('task') ){
+  selectedTask.classList.remove('selected');
+  }
+  if(event.target.classList.contains('task')){
+    event.target.classList.add('selected');
+  }
+})
+}
+
+const addCompletedTask = ( ) => {
+  listOl.addEventListener('dblclick', function(event){
+  if(event.target.classList.contains('completed') ) {
+    event.target.classList.remove('completed')
+  }
+  else {
+    event.target.classList.add('completed');
+
+  }
+  })
+}
+
 window.onload = () => {
 creatTask();
+addSelectedTask();
+addCompletedTask();
 
 
 }
