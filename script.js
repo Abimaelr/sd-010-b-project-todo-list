7a577902f052caa6dd29a8609cb2bcda7f930a9a
Igor Machado <rf59imft@gmail.com>
2021-03-11 16:14:49 -0300

    DESAFIO 5

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-// teste
+function addNewItemList() {
+  let getInputButton = document.querySelector('#criar-tarefa');
+  let getInputField = document.querySelector('#texto-tarefa');
+  let getOl = document.querySelector('#lista-tarefas');
+
+  getInputButton.addEventListener('click', function () {
+    if (getInputField.value.length > 0) {
+      let newLi = document.createElement('li');
+      newLi.innerText = getInputField.value;
+      getOl.appendChild(newLi);
+      getInputField.value = '';
+    } else {
+      alert('Error: Digite ao menos 1 caractere.');
+    }
+  });
+}
+
+addNewItemList();
