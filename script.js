7687f3889d6106cce6b4972dacd961adc8bde4eb
riquee <strikedv@hotmail.com>
2021-03-11 15:53:06 -0300

    Finalizado correcao lint

--- script.js
+++ script.js
@@ -1,22 +1,18 @@
 const selected = {};
-
-const selector = (id) => document.querySelector(id);
-const selectorAll = (id) => document.querySelectorAll(id);
+const target = (id) => document.querySelector(id);
+const list = target('#lista-tarefas');
+const buttonCreate = target('#criar-tarefa');
+const clearAll = target('#apaga-tudo');
+const inputTask = target('#texto-tarefa');
+const clearSelect = target('#remover-selecionado');
+const moveUp = target('#mover-cima');
+const moveDown = target('#mover-baixo');
+const clearComplet = target('#remover-finalizados');
+const save = target('#salvar-tarefas');
 
 // Create Task
-const createTask = () => {
-  const button = selector('#criar-tarefa');
-  button.addEventListener('click', (create) => {
-    const task = document.createElement('li');
-    selector('#lista-tarefas').appendChild(task);
-    task.textContent = selector('#texto-tarefa').value;
-    selector('#texto-tarefa').value = '';
-    task.addEventListener('click', taskSelected);
-    task.addEventListener('dblclick', taskCompleted);
-  });
-};
-let taskSelected = (elem) => {
-  elem = elem.target;
+const taskSelected = (element) => {
+  const elem = element.target;
   if (!selected.task) {
     selected.task = elem;
     selected.task.classList.add('selected');
@@ -25,48 +21,51 @@ let taskSelected = (elem) => {
   selected.task = elem;
   selected.task.classList.add('selected');
 };
-let taskCompleted = (elem) => {
-  elem = elem.target;
+const taskCompleted = (element) => {
+  const elem = element.target;
   if (elem.classList.contains('completed')) {
     elem.classList.remove('completed');
   } else {
     elem.classList.add('completed');
   }
 };
+const createTask = () => {
+  buttonCreate.addEventListener('click', () => {
+    const task = document.createElement('li');
+    list.appendChild(task);
+    task.textContent = inputTask.value;
+    inputTask.value = '';
+    task.addEventListener('click', taskSelected);
+    task.addEventListener('dblclick', taskCompleted);
+  });
+};
 // Clear
 const clearTask = () => {
-  const button = selector('#apaga-tudo');
-  button.addEventListener('click', (clear) => {
-    for (task of selectorAll('#lista-tarefas li')) {
-      task.parentNode.removeChild(task);
-    }
+  clearAll.addEventListener('click', () => {
+    list.innerHTML = '';
   });
 };
 // Clear Completed
 const clearCompleted = () => {
-  let button = selector('#remover-finalizados');
-  button.addEventListener('click', (clear) => {
-    for (task of selectorAll('.completed')) {
-      task.parentNode.removeChild(task);
-    }
+  clearComplet.addEventListener('click', () => {
+    list.innerHTML = '';
   });
 };
 // Clear Completed
 const clearSelected = () => {
-  let button = selector('#remover-selecionado');
-  button.addEventListener('click', (clear) => {
+  clearSelect.addEventListener('click', () => {
     selected.task.parentNode.removeChild(selected.task);
   });
 };
 // Save Task
 const saveTask = () => {
-  let button = selector('#salvar-tarefas');
-  button.addEventListener('click', (clear) => {
+  save.addEventListener('click', () => {
     if (localStorage.getItem('saveTask')) localStorage.clear();
     const taskObj = {};
-    for (task of selectorAll('#lista-tarefas li')) {
+    const listLi = document.querySelectorAll('#lista-tarefas li');
+    listLi.forEach((task) => {
       taskObj[`${task.innerText}`] = task.className;
-    }
+    });
     localStorage.saveTask = JSON.stringify(taskObj);
   });
 };
@@ -74,48 +73,34 @@ const saveTask = () => {
 if (localStorage.getItem('saveTask')) {
   const loadTask = JSON.parse(localStorage.saveTask);
   let create;
-  for (task in loadTask) {
+  Object.keys(loadTask).forEach((task) => {
     create = document.createElement('li');
-    selector('#lista-tarefas').appendChild(create);
+    list.appendChild(create);
     create.addEventListener('click', taskSelected);
     create.addEventListener('dblclick', taskCompleted);
     create.className = loadTask[task];
     create.innerText = task;
     if (create.classList.contains('selected')) selected.task = create;
-  }
+  });
 }
 
-let moveTaskUp = () => {
-  let button = selector('#mover-cima');
-  button.addEventListener('click', (move) => {
-    if (selected.task) {
-      let target = selected.task.previousElementSibling;
-      if (target != null) {
-        selected.task.parentNode.insertBefore(
-          target,
-          selected.task.nextElementSibling
-        );
-      }
+const moveTaskUp = () => {
+  moveUp.addEventListener('click', () => {
+    if (selected.task && selected.task.previousElementSibling != null) {
+      selected.task.parentNode.insertBefore(
+        selected.task.previousElementSibling,
+        selected.task.nextElementSibling,
+      );
     }
   });
 };
 
-let moveTaskDown = () => {
-  let button = selector('#mover-baixo');
-  button.addEventListener('click', (move) => {
-    if (selected.task) {
-      let target = selected.task.nextElementSibling;
-      if (target != null) {
-        selected.task.parentNode.insertBefore(target, selected.task);
-      }
+const moveTaskDown = () => {
+  moveDown.addEventListener('click', () => {
+    if (selected.task && selected.task.nextElementSibling != null) {
+      selected.task.parentNode.insertBefore(target, selected.task);
     }
   });
-};
-
-let checkTaskSelected = (check) => {
-  for (task of selectorAll('#lista-tarefas li')) {
-    return console.log(task);
-  }
 };
 
 saveTask();
