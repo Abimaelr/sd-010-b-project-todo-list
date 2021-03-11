78fcc458adf34c77d0912e43739c7fb5438cd92e
limaluizpaulo <limaluizpaulo@live.com>
2021-03-10 23:02:16 -0300

    sexto e setimo requisitos concluidos

--- script.js
+++ script.js
@@ -1,12 +1,26 @@
-function addItem(){
-    let item = document.getElementById('texto-tarefa')
-    let list = document.getElementById('lista-tarefas')
-    let li = document.createElement('li');
-    li.innerText = item.value
-    item.value = "";
-    list.appendChild(li)
+// ouvindo o botão add
+document.getElementById('criar-tarefa').addEventListener('click', addItem)
 
+function addItem(){
+    let text = document.getElementById('texto-tarefa');
+    let list = document.getElementById('lista-tarefas');
+    let li = document.createElement('li');
+    li.innerText = text.value;
+    li.className = 'item';
+    text.value = "";
+    list.appendChild(li);
 }   
 
-let createButton = document.getElementById('criar-tarefa')
-createButton.addEventListener('click', addItem)
+function changeColor(event){
+    event.target.style.backgroundColor = 'rgb(128, 128, 128)'
+
+}
+
+function itensListener(){
+    const itemList = document.querySelectorAll('ol')
+    for(let index = 0; index < itemList.length; index++){
+    itemList[index].addEventListener('click', changeColor)
+    }
+}
+
+itensListener()
