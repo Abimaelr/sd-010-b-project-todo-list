b7bdff5bd61c6c4bfdfc935cb4615ea52d2f9ca1
Flavio Andrade <foa12381@gmail.com>
2021-03-11 00:11:18 -0300

    adiciona btnclean completed

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ let text = document.querySelector('#texto-tarefa'); //local digitar tarefa
 const addButton = document.querySelector('#criar-tarefa'); //btn add tarefa na minha lista
 const listOl = document.querySelector('#lista-tarefas'); //ol para criação da minha li
 const buttonClean = document.querySelector('#apaga-tudo')// btn apaga tudo
+const btnRemoveFinalizados =document.querySelector("#remover-finalizados")
 
 const  creatTask = () => {
  addButton.addEventListener('click',function(){
@@ -50,12 +51,26 @@ const buttonCleanList = () => {
 
 }
 
+const buttonCleanCompleted = () => {
+  btnRemoveFinalizados.addEventListener('click' , function(){
+    let completeds = document.querySelectorAll('li.completed')
+      for(index = 0; index < completeds.length; index += 1){
+        listOl.removeChild(completeds[index]);
+      }
+          // completeds.forEach((key)=>{
+          //   listOl.removeChild(key);
+          // })
+  })
+}
+
+
+
 
 window.onload = () => {
 creatTask();
 addSelectedTask();
 addCompletedTask();
 buttonCleanList();
-
+buttonCleanCompleted();
 
 }
