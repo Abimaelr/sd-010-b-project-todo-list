d25c6ac68c79ece65a7ff8c252a77d5ad7c67bae
Johnny-Batata <johnny.arguello007@gmail.com>
2021-03-11 11:22:56 -0300

    requisito 7

--- script.js
+++ script.js
@@ -1,14 +1,19 @@
 function botaoEnviar() {
-  const pai = document.getElementById('criar-tarefa')
-  pai.addEventListener('click', function() {
-  const input = document.getElementById('texto-tarefa');
-  const lista = document.getElementById('lista-tarefas');
-  const element = document.createElement('li')
-  element.innerHTML = input.value
-  lista.appendChild(element) 
-  input.value = ''
-})
-  
+  const pai = document.getElementById('criar-tarefa');
 
+  pai.addEventListener('click', function () {
+    const input = document.getElementById('texto-tarefa');
+    const lista = document.getElementById('lista-tarefas');
+    const element = document.createElement('li');
+    element.className = 'item';
+    element.innerHTML = input.value;
+    lista.appendChild(element);
+     element.addEventListener('click', function () {
+       element.style.backgroundColor = 'rgb(128, 128, 128)'
+       
+     })
+    input.value = '';
+  });
 }
-botaoEnviar()
+botaoEnviar();
+
