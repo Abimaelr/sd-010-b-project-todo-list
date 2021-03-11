2ffd06f4338a5f96eb5be62c56de751132930294
Paulo Henrique Lima <limapaulobsb@gmail.com>
2021-03-10 21:31:00 -0300

    Requisitos 7 e 8

--- script.js
+++ script.js
@@ -2,6 +2,17 @@ document.getElementById('criar-tarefa').onclick = function () {
   let listaTarefas = document.querySelector('#lista-tarefas');
   let tarefa = document.createElement('li');
   tarefa.innerText = document.querySelector('#texto-tarefa').value;
+  tarefa.className = 'tarefa';
   listaTarefas.appendChild(tarefa);
   document.querySelector('#texto-tarefa').value = '';
 };
+
+document.addEventListener('click', (event) => {
+  if (event.target.classList.contains('tarefa')) {
+    let tarefas = document.querySelectorAll('.tarefa');
+    for (let i = 0; i < tarefas.length; i += 1) {
+      tarefas[i].style.backgroundColor = null;
+    }
+    event.target.style.backgroundColor = "rgb(128, 128, 128)"
+  }
+});
