09d6800a358bb400f9ea75de994cb91cef8816d2
renatogsouza <renatomacedo1995@gmail.com>
2021-03-10 23:29:16 -0300

    adiciona o background

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ let buton = document.getElementById('criar-tarefa');
 let lista = document.getElementById('lista-tarefas');
 let clickList = document.querySelectorAll('ol');
 
+
 buton.addEventListener('click', function(){  
   let novaLista = document.createElement('li');
   lista.appendChild(novaLista).innerText = textInput.value;
@@ -11,7 +12,10 @@ buton.addEventListener('click', function(){
 
 for (let i = 0; i < clickList.length; i++) {
 clickList[i].addEventListener('click', function() {
-
-  event.target.style.backgroundColor = 'rgb(128, 128, 128)';
+  if (event.target.className === 'color'){
+    event.target.classList.remove('color');
+  } else {
+  event.target.classList.add('color');
+  }
 });
 }
