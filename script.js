1e954ca734f267de0676606a5c2ca3d9d392843b
Felippe Correa <felippecorrea2010@gmail.com>
2021-03-11 11:59:20 -0300

    Adicionando e modificando requisito 5

---
+++ script.js
@@ -0,0 +1,10 @@
+const listTask = document.getElementById('lista-tarefas');
+const inputField = document.getElementById('texto-tarefa');
+
+function createTask(event) {
+  const task = document.createElement('li');
+  listTask.appendChild(task);
+  task.textContent = event.target.value;
+}
+
+inputField.addEventListener('change', createTask);
