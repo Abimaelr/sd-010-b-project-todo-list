bf83ae271f0f5ca9e89e8dca1ab55fda46dbec61
Raphael Gumieri <raphaelgumieri@hotmail.com>
2021-03-11 16:09:40 -0300

    requisito 7

--- script.js
+++ script.js
@@ -8,10 +8,24 @@ function createLiText () {
   let getText = document.getElementById('texto-tarefa').value;
   let element = document.createElement('li');
   if (getText !== '') { 
-    element;
+    element.className = 'list-item';
     document.getElementById('lista-tarefas').appendChild(element).innerText = getText;
     document.getElementById('texto-tarefa').value = '';
   }
 }
 
 document.getElementById('criar-tarefa').addEventListener('click', createLiText);
+
+function changeColor (event) {
+  event.target.style.backgroundColor = 'rgb(128, 128, 128)';
+}
+
+function addEventToLi () {
+  let liArray = document.getElementsByClassName('list-item');
+  for (index = 0; index < liArray.length; index += 1) {
+    liArray[index].addEventListener('click', changeColor);
+  }
+}
+ 
+addEventListener('click', addEventToLi);
+
