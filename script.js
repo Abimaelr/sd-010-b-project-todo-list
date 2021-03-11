e4ed0ec5e087c8cc7931244a677aa67c35ddca58
Lucas Rodrigues <lucasrpontesgarcia@gmail.com>
2021-03-11 18:19:24 -0300

    exercicio 5

--- script.js
+++ script.js
@@ -3,23 +3,23 @@ let linha = document.createElement('li');
 let lista = document.getElementById('lista-tarefas');
 let input = document.getElementById('texto-tarefa');
 let clickList = document.getElementsByClassName('li-style');
+
 //criação do botão
 button.addEventListener('click', function() {
     let linha = document.createElement('li');
-    linha.className = 'li-style'
+    linha.classList.add('li-style');
     lista.appendChild(linha);
     linha.innerText = input.value;
-    input.value = ' ';
+    input.value = '';
 })
 //colorir a linha que clicar
+function mudaCor(event) {
+  event.target.classList.add('corDeFundo');
+}
+
 function linhaLista() {
   for (let i = 0; i < clickList.length; i += 1) {
-    clickList[i].addEventListener('click', function() {
-      if (clickList[i].className === 'cor-de-fundo') {
-        clickList[i].classList.remove('cor-de-fundo')
-      } else {
-        event.target.classList.add('cor-de-fundo');
-      }
-    })
+    clickList[i].addEventListener('click', mudaCor);
   }
 }
+linhaLista
