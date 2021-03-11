57bf32c6378f6fe6d01a66af0c0c0708325f9215
Aremtior <marcosleandrofl@gmail.com>
2021-03-10 21:25:59 -0300

    requisito 10 - botao apaga tudo.

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+const taskList = document.getElementById('lista-tarefas');
+
 function taskSelect(e) {
   const taskSelected = document.querySelector('.selected');
   if (taskSelected) {
@@ -13,7 +15,6 @@ function taskCompleted(e) {
 function addTask() {
   const newTask = document.getElementById('texto-tarefa');
   if (newTask.value !== '') {    
-    const taskList = document.getElementById('lista-tarefas');
     const task = document.createElement('li');
     task.className = 'task';
     task.innerHTML = newTask.value;
@@ -24,5 +25,12 @@ function addTask() {
   }
 }
 
+function removeAllTasks() {
+  taskList.innerHTML = '';
+}
+
 const btnAdd = document.getElementById('criar-tarefa');
 btnAdd.addEventListener('click', addTask);
+
+const btnRmvAll = document.getElementById('apaga-tudo');
+btnRmvAll.addEventListener('click', removeAllTasks);
