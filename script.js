5778a59e180b593718f3c9f8891395818ad7f6e1
Lucas Martins da Silva <lucasmartins.dsilva@gmail.com>
2021-03-11 20:26:18 -0300

    Adicionando o 12º requisito

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const toDoList = document.getElementById('lista-tarefas');
 const taskInput = document.getElementById('texto-tarefa');
 const deleteAllTasksBtn = document.getElementById('apaga-tudo');
 const deleteCompletedTasksBtn = document.getElementById('remover-finalizados');
+const saveTasksBtn = document.getElementById('salvar-tarefas');
 
 function createTasks() {
   const toDoListItem = document.createElement('li');
@@ -12,6 +13,7 @@ function createTasks() {
 }
 createTasksBtn.addEventListener('click', createTasks);
 
+// Explorar a resolução com querySelectorAll('#lista-tarefas li')
 toDoList.addEventListener('click', (event) => {
   const element = event.target;
   const selectedItem = document.querySelector('.selected');
@@ -36,3 +38,28 @@ deleteCompletedTasksBtn.addEventListener('click', () => {
     completedTask.remove();
   }
 }, false);
+
+saveTasksBtn.addEventListener('click', () => {
+  const toDoListItems = document.querySelectorAll('#lista-tarefas li');
+  let content = [];
+  let className = [];
+  let itemSalvos = { content, className };
+  for (let index = 0; index < toDoListItems.length; index += 1) {
+    content.push(toDoListItems[index].innerHTML);
+    className.push(toDoListItems[index].className);
+  }
+  localStorage.setItem('itens-salvos', JSON.stringify(itemSalvos));
+}, false);
+
+window.onload = () => {
+  const saveItemsLocalStorage = localStorage.getItem('itens-salvos');
+  if (saveItemsLocalStorage !== null) {
+    const saveItemsObj = JSON.parse(saveItemsLocalStorage);
+    for (let index = 0; index < saveItemsObj.content.length; index += 1) {
+      const toDoListItem = document.createElement('li');
+      toDoListItem.innerHTML = saveItemsObj.content[index];
+      toDoListItem.className = saveItemsObj.className[index];
+      toDoList.appendChild(toDoListItem);
+    }
+  }
+};
