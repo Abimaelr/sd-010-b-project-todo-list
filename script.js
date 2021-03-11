1662223a7e2f208ebbeb9c49fb5cea3a1b20f446
Lucas Rodrigues <lucasrpontesgarcia@gmail.com>
2021-03-11 16:54:13 -0300

    exercicio 5

--- script.js
+++ script.js
@@ -2,22 +2,24 @@ let button = document.getElementById('criar-tarefa');
 let linha = document.createElement('li');
 let lista = document.getElementById('lista-tarefas');
 let input = document.getElementById('texto-tarefa');
-let clickList = document.querySelectorAll('ol');
+let clickList = document.getElementsByClassName('li-style');
 //criação do botão
 button.addEventListener('click', function() {
     let linha = document.createElement('li');
-    lista.appendChild(linha).innerText = input.value;
+    linha.className = 'li-style'
+    lista.appendChild(linha);
+    linha.innerText = input.value;
     input.value = ' ';
 })
 //colorir a linha que clicar
-
-for (let i = 0; i < clickList.length; i += 1) {
-  clickList[i].addEventListener('click', function() {
-    if (clickList[i].className === 'cor-de-fundo') {
-      clickList[i].classList.remove('cor-de-fundo')
-    } else {
-      event.target.classList.add('cor-de-fundo');
-    }
-  })
+function linhaLista() {
+  for (let i = 0; i < clickList.length; i += 1) {
+    clickList[i].addEventListener('click', function() {
+      if (clickList[i].className === 'cor-de-fundo') {
+        clickList[i].classList.remove('cor-de-fundo')
+      } else {
+        event.target.classList.add('cor-de-fundo');
+      }
+    })
+  }
 }
-
