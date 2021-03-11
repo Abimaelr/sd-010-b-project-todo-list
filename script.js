99847ebe54f97770577b8b12aa5f4a4da45d8ec2
Johnny-Batata <johnny.arguello007@gmail.com>
2021-03-11 19:07:58 -0300

    requisito 9.1

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ function botaoEnviar() {
     const input = document.getElementById('texto-tarefa');
     const lista = document.getElementById('lista-tarefas');
     const element = document.createElement('li');
-    element.className = 'item';
+    element.classList = 'item';
     element.innerHTML = input.value;
     lista.appendChild(element);
       
@@ -19,14 +19,15 @@ function selectedClass() {
 
   pai.addEventListener('click', function(event) {
     const filho = document.getElementsByClassName('item');
-    
-    for (i = 0; i < filho.length; i += 1) {
-      if (filho[i].className !== 'item') {
-        filho[i].className = 'item';
+    for (i = 0; i < filho.length; i += 1){  
+   
+      if (filho[i].classList.contains('selected')) {
+        filho[i].classList.remove('selected');
         filho[i].style.backgroundColor = 'white';
-      }
-      event.target.className = 'item selected';
-      }     
+      } 
+      event.target.classList.add('selected');
+      
+    }         
 
     let batata = document.getElementsByClassName('item selected')[0];
     batata.style.backgroundColor = 'rgb(128, 128, 128)';
@@ -34,48 +35,21 @@ function selectedClass() {
 }
 selectedClass()
 
+
+
 function completedClass() {
+
   const pai = document.getElementById('lista-tarefas');
 
-  pai.addEventListener('dblclick', function(event) {
-    const filho = document.getElementsByClassName('item');
-    
-    for (i = 0; i < filho.length; i += 1) {
-      if (filho[i].className !== 'item') {
-        filho[i].className = 'item';
-        filho[i].style.backgroundColor = 'white';
-      }
-      event.target.className = 'item selected completed';
-      }     
-
-    let batata = document.getElementsByClassName('item selected completed')[0];
-    batata.style.backgroundColor = 'rgb(128, 128, 128)';
-  })
-}
-completedClass()
+  pai.addEventListener('dblclick', function (event) {
+    if ((event.target.classList.value).includes('completed')) {
+      event.target.classList.remove('completed');
+    } else {
+      event.target.classList.add('completed');
+    }
+  }); 
+  
+} completedClass()
+  
 
 
-
-
-
-
-
-
-// function botaoEnviar() {
-//   const pai = document.getElementById('criar-tarefa');
-
-//   pai.addEventListener('click', function () {
-//     const input = document.getElementById('texto-tarefa');
-//     const lista = document.getElementById('lista-tarefas');
-//     const element = document.createElement('li');
-//     element.className = 'item';
-//     element.innerHTML = input.value;
-//     lista.appendChild(element);
-//      element.addEventListener('click', function () {
-//        element.style.backgroundColor = 'rgb(128, 128, 128)'
-       
-//      })
-//     input.value = '';
-//   });
-// }
-// botaoEnviar();
