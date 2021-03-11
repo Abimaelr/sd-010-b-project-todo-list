7ee9b0fbab9321a8274884aa85d7a59f91a0250e
Leandro Xavier <leandrosxavi@gmail.com>
2021-03-11 18:56:07 -0300

    Tentativa 2 de limpar o campo input

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ addTask.addEventListener('click', function () {
     let taskLi = document.createElement('li');
     getTaskList.appendChild(taskLi);
     taskLi.innerText = taskText.value;
+    taskText.value = '';
     taskLi.addEventListener('click', function () {
       for (child = 0; child < getTaskList.children.length; child += 1){
         getTaskList.children[child].classList.remove('selected');
