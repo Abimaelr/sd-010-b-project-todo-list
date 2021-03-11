464ebb0b64da9be97979c1d4574efb30651d5968
legalvao1 <legalvao1@gmail.com>
2021-03-10 22:18:14 -0300

    Removendo os todos os elementos da lista - requisito 10

--- script.js
+++ script.js
@@ -5,32 +5,47 @@ const caixaTexto = document.querySelector('#texto-tarefa');
 const todoList = document.querySelector('#lista-tarefas');
 
 function listaDeTarefas() {
-  buttom.addEventListener('click', function(){
+  buttom.addEventListener('click', function() {
     const listItem = document.createElement('li'); // Adicionando um elemento a lista
-    listItem.innerHTML = caixaTexto.value;  // adicionando o valor da caixa texto 
+    listItem.innerHTML = caixaTexto.value; // adicionando o valor da caixa texto 
     todoList.appendChild(listItem);
     caixaTexto.value = '';
-  })
+  });
 }
 listaDeTarefas();
 
-function coloreFundo(){
-  const tag = document.querySelector('ol'); // pego todos os elem de ol
-  tag.addEventListener('click', function(evento){ // adiciono o evento para o pai
-    let selecionado = document.querySelector('.selected'); //pego o elem com a classe selected
-    if(selecionado){ // se tiver selecionado
-    selecionado.classList.remove('selected'); //removo a classe dele
+function coloreFundo() {
+  const tag = document.querySelector('#lista-tarefas'); // pego todos os elem de ol
+  tag.addEventListener('click', function(evento) { // adiciono o evento para o pai
+    const selecionado = document.querySelector('.selected'); // pego o elem com a classe selected
+    if (selecionado) { // se tiver selecionado
+    selecionado.classList.remove('selected'); // removo a classe dele
     }
     evento.target.classList.add('selected'); // e adiciono a classe ao clicado
   })
 }
 coloreFundo();
 
-function riscaTarefa(){
-    const tarefas = document.querySelector('ol'); 
-    tarefas.addEventListener('dblclick', function(evento){
-        evento.target.classList.toggle('completed'); // adiciona e remove a classe quando há o dblclick
-    })
-
+function riscaTarefa() {
+  const tarefas = document.querySelector('#lista-tarefas'); 
+  tarefas.addEventListener('dblclick', function(evento){
+    evento.target.classList.toggle('completed'); // adiciona e remove a classe quando há o dblclick
+  });
 }
 riscaTarefa();
+
+const botaoApagaTudo = document.querySelector('#apaga-tudo')
+botaoApagaTudo.innerHTML = 'Limpar Lista'
+
+function apagaItensDaLista(){
+    const lista = document.querySelector('#lista-tarefas'); // porem a minha lista aqui ainda é zero
+    const itensLista = lista.getElementsByTagName('li')
+
+    botaoApagaTudo.addEventListener('click', function() {
+      for(let i = itensLista.length -1 ; i >= 0; i -=1) { //parto do final da lista até zero removendo os elem
+        itensLista[i].remove();
+      }
+    }) 
+}
+apagaItensDaLista();
+
