c4833f71abd505066326085c00936ff495e67c74
Felippe Correa <felippecorrea2010@gmail.com>
2021-03-11 16:23:10 -0300

    Adicionando requisito 7

--- script.js
+++ script.js
@@ -3,9 +3,21 @@ const inputField = document.getElementById('texto-tarefa');
 
 function createTask(event) {
   const task = document.createElement('li');
+  task.className = 'color';
+  task.addEventListener('click', paintLi);
   listTask.appendChild(task);
   task.textContent = event.target.value;
   document.getElementById('texto-tarefa').value = '';
 }
 
 inputField.addEventListener('change', createTask);
+
+function paintLi(event) {
+ const itens = document.querySelectorAll('.color');
+ for (let index = 0; index < itens.length; index += 1) {
+   if (itens[index].classList.contains('gray')) {
+    itens[index].classList.remove('gray');
+   }
+   event.target.classList.add('gray');
+ }
+}
