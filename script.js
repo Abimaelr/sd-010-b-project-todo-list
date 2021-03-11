0ec01ca6916211a0635b537131b3d2daf414e796
renatogsouza <renatomacedo1995@gmail.com>
2021-03-11 19:16:35 -0300

    requisito 8 completo

--- script.js
+++ script.js
@@ -1,26 +1,28 @@
 let textInput = document.getElementById('texto-tarefa');
 let buton = document.getElementById('criar-tarefa');
 let lista = document.getElementById('lista-tarefas');
-let clickList = document.querySelectorAll('ol');
-let listaGeral = document.querySelectorAll('ol li');
+let clickList = document.getElementsByClassName('liStyle');
 
+
+// Vai adicionando item a lista a cada click
 buton.addEventListener('click', function(){  
   let novaLista = document.createElement('li');
+  novaLista.classList.add('liStyle');
   lista.appendChild(novaLista).innerText = textInput.value;
   textInput.value = '';  
+  mudarCor();
 });
 
-for (let i = 0; i < clickList.length; i++) {
-clickList[i].addEventListener('click', function() {
-  let classColor = document.getElementsByClassName('color')[0];
-  event.target.classList.add('color');
-  classColor.classList.remove('color');
-  // for (let i = 0; i < lista.length; i++){
-  //   if (listaGeral.className === 'color'){
-  // //   event.target.classList.remove('color');
-  // } else {
-  event.target.classList.add('color');
-  // }
-  // }
-});
+//
+function mudarCor() {
+  for (let i = 0; i <  clickList.length; i++) {
+    clickList[i].addEventListener('click', function() {
+      for (let j = 0; j < clickList.length + 1; j++) {
+        if(clickList[j]){
+          clickList[j].classList.remove('color');
+        }
+        event.target.classList.add('color');   
+      }
+      });
+  }
 }
