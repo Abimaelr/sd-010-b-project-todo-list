ddbaa63a4ff158ccb37342413de73f40bf7ad4fe
Flavio Andrade <foa12381@gmail.com>
2021-03-11 03:56:32 -0300

    teste6

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let text = document.querySelector('#texto-tarefa'); //local digitar tarefa
+const text = document.querySelector('#texto-tarefa'); //local digitar tarefa
 const addButton = document.querySelector('#criar-tarefa'); //btn add tarefa na minha lista
 const listOl = document.querySelector('#lista-tarefas'); //ol para criação da minha li
 const buttonClean = document.querySelector('#apaga-tudo'); // btn apaga tudo
@@ -8,15 +8,15 @@ const btnMoveUp = document.querySelector('#mover-cima');
 const btnMoveDown = document.querySelector("#mover-baixo");
 const btnSave = document.querySelector("#salvar-tarefas");
 
-const  creatTask = () => {
- addButton.addEventListener('click',() => {
- let createElementLi = document.createElement('li');
- addTaskStyle(createElementLi);
+  const  creatTask = () => {
+  addButton.addEventListener('click',() => {
+  let createElementLi = document.createElement('li');
+    addTaskStyle(createElementLi);
 
- listOl.appendChild(createElementLi);
- text.value = "";
+    listOl.appendChild(createElementLi);
+    text.value = "";
 
- })
+  })
 }
 const addTaskStyle = (createElementLi) => {
   createElementLi.innerText = text.value;
@@ -24,13 +24,13 @@ const addTaskStyle = (createElementLi) => {
 }
 
 
-const addSelectedTask =() => {
-listOl.addEventListener('click', (event) => {
-let selectedTask = document.querySelector('.selected');
-  if(selectedTask && event.target.classList.contains('task') ){
-  selectedTask.classList.remove('selected');
-  }
-  if(event.target.classList.contains('task')){
+const addSelectedTask = () => {
+  listOl.addEventListener('click', (event) => {
+    let selectedTask = document.querySelector('.selected');
+    if (selectedTask && event.target.classList.contains('task') ){
+      selectedTask.classList.remove('selected');
+    }
+  if (event.target.classList.contains('task')){
     event.target.classList.add('selected');
   }
 })
@@ -38,19 +38,18 @@ let selectedTask = document.querySelector('.selected');
 
 const addCompletedTask = ( ) => {
   listOl.addEventListener('dblclick', (event) => {
-  if(event.target.classList.contains('completed') ) {
-    event.target.classList.remove('completed')
-  }
-  else {
+    if (event.target.classList.contains('completed') ) {
+      event.target.classList.remove('completed')
+    }
+    else {
     event.target.classList.add('completed');
-
   }
   })
 }
 
 const buttonCleanList = () => {
- buttonClean.addEventListener('click', function(){
- listOl.innerHTML = '';
+  buttonClean.addEventListener('click', () => {
+  listOl.innerHTML = '';
  })
 
 }
@@ -76,7 +75,7 @@ const buttonClearSelected =() => {
 }
 
 const moveTaskUp = () => {
-btnMoveUp.addEventListener('click',function(){
+btnMoveUp.addEventListener('click',() => {
 let taskList = document.querySelectorAll('li.task');
   for(let index = 0; index < taskList.length; index +=1){
     let position = taskList[index];
