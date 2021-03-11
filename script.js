f1f476d7af9fac67dee3e52c850be875b96f161e
Thiago Felipe <thiagoribeiromixtv@gmail.com>
2021-03-10 23:05:17 -0300

    Exercicio 7

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
 let textoInput = document.getElementById('texto-tarefa');
 let botao = document.getElementById('criar-tarefa');
 let lista = document.getElementById('lista-tarefas');
-
+let corLista = document.querySelectorAll('ol');
 
 botao.addEventListener('click', function () {
-    let novaLista = document.createElement('li');
-    lista.appendChild(novaLista).innerText = textoInput.value;
-    textoInput.value = '';
+  let novaLista = document.createElement('li');
+  novaLista.className = 'lista';
+  lista.appendChild(novaLista).innerText = textoInput.value;
+  textoInput.value = '';
 })
 
+for (let index = 0; index < corLista.length; index += 1) {
+  corLista[index].addEventListener('click', function () {
+    event.target.style.color = 'rgb(128, 128, 128)';
+  })
+}
