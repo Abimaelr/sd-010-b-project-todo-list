de83e2ec8ba18f298f755dc61d4a161affb3b9cc
Cristian <cristian.bugs@gmail.com>
2021-03-11 19:55:19 -0300

    fifth and sixth task

--- script.js
+++ script.js
@@ -1,17 +1,14 @@
 const things = document.getElementById('lista-tarefas');
 const content = document.getElementById('texto-tarefa');
-const buttn = document.getElementById('criar-tarefa');
+const button = document.getElementById('criar-tarefa');
 
-buttn.addEventListener('click', recebeClick)
+button.addEventListener('click', recebeClick)
 
 function recebeClick () {
    
-   let task = content.value;
    const item = document.createElement('li');
    things.appendChild(item);
-   item.innerText = task;
+   item.innerText = content.value;
    content.value = '';
-
-
 };
 
