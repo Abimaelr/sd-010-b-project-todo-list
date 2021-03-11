07299fb65186360334ff65c33f986b9f45219773
Paulo Henrique Lima <limapaulobsb@gmail.com>
2021-03-10 22:32:17 -0300

    Requisitos 10 e 11

--- script.js
+++ script.js
@@ -1,12 +1,3 @@
-document.getElementById('criar-tarefa').onclick = function () {
-  let listaTarefas = document.querySelector('#lista-tarefas');
-  let tarefa = document.createElement('li');
-  tarefa.innerText = document.querySelector('#texto-tarefa').value;
-  tarefa.className = 'tarefa';
-  listaTarefas.appendChild(tarefa);
-  document.querySelector('#texto-tarefa').value = '';
-};
-
 document.addEventListener('click', (event) => {
   if (event.target.classList.contains('tarefa')) {
     let tarefas = document.querySelectorAll('.tarefa');
@@ -26,3 +17,28 @@ document.addEventListener('dblclick', (event) => {
     }
   }
 });
+
+document.getElementById('criar-tarefa').onclick = function () {
+  let listaTarefas = document.querySelector('#lista-tarefas');
+  let tarefa = document.createElement('li');
+  tarefa.innerText = document.querySelector('#texto-tarefa').value;
+  tarefa.className = 'tarefa';
+  listaTarefas.appendChild(tarefa);
+  document.querySelector('#texto-tarefa').value = null;
+};
+
+document.getElementById('remover-finalizados').onclick = function () {
+  let tarefas = document.querySelectorAll('.tarefa');
+  for (let i = 0; i < tarefas.length; i += 1) {
+    if (tarefas[i].classList.contains('completed')) {
+      document.querySelector('#lista-tarefas').removeChild(tarefas[i]);
+    }
+  }
+};
+
+document.getElementById('apaga-tudo').onclick = function () {
+  let listaTarefas = document.querySelector('#lista-tarefas');
+  while (listaTarefas.hasChildNodes()) {
+    listaTarefas.removeChild(listaTarefas.firstChild);
+  }
+};
