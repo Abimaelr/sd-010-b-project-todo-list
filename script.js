91acf807828ff21c2e5498c44c36e4bd23530b8c
Angelo Bittencourt <angelogpbl.info@gmail.com>
2021-03-10 21:06:13 -0300

    item da lista so seleciona um

--- script.js
+++ script.js
@@ -49,24 +49,27 @@ function criarTarefa() {
 }
 
 // função para pintar o background
-// function pintarLinha() {
-//   for (let index = 0; index < getItem.length; index += 1) {
-//     if (getItem[index].id === 'selected') {
-//       getItem[index].style.backgroundColor = 'rgb(128, 128, 128)';
-//     }
-//   }
-// }
-
-// função para selecionar com 1 click o item a ser pintado
-function selecionaLinha(event) {
-  const selector = document.querySelector('#selected');
-  if (selector) {
-    selector.removeAttribute = ('id');
-    event.target.id = 'selected';
-  } else {
-    event.target.id = 'selected';
+function pintarLinha() {
+  for (let index = 0; index < getItem.length; index += 1) {
+    if (getItem[index].className === 'selected') {
+      getItem[index].style.backgroundColor = 'rgb(128, 128, 128)';
+    } else {
+      getItem[index].style.backgroundColor = '';
+    }
   }
-//  pintarLinha();
+}
+
+// função para selecionar um item
+function selecionaLinha(event) {
+  const unit = event.target;
+  const seletor = document.querySelector('.selected');
+  if (seletor) {
+    seletor.classList.remove('selected');
+    unit.classList.add('selected');
+  } else {
+    unit.classList.add('selected');
+  }
+  pintarLinha();
 }
 
 // riscando
@@ -78,7 +81,7 @@ function riscar() {
   }
 }
 
-// função para riscar
+// função para riscar um item
 function itemCompleto(evento) {
   const unid = evento.target;
   if (unid.className === 'completed') {
