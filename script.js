4763eb6a84611c6fc2901a86ae56ca47e62501a2
EderPaiva <edergomesdepaiva12@gmail.com>
2021-03-11 11:56:59 -0300

    botao esta criado falta armazenar o input e colocar na lista

--- script.js
+++ script.js
@@ -1,4 +1,19 @@
 window.onload = function(){
 
-    function createLi()
+    function createLi(){
+
+    let ol = document.getElementById('lista-tarefas');
+    let li = document.createElement('li');
+    ol.appendChild(li);
+
+    }
+
+    let buttonInput = document.getElementById("criar-tarefa");
+    buttonInput.addEventListener('click',function(){
+        //criando tarefas
+        createLi();
+    })
+
 }
+          
+
