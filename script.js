2ba0ce06d5294fca6c13f5569dded865e8bb481d
Camila <camilafabiabh9@gmail.com>
2021-03-11 20:33:28 -0300

    Adicionando função de colorir o funda da função selecionada

--- script.js
+++ script.js
@@ -14,4 +14,22 @@ function listaItem() {
   });
 }
 listaItem()
-//function 
+/*Clicar em um item da lista deve alterar a cor de fundo do item para cinza rgb(128,128,128)](#7---clicar-em-um-item-da-lista-deve-alterar-a-cor-de-fundo-do-item-para-cinza-rgb128128128)
+Não deve ser possível selecionar mais de um elemento da lista ao mesmo tempo](#8---não-deve-ser-possível-selecionar-mais-de-um-elemento-da-lista-ao-mesmo-tempo)
+Clicar duas vezes em um item, faz com que ele seja riscado, indicando que foi completo. Deve ser possível desfazer essa ação clicando novamente duas vezes no item]
+*/
+function backgroundCinza() {
+  let lista = document.getElementById('lista-tarefas');
+  lista.addEventListener('click', function(event){
+    let colorCinza = document.querySelector(".cinza");
+    if(colorCinza){
+      colorCinza.classList.remove('cinza');   
+    } 
+      event.target.classList.add('cinza');
+  })
+}
+backgroundCinza()
+
+
+
+
