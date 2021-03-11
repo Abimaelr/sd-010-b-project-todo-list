d7ffff761d1c1e40861ba2fc7c66a181008cd112
EderPaiva <edergomesdepaiva12@gmail.com>
2021-03-11 15:58:00 -0300

    com ajuda me ajudou refatorar o codigo habilitar a parte 5

--- script.js
+++ script.js
@@ -2,17 +2,33 @@ window.onload = function(){
 
     function createLi(){
 
-    let ol = document.getElementById('lista-tarefas');
-    let li = document.createElement('li');
-    ol.appendChild(li);
+        let ol = document.getElementById('lista-tarefas');
+        let li = document.createElement('li');
+        ol.appendChild(li); 
 
+        //esta parte do codigo precisa ser melhorada
+        let textoTarefa = document.getElementById('texto-tarefa');      
+        let textInput = textoTarefa.value;
+        textoTarefa.value = "";
+        
+    // essa parte do codigo está certa        
+            li.innerText = textInput;
+            
+            
     }
 
     let buttonInput = document.getElementById("criar-tarefa");
     buttonInput.addEventListener('click',function(){
-        //criando tarefas
         createLi();
+        
     })
+
+    
+    
+
+    
+
+    
 
 }
           
