ab302b0ca0b6d04d7a627a2444027c0669c7c9af
Camila <camilafabiabh9@gmail.com>
2021-03-11 23:01:58 -0300

    adicionando stylo

--- script.js
+++ script.js
@@ -14,50 +14,53 @@ function listaItem() {
   });
 }
 listaItem();
+
 function backgroundCinza() {
   let lista = document.getElementById('lista-tarefas');
-  lista.addEventListener('click', function(event){
+  lista.addEventListener('click', function (event) {
     let colorCinza = document.querySelector(".cinza");
-    if (colorCinza){
-      colorCinza.classList.remove('cinza');   
-    } 
-      event.target.classList.add('cinza');
+    if (colorCinza) {
+      colorCinza.classList.remove('cinza');
+    }
+    event.target.classList.add('cinza');
   })
 }
 backgroundCinza();
 // botao para click duas vezes :https://developer.mozilla.org/en-US/docs/Web/API/Element/dblclick_event
 function backgroundRiscado() {
   let lista = document.getElementById('lista-tarefas')
-  lista.addEventListener('dblclick', function(event){
+  lista.addEventListener('dblclick', function (event) {
     let riscado = document.querySelector('.completed');
     if (riscado) {
       riscado.classList.remove('completed');
-    } 
-      event.target.classList.add('completed');
+    }
+    event.target.classList.add('completed');
   })
 }
 backgroundRiscado();
-function bottonApagaLista () {
+
+function bottonApagaLista() {
   const botton = document.getElementById('apaga-tudo')
   let lista = document.getElementById('lista-tarefas');
-  botton.addEventListener('click', function(){
-    while (lista.firstChild){  
+  botton.addEventListener('click', function () {
+    while (lista.firstChild) {
       // A declaração while cria um laço que executa uma rotina especifica enquanto a condição de teste for avaliada como verdadeira
       // https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Statements/while
-    lista.removeChild(lista.firstChild);
+      lista.removeChild(lista.firstChild);
     }
   });
 }
 bottonApagaLista();
-function apagaTudo () {
+
+function apagaTudo() {
   let botton = document.getElementById('remover-finalizados');
   botton.addEventListener('click', buttonFinal);
-} 
+}
 apagaTudo();
+
 function buttonFinal() {
   let bottonCompleted = document.querySelector('.completed');
   while (bottonCompleted.parentNode) {
     bottonCompleted.parentNode.removeChild(bottonCompleted);
   }
 }
-
