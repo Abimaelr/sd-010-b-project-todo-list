351d98e4618f14a105e645faf055ee0753f6035a
Thiago Felipe <thiagoribeiromixtv@gmail.com>
2021-03-11 23:53:46 -0300

    Exercicio 9

--- script.js
+++ script.js
@@ -1,21 +1,34 @@
 let textoInput = document.getElementById('texto-tarefa');
 let botao = document.getElementById('criar-tarefa');
 let lista = document.getElementById('lista-tarefas');
-let corLista = document.querySelector('ol');
+let paiLista = document.querySelector('ol');
 
-botao.addEventListener('click', function () {
-  let novaLista = document.createElement('li');
-  lista.appendChild(novaLista).innerText = textoInput.value;
-  textoInput.value = '';
-})
+function criarListas() {
+  botao.addEventListener('click', function () {
+    let novaLista = document.createElement('li');
+    lista.appendChild(novaLista).innerText = textoInput.value;
+    textoInput.value = '';
+  })
+}
 
-corLista.addEventListener('click', function (event) {
-  let classSelected = document.querySelector('.selected');
-  if (classSelected != null) {
-    document.querySelector('.selected').classList.remove('selected');
-  }
-  event.target.classList.add('selected');
-});
+criarListas();
 
+function mudarCorSelected() {
+  paiLista.addEventListener('click', function (event) {
+    let classSelected = document.querySelector('.selected');
+    if (classSelected != null) {
+      document.querySelector('.selected').classList.remove('selected');
+    }
+    event.target.classList.add('selected');
+  });
+}
 
+mudarCorSelected();
 
+function ricarLinha() {
+  paiLista.addEventListener('dblclick', function (event) {
+    event.target.classList.toggle('completed');
+  });
+}
+
+ricarLinha();
