45ad1c085d3248cbe76207cfb258ae3988ba43c6
Lucas Rodrigues <lucasrpontesgarcia@gmail.com>
2021-03-11 22:36:02 -0300

    exercicio 9

--- script.js
+++ script.js
@@ -12,8 +12,9 @@ button.addEventListener('click', function() {
     linha.innerText = input.value;
     input.value = '';
     linhaLista();
+    riscarEDesriscarALinha();
 })
-//colorir a linha que clicar
+//colorir a linha que clicar e apagar as outras linhas que estao coloridas
 function mudaCor(event) {
   event.target.classList.add('corDeFundo');
 }
@@ -21,7 +22,7 @@ function mudaCor(event) {
 function removeCor() {
   let linhasLista = document.querySelectorAll('li'); 
   for (let j = 0; j < linhasLista.length; j += 1) {
-    if (linhasLista[j].className = 'corDeFundo'){
+    if (linhasLista[j].classList.contains('corDeFundo')){
       linhasLista[j].classList.remove('corDeFundo')
     }
   }
@@ -31,6 +32,18 @@ function linhaLista() {
   for (let i = 0; i < clickList.length; i += 1) {
     clickList[i].addEventListener('click', removeCor);
     clickList[i].addEventListener('click', mudaCor);
-    
+  }
+}
+
+
+//riscar e desriscar a linha
+function riscaALinha(event) {
+  event.target.classList.toggle('completed');
+}
+
+let todasAsLinhas = document.getElementsByTagName('li');
+function riscarEDesriscarALinha() {
+  for (let index = 0; index < todasAsLinhas.length; index += 1) {
+    todasAsLinhas[index].addEventListener('dblclick', riscaALinha);
   }
 }
