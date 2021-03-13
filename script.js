7284f48d134f55a4fd15021902e3ab0c5f27d1f7
Arlen Freitas <arlenfreitas1@gmail.com>
2021-03-13 11:58:03 -0300

    complete item

--- script.js
+++ script.js
@@ -1,27 +1,24 @@
 window.onload = function () {
   const creatingTasks = document.querySelector('#criar-tarefa');
-  const clearList = document.querySelector('#apaga-tudo');
-  const deleteCompletedItem = document.querySelector('#remover-finalizados');
-  const saveList = document.querySelector('#salvar-tarefas');
-  const removeItemButton = document.querySelector('#remover-selecionado');
-
-  creatingTasks.addEventListener('click', addingTasks);
-  clearList.addEventListener('click', deletingList);
-  deleteCompletedItem.addEventListener('click', deletingCompletedItem);
-  saveList.addEventListener('click', savingList);
-  removeItemButton.addEventListener('click', removingSelectedItem);
-
-  retrievingList();
+  creatingTasks.addEventListener('click', addTask);
 };
 
-const textEntry = document.querySelector('#texto-tarefa');
+const inputText = document.querySelector('#texto-tarefa');
 const list = document.querySelector('#lista-tarefas');
-function addingTasks() {
+function addTask() {
   const item = document.createElement('li');
   item.className = 'item';
-  item.innerText = textEntry.value;
+  item.innerText = inputText.value;
   list.appendChild(item);
-  textEntry.value = '';
+  inputText.value = '';
   item.addEventListener('click', selectingItem);
   item.addEventListener('dblclick', itemComplete);
+}
+
+function itemComplete(event) {
+  if (event.className === 'completed') {
+    event.target.classList.remove('completed');
+  } else {
+    event.target.classList.toggle('completed');
+  }
 }
