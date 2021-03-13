672c40d527d31b5acec4fc78413299f74d78d810
Aladino Borges <eualadino@gmail.com>
2021-03-13 18:07:43 -0300

    Requisito 7. Ao clicar num item da lista a sua cor de furndo e alterada.

--- script.js
+++ script.js
@@ -7,18 +7,33 @@ function generateTask() {
   const value = dataInserted.value;
   const listElements = document.createElement('li');
   listElements.id = 'item';
+  // Criar o EventListener durante a criação dinámica do elemento aprendida no link 'https://ultimatecourses.com/blog/attaching-event-handlers-to-dynamically-created-javascript-elements':
+  listElements.addEventListener('click', changeBackground);
+  listElements.style.backgroundColor = 'white';
   listElements.innerText = value;
 
-  //function que verifica se foi introduzido algo no input:
+  // function que verifica se foi introduzido algo no input:
   function verifiesInput() {
     if (value !== '') {
       listFather.appendChild(listElements);
+      dataInserted.focus();
     } else {
       alert('ERRO: CAMPO DE PREENCHIMENTO VAZIO!');
     }
     dataInserted.value = '';
   }
-  verifiesInput()
+  verifiesInput();
+}
+
+// 7. Mudar a cor de fundo do item ao clicar nele:
+function changeBackground(event) {
+  const element = event.target;
+
+  if (element.style.backgroundColor === 'white') {
+    element.style.backgroundColor = 'rgb(128, 128, 128)';
+  } else {
+    element.style.backgroundColor = 'white';
+  }
 }
 
 button.addEventListener('click', generateTask);
