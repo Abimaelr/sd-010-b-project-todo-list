12e2a12e5f425deae749e16e64ad5829cab6e007
brunobastosfer <brunobastos@edu.unifor.br>
2021-03-13 17:21:48 -0300

    adicionando a resolução da oitava questão

--- script.js
+++ script.js
@@ -1,18 +1,20 @@
 function getList() {
-    let getButton = document.querySelector('#criar-tarefa')
-    let getOl = document.querySelector('#lista-tarefas')
-    let getInput = document.querySelector('#texto-tarefa')
+    let getButton = document.querySelector('#criar-tarefa');
+    let getOl = document.querySelector('#lista-tarefas');
+    let getInput = document.querySelector('#texto-tarefa');
+    let changeClass = document.querySelector('ol');
         getButton.addEventListener('click', function() {
             let creationLi = document.createElement('li')
             getOl.appendChild(creationLi).innerHTML = getInput.value
             getInput.value = '';
     })
-    let getLi = document.querySelectorAll('ol')
-    for(let index = 0; index < getLi.length; index+=1){
-        getLi[index].addEventListener('click', function(evt) {
-            evt.target.style.color='yellow';
-            evt.target.style.backgroundColor='rgb(128, 128, 128)';
-        })
-    }
+    changeClass.addEventListener('click', function(evt) {
+        let selected = document.querySelector('.selected')
+        if(selected != null){
+            document.querySelector('.selected').classList.remove('selected');
+        } else {
+            evt.target.classList.add('selected')
+        }
+    })
 }
 getList()
