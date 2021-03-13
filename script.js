bf2d9e6e77b6f452b7e30067c4b847d08f1be5b4
Jander Liborio <jandernunes14@gmail.com>
2021-03-12 22:31:30 -0400

    so um elemento pode ter cor cinza

--- script.js
+++ script.js
@@ -5,7 +5,6 @@ function adicionar() {
     let listTexto = inputList.value;
     let tarefaItem = document.createElement('li');
     let listaTarefas = document.querySelector('#lista-tarefas');
-    tarefaItem.className = 'selected';
     tarefaItem.innerHTML = listTexto;
     listaTarefas.appendChild(tarefaItem);
     inputList.value = '';
@@ -17,7 +16,13 @@ function trocarCinza() {
   let listaTamanho = lista.children;
   lista.addEventListener('click', event => {
     for (let index = 0; index < listaTamanho.length; index += 1){
-      event.target.style.backgroundColor = 'rgb(128,128,128)';
+      for (let procurar = 0; procurar < listaTamanho.length; procurar += 1){
+        let tem = listaTamanho[procurar].classList.contains('cor-cinza');
+        if (tem) {
+          listaTamanho[procurar].classList.remove('cor-cinza');
+        }
+      };
+      event.target.className = 'cor-cinza';
     }
   });
 }
