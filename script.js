91b4aef267f38504cdd473576ea5b3bead6df43d
Jander Liborio <jandernunes14@gmail.com>
2021-03-12 21:47:11 -0400

    setimo requisito

--- script.js
+++ script.js
@@ -5,12 +5,23 @@ function adicionar() {
     let listTexto = inputList.value;
     let tarefaItem = document.createElement('li');
     let listaTarefas = document.querySelector('#lista-tarefas');
+    tarefaItem.className = 'selected';
     tarefaItem.innerHTML = listTexto;
     listaTarefas.appendChild(tarefaItem);
     inputList.value = '';
     inputList.focus();
   });
 }
-window.onload = function(){
+function trocarCinza() {
+  let lista = document.querySelector('#lista-tarefas');
+  let listaTamanho = lista.children;
+  lista.addEventListener('click', event => {
+    for (let index = 0; index < listaTamanho.length; index += 1){
+      event.target.style.backgroundColor = 'rgb(128,128,128)';
+    }
+  });
+}
+window.onload = function() {
   adicionar();
+  trocarCinza();
 };
