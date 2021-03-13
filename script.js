bc6e311311b4d09dd3cb8763647c915aba3f8ab5
Thiago Leite <leite.tjs@gmail.com>
2021-03-12 23:19:47 -0300

    add if to createListItem

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
-// requesito 5
+// requesito 5 e 6
 
-function createListItem () {  
-  const makeList = document.querySelector('#lista-tarefas');
-  const listItem = document.createElement('li');
+function createListItem () {
   const taskName = document.querySelector('#texto-tarefa');
-  listItem.innerText = taskName.value;
-  makeList.appendChild(listItem);
-  taskName.value = '';
-    
-}
+  if (taskName.value !== '') {
+    const makeList = document.querySelector('#lista-tarefas');
+    const listItem = document.createElement('li');    
+    listItem.innerText = taskName.value;
+    makeList.appendChild(listItem);
+    taskName.value = ''; 
+  }
+};
 
 const btnList = document.querySelector('#criar-tarefa');
 btnList.addEventListener('click', createListItem);
-
