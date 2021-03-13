a3332fd335247f623f574d985f52231e81480a62
Letycia Barreto <letyciasbarreto@gmail.com>
2021-03-13 10:00:37 -0300

    requisito 5 e 6

--- script.js
+++ script.js
@@ -1,17 +1,34 @@
 const body = document.body;
 const header = document.createElement('header');
-header.textContent = 'Minha Lista de Tarefas'
-body.appendChild(header)
+header.textContent = 'Minha Lista de Tarefas';
+body.appendChild(header);
 
 const paragraph = document.createElement('p');
-paragraph.id = 'funcionamento'
-paragraph.textContent = 'Clique duas vezes em um item para marcá-lo como completo.'
-body.appendChild(paragraph)
+paragraph.id = 'funcionamento';
+paragraph.textContent = 'Clique duas vezes em um item para marcá-lo como completo.';
+body.appendChild(paragraph);
 
 const input = document.createElement('input');
-input.id = 'texto-tarefa'
-body.appendChild(input)
+input.id = 'texto-tarefa';
+body.appendChild(input);
+
+const button = document.createElement('button');
+button.id = 'criar-tarefa';
+button.textContent = 'criar tarefa'
+body.appendChild(button);
 
 const listaTarefas = document.createElement('ol');
-listaTarefas.id = 'lista-tarefas'
-body.appendChild(listaTarefas)
+listaTarefas.id = 'lista-tarefas';
+body.appendChild(listaTarefas);
+ 
+const textoInput = document.getElementById('texto-tarefa');
+const btn = document.getElementById('criar-tarefa');
+const list = document.getElementById('lista-tarefas');
+
+btn.addEventListener('click', adcTarefa);
+function adcTarefa() {
+    const li = document.createElement('li');
+    li.innerText = textoInput.value;
+    list.appendChild(li);
+    textoInput.value ='';
+}
