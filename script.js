df1961f7f5ca5ec0e5810bc5fd103cba23c9c8f5
Italo Matos <italomatos764@gmail.com>
2021-03-12 23:17:04 -0400

    Funcao double click

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ function corBackground() {
   let listaTarefas = document.querySelector('#lista-tarefas');
   let items = listaTarefas.children;
   listaTarefas.addEventListener('click', function(evento) {
-    for(let index = 0; index < items.length; index++) {
+    for(let index = 0; index < items.length; index += 1) {
       removerSelected();
       evento.target.classList.add('selected');
     }
@@ -28,9 +28,25 @@ corBackground();
 
 function removerSelected() {
   let items = document.querySelector('#lista-tarefas').children;
-  for(let index = 0; index < items.length; index++) {
+  for(let index = 0; index < items.length; index += 1) {
     if(items[index].classList.contains('selected')) {
       items[index].classList.remove('selected');
     }
   }
 }
+
+function doubleClick() {
+  let listaTarefas = document.querySelector('#lista-tarefas');
+  let items = listaTarefas.children;
+  listaTarefas.addEventListener('dblclick', function(event) {
+    for(let index = 0; index < items.length; index += 1) {
+      if(event.target.classList.contains('completed')) {
+        event.target.classList.remove('completed');
+      }
+      else {
+        event.target.classList.add('completed');
+      }
+    }
+  })
+}
+doubleClick();
