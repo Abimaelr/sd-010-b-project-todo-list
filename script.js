027f69d5830f3157c6ed46989e32f9f68d1068e2
brunobastosfer <brunobastos@edu.unifor.br>
2021-03-14 15:38:40 -0300

    adicionando a resolução da nona questão

--- script.js
+++ script.js
@@ -2,21 +2,35 @@ function getList() {
     let getButton = document.querySelector('#criar-tarefa');
     let getOl = document.querySelector('#lista-tarefas');
     let getInput = document.querySelector('#texto-tarefa');
-    let changeClass = document.querySelector('ol');
+
         getButton.addEventListener('click', function() {
             let creationLi = document.createElement('li')
             getOl.appendChild(creationLi).innerHTML = getInput.value
             getInput.value = '';
     })
-    changeClass.addEventListener('click', function(evt) {
-        let selected = document.querySelector('.selected')
-        // Fiz uma pesquisa sobre a utilização das propriedades do ClassList no:
-        // https://www.w3schools.com/jsref/prop_element_classlist.asp
-        if(selected != null){
-            document.querySelector('.selected').classList.remove('selected');
-        } else {
-            evt.target.classList.add('selected')
-        }
-    })
 }
 getList()
+
+function changeColor() { 
+let changeClass = document.querySelector('ol');
+changeClass.addEventListener('click', function(evt) {
+    let selected = document.querySelector('.selected')
+    // Fiz uma pesquisa sobre a utilização das propriedades do ClassList no:
+    // https://www.w3schools.com/jsref/prop_element_classlist.asp
+    if(selected != null){
+        document.querySelector('.selected').classList.remove('selected');
+    } else {
+        evt.target.classList.add('selected')
+    }
+})
+}
+changeColor()
+
+function completed() {
+    let getLi =  document.querySelector('#lista-tarefas')
+    getLi.addEventListener('dblclick', function(evt){
+        evt.target.classList.toggle('completed')
+    })
+}
+completed()
+
