73af5d289bfcc1ead7ba869ab2494813dbd523f2
Dieghonm <dieghonm@gmail.com>
2021-03-14 01:46:06 -0300

    Quesito 9 tentativa 4

--- script.js
+++ script.js
@@ -1,44 +1,41 @@
 let tamanhoDaLista = []
 const paiDaLista = document.getElementById('lista-tarefas');
 const botão = document.getElementById('criar-tarefa');
-
 botão.addEventListener('click', vaiPraLista); 
-
+const botãoApaga = document.getElementById('apaga-tudo');
 // **************Adciona o texto na lista****************
 function vaiPraLista() { //função do botão vaiPraLista
-  let entradaDeTarefa = document.getElementById('texto-tarefa').value
+  let entradaDeTarefa = document.getElementById('texto-tarefa').value;
   const li = document.createElement('li');
   li.innerText = entradaDeTarefa;
   entradaDeTarefa.value = '';
-  li.addEventListener('click',  seleciona)
+  li.addEventListener('click',  seleciona);
   // ***********risca o texto ao clicar duas vezes*********
   li.addEventListener('dblclick', function(event){
-    console.log('ola')
+    console.log('ola');
     event.target.classList.toggle('completed');
   })
   paiDaLista.appendChild(li);
-}
-// ***********seleciona o texto ao clicar************
+}// ***********seleciona o texto ao clicar************
 function seleciona() {
-  let apagaACor = document.querySelector('.grey')
+  let apagaACor = document.querySelector('.grey');
   if(apagaACor !== null){
     apagaACor.classList.remove('grey');
   }
   this.classList.add('grey');
-};
-// ***********Apaga tudo****************************
-const botãoApaga = document.getElementById('apaga-tudo');
+};// ***********Apaga tudo***************************
 botãoApaga.addEventListener('click', function(){
-  let list = document.getElementById("lista-tarefas")
+  let list = document.getElementById("lista-tarefas");
   for(cont = 0; 0 < (list.childElementCount); cont += 1){
   list.removeChild(list.childNodes[0]);
   }
-})
-// ***********Apaga Riscados****************************
-const botãoApagaRiscados = document.getElementById('remover-finalizados')
+})// ***********Apaga Riscados**********************
+const botãoApagaRiscados = document.getElementById('remover-finalizados');
   botãoApagaRiscados.addEventListener('click', function (){
    const riscados = document.getElementsByClassName('completed');
    for (let index = 0; index < riscados.length; index = 0) {
      riscados[0].parentNode.removeChild(riscados[0]);
    }
- })
+ });// ***************Salva************************
+
+
