8b84289ceb36eee04ea4533d84df6435595434a4
Dieghonm <dieghonm@gmail.com>
2021-03-13 22:21:01 -0300

    Quesito 9 tentativa 2

--- script.js
+++ script.js
@@ -1,47 +1,45 @@
-
-let tamanhoDaLista =[]
-
+let tamanhoDaLista = []
 // **************Armazena o texto****************
-
 let entradaDeTarefa;
-let caixaDeEntrada = document.getElementById('texto-tarefa');
+const caixaDeEntrada = document.getElementById('texto-tarefa');
 caixaDeEntrada.addEventListener('keyup', tarefa);
-
-function tarefa()  {
- entradaDeTarefa = caixaDeEntrada.value;
-}
-// **************Adciona o texto na lista****************
-let paiDaLista = document.getElementById('lista-tarefas');
-let botão = document.getElementById('criar-tarefa');
+function tarefa() {
+  entradaDeTarefa = caixaDeEntrada.value;
+}// **************Adciona o texto na lista****************
+const paiDaLista = document.getElementById('lista-tarefas');
+const botão = document.getElementById('criar-tarefa');
 botão.addEventListener('click', vaiPraLista); 
-
 function vaiPraLista() {
-  let li = document.createElement('li');
+  const li = document.createElement('li');
   li.innerText = entradaDeTarefa;
   caixaDeEntrada.value = '';
   paiDaLista.appendChild(li);
-  tamanhoDaLista=document.querySelectorAll('li')
-  açoes()
-}
-// ***********Pinta de cinza o item selecionado***************
-let teste
-function açoes(){
+  tamanhoDaLista=document.querySelectorAll('li');
+  açoes();
+}// ***********risca o texto ao clicar duas vezes*********
+function risca() {
+  paiDaLista.addEventListener('dblclick', function(event){
+    event.target.classList.toggle('completed');
+  });
+}// ***********Pinta de cinza o item selecionado***************
+function açoes() {
 for (let cont = 0; cont < tamanhoDaLista.length; cont += 1) {
   // ***********seleciona o texto ao clicar************
-  tamanhoDaLista[cont].addEventListener('click',function() {
-      let apagaACor = document.querySelector('.grey')
-      if(apagaACor !== null){
-      apagaACor.classList.remove('grey')
-      }
-      this.classList.add('grey');
+  tamanhoDaLista[cont].addEventListener('click', function() {
+    let apagaACor = document.querySelector('.grey')
+    if(apagaACor !== null){
+      apagaACor.classList.remove('grey');
+    }
+    this.classList.add('grey');
     });
-    risca()
+    risca();
   }
 }
-// ***********risca o texto ao clicar duas vezes*********
-function risca(){
-  paiDaLista.addEventListener('dblclick',function(event){
-    event.target.classList.toggle('completed')
-  })
-}
-
+// ***********Apaga tudo****************************
+const botãoApaga = document.getElementById('apaga-tudo');
+botãoApaga.addEventListener('click', function(){
+  let list = document.getElementById("lista-tarefas")
+  for(cont = 0; 0 < (list.childElementCount); cont += 1){
+  list.removeChild(list.childNodes[0]);
+  }
+})
