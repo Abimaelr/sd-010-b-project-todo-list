c76734233f723bdb3c3b30c351598054ffa85c02
André Arnoni <andre_arnoni@yahoo.com.br>
2021-03-14 12:34:29 -0300

    executado requisito 3

--- script.js
+++ script.js
@@ -4,3 +4,6 @@ document.body.appendChild(firstParagraph)
 firstParagraph.id = 'funcionamento'
 firstParagraph.innerHTML = 'Clique duas vezes em um item para marcá-lo como completo'
 
+let fieldOfListItem = document.createElement('input')
+document.body.appendChild(fieldOfListItem)
+fieldOfListItem.id = 'texto-tarefa'
