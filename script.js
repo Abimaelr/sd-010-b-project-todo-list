0fa48676a2e4ee937aa2be740c967678c994b8b6
André Arnoni <andre_arnoni@yahoo.com.br>
2021-03-14 12:44:59 -0300

    executado requisito 4

--- script.js
+++ script.js
@@ -4,6 +4,12 @@ document.body.appendChild(firstParagraph)
 firstParagraph.id = 'funcionamento'
 firstParagraph.innerHTML = 'Clique duas vezes em um item para marcá-lo como completo'
 
+// inserindo o campo para preenchimento
 let fieldOfListItem = document.createElement('input')
 document.body.appendChild(fieldOfListItem)
 fieldOfListItem.id = 'texto-tarefa'
+
+// inserindo uma lista ordenada
+let toDoList = document.createElement('ol')
+document.body.appendChild(toDoList)
+toDoList.id = 'lista-tarefas'
