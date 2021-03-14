ddc700618d06cd269521ba7bbd35f261e426004a
André Arnoni <andre_arnoni@yahoo.com.br>
2021-03-14 12:30:48 -0300

    inserindo o primeiro paragrafo

---
+++ script.js
@@ -0,0 +1,6 @@
+// inserindo o primeiro parágrafo 
+let firstParagraph = document.createElement('p')
+document.body.appendChild(firstParagraph)
+firstParagraph.id = 'funcionamento'
+firstParagraph.innerHTML = 'Clique duas vezes em um item para marcá-lo como completo'
+
