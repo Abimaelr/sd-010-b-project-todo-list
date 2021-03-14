240101f8a3422d8e417143a49324008d63659633
Carlos <cemargato@gmail.com>
2021-03-14 15:45:35 -0300

    realizando exercicios 7 e 8

--- script.js
+++ script.js
@@ -1,11 +1,26 @@
-const buttonAdd = document.getElementById('criar-tarefa')
-buttonAdd.addEventListener('click', function(){
-  event.preventDefault()
-  let listaTarefas = document.getElementById('lista-tarefas')
-  let tarefas = document.querySelector('#tarefas')
+const buttonAdd = document.getElementById('criar-tarefa');
+let listaTarefas = document.getElementById('lista-tarefas');
+buttonAdd.addEventListener('click', function () {
+  event.preventDefault();
+  let tarefas = document.querySelector('#tarefas');
   let textUser = tarefas.texto.value;
   let listaLi = document.createElement('li');
-  listaLi.textContent = textUser
+  listaLi.textContent = textUser;
   listaTarefas.appendChild(listaLi);
+  listaLi.className = ('listada');
   tarefas.reset();
-}) 
+ })
+//verifiquei a escrita do Carlos Vieira - t10-B pra entender que a minha variavel listada deveria ser li e dentro da função
+ function choose(clickado){
+   let listada = document.querySelectorAll('li');
+for (index = 0; index <listada.length; index+=1){
+   if (listada[index].classList.contains('listada')) {
+      listada[index].classList.remove('selected');
+    }
+  }
+  clickado.target.classList.add('selected');
+ }
+
+ listaTarefas.addEventListener('click', choose);
+
+
