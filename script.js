3a075b8d5e033ef473eaf03d0358371745db185c
Dieghonm <dieghonm@gmail.com>
2021-03-14 14:29:44 -0300

    Quesito 10 tentativa 1

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ function vaiPraLista() { //função do botão vaiPraLista
   entradaDeTarefa.value = '';
   li.addEventListener('click',  seleciona);
   // ***********risca o texto ao clicar duas vezes*********
-  li.addEventListener('dblclick', function(event){
+  li.addEventListener('dblclick', function(event) {
     console.log('ola');
     event.target.classList.toggle('completed');
   })
@@ -19,23 +19,32 @@ function vaiPraLista() { //função do botão vaiPraLista
 }// ***********seleciona o texto ao clicar************
 function seleciona() {
   let apagaACor = document.querySelector('.grey');
-  if(apagaACor !== null){
+  if (apagaACor !== null) {
     apagaACor.classList.remove('grey');
   }
   this.classList.add('grey');
 };// ***********Apaga tudo***************************
-botãoApaga.addEventListener('click', function(){
+botãoApaga.addEventListener('click', function() {
   let list = document.getElementById("lista-tarefas");
-  for(cont = 0; 0 < (list.childElementCount); cont += 1){
+  for (cont = 0; 0 < (list.childElementCount); cont += 1) {
   list.removeChild(list.childNodes[0]);
   }
 })// ***********Apaga Riscados**********************
 const botãoApagaRiscados = document.getElementById('remover-finalizados');
-  botãoApagaRiscados.addEventListener('click', function (){
-   const riscados = document.getElementsByClassName('completed');
-   for (let index = 0; index < riscados.length; index = 0) {
-     riscados[0].parentNode.removeChild(riscados[0]);
-   }
- });// ***************Salva************************
-
-
+botãoApagaRiscados.addEventListener('click', function () {
+  const riscados = document.getElementsByClassName('completed');
+  for (let index = 0; index < riscados.length; index = 0) {
+    riscados[0].parentNode.removeChild(riscados[0]);
+  }
+});// ***************Salva************************
+//https://warcontent.com/localstorage-javascript/
+let botãoSalvar = document.getElementById('salvar-tarefas');
+botãoSalvar.addEventListener('click', salvar); 
+function salvar() {
+  let itensASalvar = paiDaLista.innerHTML;
+  localStorage.setItem('Salvo', itensASalvar);
+  //por innerHTMH pois o localStorageso salva texto
+}
+window.onload = function () {
+  paiDaLista.innerHTML = localStorage.getItem ('Salvo');
+}
