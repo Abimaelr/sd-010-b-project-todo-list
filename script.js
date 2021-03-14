42ba686236eed659ae9c0bcdf52289cb5401932f
root <diegoserafim1@gmail.com>
2021-03-14 17:17:15 -0300

    D5 verifica se o input esta vazio e cria o conteudo dentro do li

--- script.js
+++ script.js
@@ -1,8 +1,23 @@
-let textoTarefa = document.querySelector('#texto-tarefa');
-let listaTarefas = document.querySelector('#lista-tarefas');
+let ul = document.getElementById('#lista-tarefas');
+let li;
+let itemId;
+let itens;
 
-textoTarefa.style.backgroundColor = 'yellow';
-textoTarefa.style.border = 'solid 3px rgb[123, 123, 123]';
-textoTarefa.style.padding = '3px';
+function adicionar() {
+  if (document.getElementById('#texto-tarefa').value != '') {
+    item = document.getElementById('#texto-tarefa').value;
+    itemId = ul.childElementCount;
+    li = createItemElemet(itemId);
+  }
+}
+// cria o elemento dentro de li
+function criarItem(itemValue, itemId) {
+  let li = document.createElement('li');
+  li.setAttribute('index', itemId);
+  li.appendChild(document.createTextNode(itemValue));
+  return li;
+}
 
-listaTarefas.childNodes = textoTarefa;
+function remover() {
+
+}
