0b58ce8a904e6611975cd1e56bedff5cbfcfeea2
tonpasso <wellpasso@gmail.com>
2021-03-14 12:29:38 -0300

    Requisitos 07 e 08

--- script.js
+++ script.js
@@ -2,28 +2,24 @@
 const lista = document.getElementById('lista-tarefas');
 const tarefas = document.getElementById('texto-tarefa');
 const addButton = document.getElementById('criar-tarefa')
+const PAINT = 'paintSelected';
 
 addButton.addEventListener('click', function() {
   const newItem = document.createElement('li');
+  newItem.classList.add('itemTask');
   newItem.innerText = tarefas.value;
-  lista.appendChild(newItem);
-  tarefas.value = '';
-})
-
-// function addItem() {
-  
-//   const newItem = document.createElement('li');
-//   newItem.innerText = tarefas.value;
-//   lista.appendChild(newItem); 
-
-//   const checkButton = document.createElement('button');
-//   checkButton.innerHTML = `<i class="fas fa-check"></i>`
-//   newItem.appendChild(checkButton);
-
-//   const deleteButton = document.createElement('button');
-//   deleteButton.innerHTML = `<i class="fas fa-trash"></i>`;
-//   newItem.appendChild(deleteButton);
-// }
-
-// addItem();
-
+  if (newItem.innerText) {
+    lista.appendChild(newItem);
+    tarefas.value = '';
+  } else {
+    alert('Digite uma tarefa');
+  }  
+  newItem.addEventListener('click', function(event) {
+    let itemSelected = event.target;
+    let tasks = document.getElementsByTagName('li');
+    for (index = 0; index < tasks.length; index += 1) {
+      tasks[index].classList.remove(PAINT);
+    }
+    itemSelected.classList.add(PAINT);    
+  })
+});
