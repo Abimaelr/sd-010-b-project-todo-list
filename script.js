30fc0b053fc6a4d4a6e11a5f6650362e73f0d49a
Maiara Borsatti <maiara.borsatti@gmail.com>
2021-03-15 11:50:47 -0300

    requisito 7

--- script.js
+++ script.js
@@ -1,11 +1,29 @@
 let button = document.getElementById('criar-tarefa');
 let iten = document.getElementById('texto-tarefa');
 let list = document.getElementById('lista-tarefas');
-
 function addTask () {
    let task = document.createElement('li');
-   list.appendChild(task).innerText = iten.value;
-   iten.value = "";
+   list.appendChild(task).innerText = iten.value; // primeira ação: adicionar valor
+   iten.value = ""; // segunda ação: deixar o input em branco
    }
+button.addEventListener('click', addTask);
 
-button.addEventListener('click',addTask);
+/* function changeBackgroundColor () {
+    let itenTask = document.getElementById('lista-tarefas').children;
+    for (let index = 0; index < itenTask.length; index++) {
+        itenTask[index].addEventListener('click',function (event) {
+            event.target.classList.add('grey');
+            event.target.classList.remove('grey');
+        });
+    }
+} */
+
+function changeBackgroundColor () {
+    let itenTask = document.getElementById('lista-tarefas');
+        itenTask.addEventListener('click',function (event) {
+        event.target.classList.add('grey');
+    }); 
+}
+
+changeBackgroundColor();
+
