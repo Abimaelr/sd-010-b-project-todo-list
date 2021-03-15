2ab1a0b39fe7bbc796cd35d48860095489e017bf
Cristian <cristian.bugs@gmail.com>
2021-03-15 13:19:11 -0300

    task seven completed and 9 and 10 unfinished

--- script.js
+++ script.js
@@ -9,6 +9,42 @@ function recebeClick () {
    const item = document.createElement('li');
    things.appendChild(item);
    item.innerHTML = content.value;
+   item.className = 'whatToDo';
+   item.addEventListener ('click', function (event){
+      let Col = getComputedStyle(event.target);
+      let or = Col.backgroundColor;
+      event.target.style.backgroundColor = 'rgb(128, 128, 128)';
+           
+   });
    content.value = '';
 };
+
+
+//Exercicio 9
+const done = document.querySelectorAll('.whatToDo');
+
+
+for (let index = 0; index < done.length; index += 1){
+   done.addEventListener ('dbclick', function (){
+      done[index].className = 'completed';
+
+   })
+}
+
+//Exercicio 10
+const clear = document.getElementById('apaga-tudo');// atribui na constante clear o elemento button de apagar tudo
+let rem = document.getElementById('lista-tarefas');// atribui na constante rem a tag ol
+const posted = document.querySelectorAll('.whatToDo'); // atribui na constante posted as li com classe whatToDo
+
+clear.addEventListener('click', Erase)
+
+function Erase () {
+   for(let index = 0; index < posted.length; index += 1) {
+      rem.removeChild(posted[index]);
+   };
+     
+};
+
+//Exercicio 11
+
 
