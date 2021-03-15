ac89d37abfa871742d55c3e26d2b074da2a3a04f
Maiara Borsatti <maiara.borsatti@gmail.com>
2021-03-15 19:25:05 -0300

    alteraçao do codigo - requisito 7 e 8

--- script.js
+++ script.js
@@ -1,32 +1,26 @@
 let button = document.getElementById('criar-tarefa');
 let iten = document.getElementById('texto-tarefa');
 let list = document.getElementById('lista-tarefas');
+
+// Requisito 5 e 6 criar uma lista ordenada de tarefas:
 function addTask () {
    let task = document.createElement('li');
    list.appendChild(task).innerText = iten.value; // primeira ação: adicionar valor
+   // Ajuda de colegas nas salas de estudo:
    iten.value = ""; // segunda ação: deixar o input em branco
    }
 button.addEventListener('click', addTask);
 
-/* function changeBackgroundColor () {
-    let itenTask = document.getElementById('lista-tarefas').children;
-    for (let index = 0; index < itenTask.length; index++) {
-        itenTask[index].addEventListener('click',function (event) {
-            event.target.classList.add('grey');
-            event.target.classList.remove('grey');
-        });
-    }
-} */
-
-function changeBackgroundColor () {
-    let itenTask = document.getElementById('lista-tarefas');
-        itenTask.addEventListener('click',function (event) {
-        event.target.classList.add('grey');
-    }); 
+// Requisito 7 e 8 colocar Background em apenas um item ao clicar uma vez:
+function changeBackgroundColor (event){
+    let selectedColor = document.querySelector('.grey')
+    event.target.classList.add('grey');
+    // Ajuda do Isaac no Plantão:
+    if (selectedColor) selectedColor.classList.remove('grey');
 }
+list.addEventListener('click',changeBackgroundColor);
 
-changeBackgroundColor();
-
+// Requisito 9 sublinhar tarefa ao clicar duas vezes:
 // Entendimento da função toggle em: https://www.w3schools.com/howto/howto_js_toggle_class.asp
 function lineThrough () {
     let itenTask = document.getElementById('lista-tarefas');
@@ -34,6 +28,5 @@ function lineThrough () {
         event.target.classList.toggle('completed');
     }); 
 }
-
 lineThrough();
 
