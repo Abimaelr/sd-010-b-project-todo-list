69d0ec2630616e64c30aac7a5070e72a2ef57f57
Cristian <cristian.bugs@gmail.com>
2021-03-15 14:00:16 -0300

    tasks 9 and 10

--- script.js
+++ script.js
@@ -2,49 +2,35 @@ const things = document.getElementById('lista-tarefas');
 const content = document.getElementById('texto-tarefa');
 const button = document.getElementById('criar-tarefa');
 
-button.addEventListener('click', recebeClick)
-
-function recebeClick () {
-   
-   const item = document.createElement('li');
-   things.appendChild(item);
-   item.innerHTML = content.value;
-   item.className = 'whatToDo';
-   item.addEventListener ('click', function (event){
-      let Col = getComputedStyle(event.target);
-      let or = Col.backgroundColor;
-      event.target.style.backgroundColor = 'rgb(128, 128, 128)';
-           
-   });
-   content.value = '';
-};
-
-
-//Exercicio 9
-const done = document.querySelectorAll('.whatToDo');
-
-
-for (let index = 0; index < done.length; index += 1){
-   done.addEventListener ('dbclick', function (){
-      done[index].className = 'completed';
-
-   })
+function recebeClick() {
+  const item = document.createElement('li');
+  things.appendChild(item);
+  item.innerHTML = content.value;
+  item.className = 'whatToDo';
+  item.addEventListener ('dblclick', function(evento) {
+      evento.target.className = 'completed';
+  })
+  item.addEventListener('click', function (event) {
+    const Col = getComputedStyle(event.target);
+    const or = Col.backgroundColor;
+    event.target.style.backgroundColor = 'rgb(128, 128, 128)';
+  });
+  content.value = '';
 }
+button.addEventListener('click', recebeClick);
 
-//Exercicio 10
+
+
+// Exercicio 10
 const clear = document.getElementById('apaga-tudo');// atribui na constante clear o elemento button de apagar tudo
-let rem = document.getElementById('lista-tarefas');// atribui na constante rem a tag ol
-const posted = document.querySelectorAll('.whatToDo'); // atribui na constante posted as li com classe whatToDo
+const rem = document.getElementById('lista-tarefas');// atribui na constante rem a tag ol
 
-clear.addEventListener('click', Erase)
-
-function Erase () {
-   for(let index = 0; index < posted.length; index += 1) {
-      rem.removeChild(posted[index]);
-   };
-     
-};
-
-//Exercicio 11
-
-
+function erase() {
+ const posted = document.querySelectorAll('.whatToDo');
+   for (let index = 0; index < posted.length; index += 1) {
+   rem.removeChild(posted[index]);
+  }
+}
+clear.addEventListener('click', erase);
+erase();
+// Exercicio 11
