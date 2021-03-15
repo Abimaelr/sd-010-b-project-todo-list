f94fc83696b93a1a594b442812a11aa3c7363366
ViniciusAlvesRocha <vinialvesrocha@gmail.com>
2021-03-15 19:37:32 -0300

    13 - Adicione dois botões, um com id=mover-cima e outro com id=mover-baixo, que permitam mover o item selecionado para cima ou para baixo na lista de tarefas

--- script.js
+++ script.js
@@ -1,88 +1,85 @@
-/* function localStorageTaskList(tasksObject) {
+function localStorageTaskList(tasksObject) {
+  const taskList = document.getElementById('lista-tarefas');
+  let cont = -1;
   for (let key in tasksObject) {
     taskItem = document.createElement('li');
     taskItem.innerText = tasksObject[key][0];
     taskItem.className = tasksObject[key][1];
     taskItem.style.backgroundColor = tasksObject[key][2];
+    taskItem.setAttribute('id', cont += 1);
     taskList.appendChild(taskItem);
-    addEventClickInTask(taskItem);
+    addEventClickAndDobleClickInTask(taskItem);
   }
 }
-if(localStorage.getItem('tasks')) {
+if (localStorage.getItem('tasks')) {
   localStorageTaskList(JSON.parse(localStorage.getItem('tasks')));
 }
 
-
-let buttonCriarTarefa = document.getElementById('criar-tarefa');
-
-const buttonSaveTasks = document.getElementById('salvar-tarefas');
-const buttonMoveTaskUp = document.getElementById('mover-cima');
-const buttonMoveTaskDn = document.getElementById('mover-baixo');
-
-function addEventClickInTask(task) {
-    task.addEventListener('click', changeColorTask)
-    task.addEventListener('dblclick', lineThrough)
-}
- var booLineThrough = true;
-function lineThrough(element) {
-  colorNone();
-  if (booLineThrough) {
-    element.target.className = 'completed';
-    booLineThrough = false;
-  } else {
-    element.target.className = '';
-    booLineThrough = true;
+function setAllTasksWithBackgroundColorWhite() {
+  const tasks = document.getElementsByTagName('li');
+  for (let index = 0; index < tasks.length; index += 1) {
+    tasks[index].style.backgroundColor = '';
   }
-}
-
-function addTaskInList() {
-  let taskText = document.getElementById('texto-tarefa');
-  let task = document.createElement('li');
-  task.innerText = taskText.value;
-  taskList.appendChild(task);
-  taskText.value = "";
-  addEventClickInTask(task);
-}
-
-function colorNone() {
-    let tasks = document.getElementsByTagName('li');
-    for (let task of tasks) {
-        task.style.backgroundColor = '';
-    }
 }
 
 function changeColorTask(element) {
-  colorNone();
-  element.target.style.backgroundColor = 'rgb(128,128,128)';
+  const el = element;
+  setAllTasksWithBackgroundColorWhite();
+  el.target.style.backgroundColor = 'rgb(128,128,128)';
 }
 
-buttonCriarTarefa.addEventListener('click', addTaskInList);
-
-function deleteTasks() {
-  taskList.innerHTML = '';
+function lineThrough(element) {
+  const el = element;
+  if (element.target.className === 'completed') {
+    el.target.className = '';
+  } else {
+    el.target.className = 'completed';
+  }
 }
 
-/* Evento para deletar todas as tarefas 
-buttonDeleteTasks.addEventListener('click', deleteTasks);
+function addEventClickAndDobleClickInTask(task) {
+  task.addEventListener('dblclick', lineThrough);
+  task.addEventListener('click', changeColorTask);
+}
 
-function qtdTasksEnded() {
-  let tasks = document.getElementsByClassName('completed');
-  return tasks.length;
-}*/
-const buttonDeleteTasksEnded = document.getElementById('remover-finalizados');
+let cont = -1;
+function addTaskInList() {
+  const taskList = document.getElementById('lista-tarefas');
+  const taskText = document.getElementById('texto-tarefa');
+  const task = document.createElement('li');
+  task.innerText = taskText.value;
+  task.setAttribute('id', cont += 1);
+  taskList.appendChild(task);
+  taskText.value = '';
+  addEventClickAndDobleClickInTask(task);
+}
+
 function deleteTasksEnded() {
-  let taskList = document.getElementById('lista-tarefas');
-  while(document.getElementsByClassName('completed')[0]) {
+  while (document.getElementsByClassName('completed')[0]) {
     document.getElementsByClassName('completed')[0].remove();
   }
 }
+
+function clearListTasks() {
+  const taskList = document.getElementById('lista-tarefas');
+  taskList.innerHTML = '';
+}
+
+const buttonCriarTarefa = document.getElementById('criar-tarefa');
+buttonCriarTarefa.addEventListener('click', addTaskInList);
+
+const buttonDeleteTasksEnded = document.getElementById('remover-finalizados');
 buttonDeleteTasksEnded.addEventListener('click', deleteTasksEnded);
 
+const buttonClearListTasks = document.getElementById('apaga-tudo');
+buttonClearListTasks.addEventListener('click', clearListTasks);
+
 /* Exercício 12: */
+const buttonSaveTasks = document.getElementById('salvar-tarefas');
 function saveTasks() {
-  let tasks = document.getElementsByTagName('li');
-  let tasksArray = [];
-  let tasksObject = {};
+  const tasks = document.getElementsByTagName('li');
+  const tasksArray = [];
+  const tasksObject = {};
   for (let task of tasks) {
     tasksArray.push([task.innerText, task.className, task.style.backgroundColor]);
   }
@@ -92,56 +89,91 @@ function saveTasks() {
   }
   localStorage.setItem('tasks', JSON.stringify(tasksObject));
 }
-buttonSaveTasks.addEventListener('click', saveTasks)
+buttonSaveTasks.addEventListener('click', saveTasks);
 
-//Adicionando botoes para mover as tarefas para cima ou para baixo
-function showConsole(values) {
-  return console.log(values);
-}
-
-function mountTaskList(tasks) {
-  list = document.getElementById('lista-tarefas');
-  console.log(list);
-  for (let index = 1; index <= tasks.length; index += 1) {
-    let li = document.createElement('li');
-    li.innerText = 'ola';//tasks[index].innerText;
-    list.appendChild(li);
-  }
-}
-
-function getTasksObject() {
-  tasks = document.getElementsByTagName('li');
-  console.log(tasks);
-  tasksObj = {};
-  for (let index = 1; index <= tasks.length; index += 1) {
-    tasksObj[index] = tasks[index-1];
-  }
-  return tasksObj;
-}
-
-function getTaskSelected() {
-  let tasks = document.getElementsByTagName('li');
-  let taskSelected = null;
+/* 13 movimentação dos elementos para cima e para baixo: */
+function createTaskList(tasks) {
+  let cont = -1;
+  const taskList = document.getElementById('lista-tarefas');
+  taskList.innerHTML = '';
   for (let index = 0; index < tasks.length; index += 1) {
-    if (tasks[index].style.backgroundColor = 'rgb(128, 128, 128)'){
-      taskSelected = tasks[index];
-    }
+    let li = document.createElement('li');
+    li.innerText = tasks[index][0];
+    li.className = tasks[index][1];
+    li.style.backgroundColor = tasks[index][2]
+    li.setAttribute('id', cont += 1);
+    taskList.appendChild(li);
   }
-  return taskSelected;
+  addEventClickAndDobleClickInTask(taskList);
 }
-function moveTaskUp(element) {
-  let tasksObj = {};
-  tasksObj = getTasksObject();
-  console.log(tasksObj);
-  let taskSelected = getTaskSelected();
-  console.log(taskSelected);
-  for (let index = 1; index <= tasksObj.length; index += 1) {
-    if(tasksObj[index] === taskSelected) {
-      tasksObj[index-1] = taskSelected;
-      tasksObj[index] = taskSelected.previousSibling();
+
+const buttonMoveTaskUp = document.getElementById('mover-cima');
+const buttonMoveTaskDn = document.getElementById('mover-baixo');
+
+function getSelectedItem() {
+  const tasks = document.getElementsByTagName('li');
+  let task = null;
+  for (let index = 0; index < tasks.length; index += 1) {
+    if (tasks[index].style.backgroundColor === 'rgb(128, 128, 128)') {
+      task = tasks[index];
     }
   }
-  mountTaskList(tasksObj);
+  return task;
+}
+
+function moveTaskUp(el) {
+  const tasks = document.getElementsByTagName('li');
+  console.log(getSelectedItem(), tasks[0], tasks[tasks.length - 1]);
+  if (getSelectedItem() !== tasks[0] && getSelectedItem()) {
+    let arrayTasks = [];
+    for (let index = 0; index < tasks.length; index += 1) {
+      arrayTasks.push([tasks[index].innerText,
+      tasks[index].className,
+      tasks[index].style.backgroundColor,
+      tasks[index].getAttribute('id')]);
+    }
+    for (let index = 0; index < arrayTasks.length; index += 1) {
+      let backColor = arrayTasks[index][2];
+      if (backColor === 'rgb(128, 128, 128)') {
+        let notSelected = arrayTasks[index - 1];
+        let selected = arrayTasks[index];
+        arrayTasks[index - 1] = selected;
+        arrayTasks[index] = notSelected;
+      }
+    }
+    createTaskList(arrayTasks);
+  }
+}
+
+function moveTaskDn() {
+  const tasks = document.getElementsByTagName('li');
+  if (getSelectedItem() !== tasks[tasks.length - 1] && getSelectedItem()) {
+  let arrayTasks = [];
+  let positionChange = null;
+  let notSelected = null;
+  let selected = null;
+
+  for (let index = 0; index < tasks.length; index += 1) {
+    arrayTasks.push([tasks[index].innerText,
+    tasks[index].className,
+    tasks[index].style.backgroundColor,
+    tasks[index].getAttribute('id')
+  ]);
+  }
+  
+  for (let index = 0; index < arrayTasks.length; index += 1) {
+    let backColor = arrayTasks[index][2];
+    if (backColor === 'rgb(128, 128, 128)') {
+      positionChange = index;
+      notSelected = arrayTasks[index + 1];
+      selected = arrayTasks[index];
+    }
+  }
+  arrayTasks.splice(positionChange, 1, notSelected);
+  arrayTasks.splice(positionChange + 1, 1, selected);
+  createTaskList(arrayTasks);
+}
 }
 
 buttonMoveTaskUp.addEventListener('click', moveTaskUp);
+buttonMoveTaskDn.addEventListener('click', moveTaskDn);
