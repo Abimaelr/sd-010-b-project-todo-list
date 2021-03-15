059c677dfe249011369ff7264114e3166c5174c4
André <hammel.engineer@gmail.com>
2021-03-15 18:26:22 -0300

    ->item 10

--- script.js
+++ script.js
@@ -1,40 +1,76 @@
+// let listItems = ['item um', 'item dois', 'item tres', 'item quatro', 'item cinco'];
+let listItems = [];
+
+// referencia do botao Adicionar
+const btn = document.querySelector('#criar-tarefa');
+// referencia do input
+const input = document.querySelector('#texto-tarefa');
+// lista ordenada 
+const orderList = document.querySelector('#lista-tarefas');
+// referencia do botão Limpar Lista
+const btnCleanList = document.querySelector('#apaga-tudo')
+
+// // Função que com o click duplo faz o item ficar tachado;
 function doubleClickCheck(lista) {
   lista.addEventListener('dblclick', () => {
-    console.log('clicado duas vezes');
+    // console.log('clicado duas vezes');
     if (lista.style.backgroundColor === 'rgb(128, 128, 128)' && lista.className !== 'completed') {
       lista.setAttribute('class', 'completed');
     } else if (lista.style.backgroundColor === 'rgb(128, 128, 128)' && lista.className === 'completed') {
-      lista.setAttribute('class', 'new-item-list' )
+      lista.setAttribute('class', 'new-item-list')
     }
   })
 }
 
+// // Função quer muda a cor de fundo quando clicado o item;
 function changeBackground(lista) {
-  lista.addEventListener('click', () => {
-    itemsList = document.querySelectorAll('.new-item-list');
-    console.log(itemsList.length);
 
+  // Captura evento de click sobre a lista;
+  lista.addEventListener('click', () => {
+
+    // Array com os item da lista, pego pela classe .new-item-list;
+    const itemsList = document.querySelectorAll('.new-item-list');
+
+    // Loop-for sobre o array itemList;
     for (let i = 0; i < itemsList.length; i += 1) {
+
+      // atribui background white para TODOS os itens do array itemList;
       itemsList[i].style.backgroundColor = "white";
     }
+
+    // se background diferente de cinza atribuir background cinza;
     if (lista.style.backgroundColor !== 'rgb(128, 128, 128)') {
       lista.style.backgroundColor = 'rgb(128, 128, 128)';
-    } 
+    }
   })
 }
 
-function addTask() {
-  // referencia o input
-  const input = document.querySelector('#texto-tarefa');
+// criar ação no botão "Limpar Lista"
+btnCleanList.addEventListener('click', () => {
 
-  // referencia o botao
-  const btn = document.querySelector('#criar-tarefa');
+  // Limpa lista na tela;
+  orderList.innerHTML = '';
 
-  // criar ação no botão
-  btn.addEventListener('click', () => {
-    // o pai da lista ordenada
-    const orderList = document.querySelector('#lista-tarefas');
+  // Limpa o array listItems;
+  listItems = []
+})
 
+// criar ação no botão "Adicionar"
+btn.addEventListener('click', () => {
+
+  let newItem = input.value;
+
+  listItems.push(newItem);
+
+  showUpList();
+})
+
+// Função quer mostra a lista na tela;
+function showUpList() {
+ 
+  orderList.innerHTML = '';
+
+  for (item of listItems) {
     // criar elemento de lista
     const lista = document.createElement('li');
 
@@ -42,7 +78,7 @@ function addTask() {
     lista.setAttribute('class', 'new-item-list');
 
     // atribui o texto digitado no input ao texto do elemento de lista
-    lista.innerText = input.value;
+    lista.innerText = item;
 
     // adiciona o elemento de lista dentro da lista ordenada
     orderList.appendChild(lista);
@@ -52,27 +88,7 @@ function addTask() {
 
     changeBackground(lista);
     doubleClickCheck(lista);
-  })
+  }
 }
 
-addTask();
-// function changeColorItemList(item){
-//   // console.log(item)
-//   item.style.backgroundColor = 'rgb(128, 128, 128)';
-// }
-
-// lista.onclick = function () {
-//   changeColorItemList(this);
-// }
-
-// lista.addEventListener('click', () => {
-//   test = document.querySelectorAll('.new-item-list');
-//   console.log(test.length);
-
-//   for (let i = 0; i < test.length; i += 1) {
-//     test[i].style.backgroundColor = "white";
-//   }
-//   if (lista.style.backgroundColor !== 'rgb(128, 128, 128)') {
-//     lista.style.backgroundColor = 'rgb(128, 128, 128)';
-//   } 
-// })
+showUpList();
