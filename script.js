0d651025add12342d8cb4f089aa8f5d23bbede7a
Carlos Henrique <henrique.joaovictor@hotmail.com>
2021-03-15 13:29:38 -0300

    Requisito 2

--- script.js
+++ script.js
@@ -2,3 +2,8 @@ let header = document.createElement('header');
 header.innerHTML = 'Minha Lista de Tarefas';
 header.setAttribute('id', 'header');
 document.body.appendChild(header);
+
+let par = document.createElement('p');
+par.innerHTML = 'Clique duas vezes em um item para marcá-lo como completo';;
+par.id = 'funcionamento';
+header.appendChild(par);
