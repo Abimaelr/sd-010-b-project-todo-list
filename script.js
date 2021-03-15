40e5381e65a584290cd5053631a4a2786bb0bbc2
Carlos Henrique <henrique.joaovictor@hotmail.com>
2021-03-15 13:47:26 -0300

    Requisito 3

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
+let cab = document.getElementById('cabecalho');
 let header = document.createElement('header');
 header.innerHTML = 'Minha Lista de Tarefas';
 header.setAttribute('id', 'header');
-document.body.appendChild(header);
+cab.appendChild(header);
 
 let par = document.createElement('p');
 par.innerHTML = 'Clique duas vezes em um item para marcá-lo como completo';;
 par.id = 'funcionamento';
 header.appendChild(par);
+
+let ent = document.getElementById('entrada');
+let input = document.createElement('input');
+input.id = 'texto-tarefa';
+ent.appendChild(input);
