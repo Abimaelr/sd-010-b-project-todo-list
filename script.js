94d8ac3fdac10b3c7d9ac638ce01aa8714554e95
aline <aline.melryaa@gmail.com>
2021-03-16 08:33:12 -0400

    excluir item

--- script.js
+++ script.js
@@ -1,56 +1,53 @@
+function createLiText() {
+  let novaLi = document.createElement('li');
+  let inputTextLi = document.getElementById('texto-tarefa');
+  novaLi.innerText = inputTextLi.value;
+  let listaTarefa = document.getElementById('lista-tarefas');
+  listaTarefa.appendChild(novaLi);
+  inputTextLi.value = '';
+}
 
-function createLiText () {
-    let novaLi = document.createElement('li');
-    let inputTextLi = document.getElementById('texto-tarefa');
-    novaLi.innerText = inputTextLi.value;
-    let listaTarefa = document.getElementById('lista-tarefas');
-    listaTarefa.appendChild(novaLi); 
-    inputTextLi.value = '';  
+let butaDicionarLista = document.getElementById('criar-tarefa');
+butaDicionarLista.addEventListener('click', createLiText);
+
+function selecionarItem(event) {
+  let itemSelecionado = document.querySelector('.selected');
+  if (itemSelecionado) {
+    itemSelecionado.classList.remove('selected');
   }
+  event.target.classList.add('selected');
+}
 
-  let butaDicionarLista=  document.getElementById('criar-tarefa');
-  butaDicionarLista.addEventListener('click', createLiText);
-   
+let listaTarefa = document.getElementById('lista-tarefas');
+listaTarefa.addEventListener('click', selecionarItem);
 
-  function selecionarItem(event){
-    let itemSelecionado = document.querySelector('.selected');
-    if(itemSelecionado){
-      itemSelecionado.classList.remove('selected');
-    }
-    event.target.classList.add('selected');
-  }
-
-  let listaTarefa = document.getElementById('lista-tarefas')
-  listaTarefa.addEventListener("click", selecionarItem)
-  listaTarefa.addEventListener("dblclick", itemDuploClick);
-
- function itemDuploClick(event){
+function itemDuploClick(event) {
   let duploClick = document.querySelector('.completed');
-  if(duploClick){
+  if (duploClick) {
     duploClick.classList.remove('completed');
     event.target.classList.add('completed');
   }
   event.target.classList.add('completed');
   duploClick.classList.remove('completed');
-  
 }
+let listaTa = document.getElementById('lista-tarefas');
+listaTa.addEventListener('dblclick', itemDuploClick);
 
-function LimpaLista(){
+function LimpaLista() {
   let limpaTudo = document.getElementById('lista-tarefas');
- while(limpaTudo.firstChild){
-   limpaTudo.removeChild(limpaTudo.lastChild);
- }
-
+  while (limpaTudo.firstChild) {
+    limpaTudo.removeChild(limpaTudo.lastChild);
   }
+}
 
 let buttonLimpaTudo = document.getElementById('apaga-tudo');
-  buttonLimpaTudo.addEventListener('click', LimpaLista);
+buttonLimpaTudo.addEventListener('click', LimpaLista);
 
-  function excluirItemLista(){
-    let excluiItem = document.getElementById('lista-tarefas');
-    while(excluiItem.firstChild[0]){
-      excluiItem.removeChild(excluiItem.lastChild[0]);
-  }    
+function excluirItemLista(event) {
+  let elemento = document.querySelectorAll('li');
+  event.target.classList.value = elemento[0].remove('lista-tarefas');
 }
-let buttonExcluir= document.getElementById('remover-finalizados');
+
+
+let buttonExcluir = document.getElementById('remover-finalizados');
 buttonExcluir.addEventListener('click', excluirItemLista);
