5853b7a121607636f3c9a58a13c3cffba91bfbbc
Johnny Rubi <johnnyrubischreiner@gmail.com>
2021-03-16 15:26:24 -0300

    continuação

--- script.js
+++ script.js
@@ -2,4 +2,6 @@ const creatTask = document.querySelector('#criar-tarefa');
 
 creatTask.addEventListener('click', addTask);
 
-function
+function addTask(event, task, className) {
+    const addTaskList = document.querySelector("#lista-tarefas");
+}
