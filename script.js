1643a8d57db9e73ee87875de546d10658a493cef
Janaina P de Oliveira Minutti <janainapp@gmail.com>
2021-03-16 11:57:29 -0300

    requisito 6

--- script.js
+++ script.js
@@ -1,10 +1,25 @@
 let task = document.getElementById('texto-tarefa');
 let addTask = document.getElementById('criar-tarefa');
 let list = document.getElementById('lista-tarefas');
+let clickItem = document.getElementsByClassName('listItem');
 
 addTask.addEventListener('click', function(){  
   let newTask = document.createElement('li');
   newTask.classList.add('listItem');
   list.appendChild(newTask).innerHTML = task.value;
   task.value = '';  
+  changeColor();  
 });
+
+function changeColor() {
+  for (let index= 0; index < clickItem.length; index += 1) {
+    clickItem[index].addEventListener('click', function() {
+      for (let idx = 0; idx < clickItem.length + 1; idx += 1) {
+        if(clickItem[idx]) {
+          clickItem[idx].classList.remove('color');
+        }
+        event.target.classList.add('color');   
+      }
+    });
+  }
+}
