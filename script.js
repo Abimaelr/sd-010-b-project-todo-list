8b3e32a706a3cd3033f98e4050b7ac18a1528523
Breno Rodrigues <brenorm@gmail.com>
2021-03-15 23:32:56 -0300

    Requisito 7

--- script.js
+++ script.js
@@ -5,11 +5,31 @@ const orderedList = document.querySelector('#lista-tarefas');
 
 // ----------------------------------------------------------------------------
 
+// Função "Item focado": Altera background color do item de lista selcionado
+function focusedItem(li) {
+  const currentlyFocusedIte = document.querySelector('.selected-list-item');
+  if (currentlyFocusedIte != null) {
+    currentlyFocusedIte.className = '';
+  }
+  const changeBackgroundColor = li.target;
+  changeBackgroundColor.className = 'selected-list-item';
+}
+
+// Função "Item da Lista Ouvindo" Escuta click em cada item da lista de tarefas e chama função "Item focado"
+function listItemListening() {
+  const listItems = document.querySelectorAll('li');
+  for (let index = 0; index < listItems.length; index += 1) {
+    listItems[index].addEventListener('click', focusedItem);
+  }
+}
+
+// Função "Insere Item lista"
 function insertListItem() {
   const newListItem = document.createElement('li');
   newListItem.innerText = inputBox.value;
-  inputBox.value = '';
   orderedList.appendChild(newListItem);
+  inputBox.value = '';
+  listItemListening();
 }
 
 // ----------------------------------------------------------------------------
