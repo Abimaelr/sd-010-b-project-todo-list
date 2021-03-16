a36175c6bb2e05378997acaf8403617cb998cd1c
Johnny Rubi <johnnyrubischreiner@gmail.com>
2021-03-16 20:31:21 -0300

    exercicio 5/6

--- script.js
+++ script.js
@@ -1,20 +1,40 @@
-const creatTask = document.querySelector('#criar-tarefa');
+const createTask = document.querySelector('#criar-tarefa');
+const clearTasks = document.querySelector('#apaga-tudo');
+const removeCompletedButton = document.querySelector('#remover-finalizados');
+ 
+createTask.addEventListener('click', addTask);
+clearTasks.addEventListener('click', clearTasks);
+removeCompletedButton.addEventListener('click', removeCompleted);
 
-creatTask.addEventListener('click', addTask);
+function changeCompletedTask(event) {
+  if (event.target.className === 'completed selected') {
+    event.target.classList.remove('completed');
+  } else {
+    event.target.classList.add('completed');
+  }
+}
 
-function addTask(event, task, className) {
-    const addTaskList = document.querySelector("#lista-tarefas");
-    const listTask = document.createElement("li")
-if (!task) {
-    const task = document.querySelector("#texto-tarefa")
-    task = task.nodeValue;
-    task.value = "";
+function changeSelectedTask(event) {
+  const selectedTask = document.querySelector('.selected');
+  if (selectedTask) {
+    selectedTask.classList.remove('selected');
+  }
+  event.target.classList.add('selected');
 }
-if (className) {
-    listTask.className = className;
-}
-listTask.innerText = task
-listTask.addEventListener("click", changeSelectedTask);
-listTask.addEventListener("dblclick", changeCompletedTask);
-addTaskList.appendChild(ListTask);
+
+function addTask(event, taskName, className) {
+  const taskList = document.querySelector('#lista-tarefas');
+  const listItem = document.createElement('li');
+  if (!taskName) {
+    const task = document.querySelector('#texto-tarefa');
+    taskName = task.value;
+    task.value = '';
+  }
+  if (className) {
+    listItem.className = className;
+  }
+  listItem.innerText = taskName;
+  listItem.addEventListener('click', changeSelectedTask);
+  listItem.addEventListener('dblclick', changeCompletedTask);
+  taskList.appendChild(listItem);
 }
