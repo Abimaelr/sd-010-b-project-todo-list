c62b68b5395b22f663bb363bc478a47c6e764a53
Jonathan Souza <souzajonathan570@gmail.com>
2021-03-15 22:06:15 -0300

    adicionado um botao limpar para apagar a lista de tarefas (item 10)

--- script.js
+++ script.js
@@ -2,16 +2,10 @@
 const valueInput = document.getElementById('texto-tarefa');
 const btnCriarTarefa = document.getElementById('criar-tarefa');
 const olListaTarefas = document.getElementById('lista-tarefas');
+const btnLimpar = document.getElementById('apaga-tudo');
 
-btnCriarTarefa.addEventListener('click', () => {
-  const liTarefa = document.createElement('li');
-  liTarefa.className = 'tarefa';
-  olListaTarefas.appendChild(liTarefa).innerHTML = valueInput.value;
-
-  valueInput.value = '';
-});
-
-function bgCinza(e) {
+// área das funções
+function alteraFundoParaCinza(e) {
   const evento = e;
   const liSelecionado = document.querySelector('.selected');
   if (liSelecionado !== null) {
@@ -19,7 +13,6 @@ function bgCinza(e) {
   }
   evento.target.classList.add('selected');
 }
-olListaTarefas.addEventListener('click', bgCinza);
 
 function addCompleted(parameter) {
   const addStyleCompleted = parameter;
@@ -31,6 +24,23 @@ function remCompleted(parameter) {
   remStyleCompleted.classList.remove('completed');
 }
 
+function apagaLista() {
+  while (olListaTarefas.hasChildNodes()) {
+    olListaTarefas.removeChild(olListaTarefas.firstChild);
+  }
+}
+
+// eventos de chamada das funções
+btnCriarTarefa.addEventListener('click', () => {
+  const liTarefa = document.createElement('li');
+  liTarefa.className = 'tarefa';
+  olListaTarefas.appendChild(liTarefa).innerHTML = valueInput.value;
+
+  valueInput.value = '';
+});
+
+olListaTarefas.addEventListener('click', alteraFundoParaCinza);
+
 olListaTarefas.addEventListener('dblclick', (e) => {
   if (e.target.classList.contains('completed')) {
     remCompleted(e.target);
@@ -38,3 +48,5 @@ olListaTarefas.addEventListener('dblclick', (e) => {
     addCompleted(e.target);
   }
 });
+
+btnLimpar.addEventListener('click', apagaLista);
