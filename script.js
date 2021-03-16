0d0be3fc62735b2c9cb084815ff4e8eb012adb71
Nikolas <nikolas.silva1990@gmail.com>
2021-03-16 11:10:15 -0300

    Requisito 9 completo

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
 let addTask = document.getElementById('criar-tarefa');
 
 addTask.addEventListener('click', function () {
-  let addItenList = document.createElement('li')
+  let addItenList = document.createElement('li');
   let inputText = document.getElementById('texto-tarefa').value
   addItenList.innerText = inputText
   document.getElementById('lista-tarefas').appendChild(addItenList)
@@ -11,10 +11,29 @@ addTask.addEventListener('click', function () {
 })
 
 // <!-- Requisito 7 -->
-
+// <!-- Requisito 8 -->
 let liColor = document.getElementById('lista-tarefas');
 
 liColor.addEventListener('click', function (event) {
-  event.target.classList.add('itemSelected')
+  let itemColorized = document.querySelector('.itemSelected');
+
+  if (itemColorized != null) {
+    itemColorized.classList.remove('itemSelected');
+  }
+
+  event.target.classList.add('itemSelected');
 })
+
+// <!-- Requisito 9 -->
+liColor.addEventListener('dblclick', function(event) {
+event.target.classList.toggle('completed');
+})
+
+
+
+
+
+
+
+
 
