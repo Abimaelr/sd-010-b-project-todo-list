6ed9a4ae872f05fe5a550296f7877198bf0b4bbb
renanpbraga <renanpbraga@gmail.com>
2021-03-16 16:26:49 -0300

    adiciona estilo e a adiçao de tarefas

---
+++ script.js
@@ -0,0 +1,49 @@
+ /*código executado por interação entre os estudantes 
+Rafael Mathias, Lotar Lucas, Renan Braga, Felippe Correa,
+com colaboração do Gustavo Cerqueira e Matheus Gois.
+*/
+
+const task = document.getElementById('texto-tarefa');
+const btnCriaTarefa = document.getElementById('criar-tarefa');
+const list = document.getElementById('lista-tarefas');
+const btnLimpar = document.querySelector('#apaga-tudo');
+const btnRemoverFinalizadas = document.querySelector('#remover-finalizados');
+const btnSalvaLista = document.querySelector('#salvar-tarefas');
+const btnMoverCima = document.querySelector('#mover-cima');
+const btnMoverBaixo = document.querySelector('#mover-baixo');
+
+function selectLi(event) {
+  const listLi = document.querySelectorAll('.tarefa');
+  for (let i = 0; i < listLi.length; i += 1) {
+    if (listLi[i].classList.contains('selected')) {
+      listLi[i].classList.remove('selected');
+    }
+    event.target.classList.add('selected');
+  }
+}
+
+/* pesquisa toggle */
+function mark(event) {
+  event.target.classList.toggle('completed');
+}
+
+function criarTarefa() {
+  const novoItem = document.createElement('li');
+  novoItem.className = 'tarefa';
+  novoItem.addEventListener('click', selectLi);
+  novoItem.addEventListener('dblclick', mark);
+  novoItem.innerText = task.value;
+  if (!task.value.trim()) {
+    return alert('Adicione uma tarefa');
+  }
+  list.appendChild(novoItem);
+  task.value = '';
+  task.focus();
+}
+
+btnCriaTarefa.addEventListener('click', criarTarefa);
+window.addEventListener('keyup', (event) => {
+  if (event.key === 'Enter') {
+    criarTarefa();
+  }
+});
