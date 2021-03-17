d4ec6cf67db911d1e2b0c90298794b07d9827d14
Maximiliano Alvarenga <maximilianoabispo@gmail.com>
2021-03-17 18:58:09 -0400

    Requisito 05, adicionado botao e interação com a lista ordenada e input.

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
-console.log("Hello World!!");
+let btn = document.getElementById('criar-tarefa');
+let arrayListItens = [];
+const ordenedList = document.getElementById('lista-tarefas');
+
+btn.addEventListener('click', function () {
+  clearList();
+  arrayListItens.push(document.querySelector('#texto-tarefa').value);
+  createElementList();
+  document.querySelector('#texto-tarefa').value = "";
+});
+
+function clearList() {
+  document.getElementById("lista-tarefas").innerHTML = "";
+}
+
+
+
+function createElementList() {
+  for (let indexCreate = 0; indexCreate < arrayListItens.length; indexCreate += 1) {
+    let textItem = arrayListItens[indexCreate];
+
+    let newLI = document.createElement('li');
+    newLI.innerText = textItem;
+
+    ordenedList.appendChild(newLI);
+
+  }
+}
