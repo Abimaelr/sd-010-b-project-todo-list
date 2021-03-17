0e540aee830366fa63ec501d7ed5a36ce3b6e7ef
Holistic <paulofreire.si@outlook.com>
2021-03-17 20:51:13 -0300

    fixed req 1, trying req 2

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
-
 function spawn(element) {
   const elementMain = document.querySelector('main');
   elementMain.appendChild(element);
   return elementMain;
 }
 
-const title = document.createElement('h1');
+const title = document.createElement('header');
 title.id = 'title';
 title.innerText = 'Minha Lista de Tarefas';
 spawn(title);
+
+const p = document.createElement('p');
+p.id = 'funcionamento';
+p.innerText = 'Clique duas vezes em um site para marcá-lo como completo';
+spawn(p);
