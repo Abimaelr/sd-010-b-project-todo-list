f8b0cc1cc8cde5eead6ba00f65a091e101f1812a
Aderson Santos <assaderson@gmail.com>
2021-03-17 17:47:40 -0300

    Começando o javascript

---
+++ script.js
@@ -0,0 +1,12 @@
+//Exemplo do site https://stackoverflow.com/questions/26946235/pure-javascript-listen-to-input-value-change
+
+/* event listener */
+document.getElementsByName("texto")[0].addEventListener('change', adicionar);
+/* function */
+function adicionar(){
+  let li = document.createElement("li");
+  let ol = document.getElementById("lista-tarefas");
+  let input = document.getElementById("texto-tarefa").value;
+  li.appendChild(document.createTextNode(input));
+  ol.appendChild(li);
+}
