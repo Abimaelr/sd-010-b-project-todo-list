74883fd7647e61e79cf5ef13dc753db91a67aab8
Michelle <mls.srbek@gmail.com>
2021-03-17 17:21:10 -0300

    dblClick function, add a line in the taskwhen double clicked

--- script.js
+++ script.js
@@ -9,19 +9,31 @@ function newItem() {
 }
 
 function changeColor(event) {
-let eventSelect = event.target;
-let arr = document.getElementsByClassName('item-task');
-if (eventSelect) {
-  for (let i = 0; i < arr.length; i++) {
-    arr[i].classList.remove('selected');
+  let eventSelect = event.target;
+  let arr = document.getElementsByClassName('item-task');
+  if (eventSelect) {
+    for (let i = 0; i < arr.length; i++) {
+      arr[i].classList.remove('selected');
+    }
+    eventSelect.classList.add('selected');
   }
-  eventSelect.classList.add('selected');
 }
+
+function dblClick(event) {
+  let eventClick = event.target;
+  let arr = document.getElementsByClassName('item-task');
+  if (eventClick) {
+    if (eventClick.classList.contains('completed')) {
+      eventClick.classList.remove('completed');
+    } else {
+      eventClick.classList.add('completed');
+    }
+  }
 }
 
 
 window.onload = function() {
 document.getElementById('criar-tarefa').addEventListener('click', newItem);
 document.getElementById('lista-tarefas').addEventListener('click', changeColor);
-
+document.getElementById('lista-tarefas').addEventListener('dblclick', dblClick);
 }
