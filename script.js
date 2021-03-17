6f228fef774a5e9713583620030196c0fde1a40f
Michelle <mls.srbek@gmail.com>
2021-03-17 13:54:58 -0300

    changeColor function, changes task background color

--- script.js
+++ script.js
@@ -1,13 +1,27 @@
 function newItem() {
-    let item = document.createElement('li');
-    let taskList = document.getElementById('lista-tarefas');
-    taskList.appendChild(item);
-    let text = document.getElementById('texto-tarefa');
-    item.innerHTML = text.value;
-    text.value = '';
+  let item = document.createElement('li');
+  let taskList = document.getElementById('lista-tarefas');
+  item.className = 'item-task';
+  taskList.appendChild(item);
+  let text = document.getElementById('texto-tarefa');
+  item.innerHTML = text.value;
+  text.value = '';
+}
+
+function changeColor(event) {
+let eventSelect = event.target;
+let arr = document.getElementsByClassName('item-task');
+if (eventSelect) {
+  for (let i = 0; i < arr.length; i++) {
+    arr[i].classList.remove('selected');
+  }
+  eventSelect.classList.add('selected');
+}
 }
 
 
 window.onload = function() {
-    document.getElementById('criar-tarefa').addEventListener('click', newItem);
+document.getElementById('criar-tarefa').addEventListener('click', newItem);
+document.getElementById('lista-tarefas').addEventListener('click', changeColor);
+
 }
