ac6ae543d96cd95cbda42f05b61c898c77f05a78
Cesar Lopez <cglopez874@hotmail.com>
2021-03-18 05:15:25 -0300

    exercicio 5

---
+++ script.js
@@ -0,0 +1,12 @@
+let button = document.getElementById('criar-tarefa');
+let lista = document.getElementById('lista-tarefas');
+let input = document.getElementById('texto-tarefa');
+
+button.addEventListener('click', function(){
+var ordenalista = document.createElement('ol')
+ordenalista.innerText = input.value;
+lista.appendChild(ordenalista);
+document.getElementById("texto-tarefa").value = "";
+})
+
+
