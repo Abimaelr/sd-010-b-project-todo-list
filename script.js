eccdaa06060e19330f21430ae03c28262da6efbc
Aderson Santos <assaderson@gmail.com>
2021-03-17 21:04:17 -0300

    Tentando fazer o JavaScript

--- script.js
+++ script.js
@@ -1,12 +1,28 @@
 //Exemplo do site https://stackoverflow.com/questions/26946235/pure-javascript-listen-to-input-value-change
 
-document.getElementById("criar-tarefa").addEventListener('click' , adicionar);
-function adicionar(){
+function adicionar() {
+  let botao = document.getElementById("criar-tarefa")
+  botao.addEventListener('click' , adicionar);
+  ol = document.getElementById("lista-tarefas");
   let li = document.createElement("li");
-  let ol = document.getElementById("lista-tarefas");
   let input = document.getElementById("texto-tarefa").value;
+  li.classList = "texto";
   li.appendChild(document.createTextNode(input));
   ol.appendChild(li);
   document.getElementById("texto-tarefa").value = "";
 }
+adicionar();
 
+//document.getElementById("#lista-tarefas");
+/*document.getElementById("lista-tarefas").addEventListener('click' , escolherLista);
+function escolherLista(){  
+    let selecionado = document.querySelector(".selected");
+    if(selecionado === ""){
+      classList.add("selected");
+    }else{
+      selecionado.classList.remove("selected");
+      classList.add("selected");
+    }
+}*/
+
+//alert(document.getElementById("lista-tarefas").innerText)
