75119163849afd7a15720d4d8a407fae7cec9a11
Aderson Santos <assaderson@gmail.com>
2021-03-18 08:20:53 -0300

    Depois das 22:00 deu erro no corretor e colocou minha nota como 0 estou tentando arrumar

--- script.js
+++ script.js
@@ -1,28 +1,38 @@
 //Exemplo do site https://stackoverflow.com/questions/26946235/pure-javascript-listen-to-input-value-change
 
+let itens = [];
+
+document.getElementById("criar-tarefa").addEventListener('click' , adicionar);
 function adicionar() {
-  let botao = document.getElementById("criar-tarefa")
-  botao.addEventListener('click' , adicionar);
-  ol = document.getElementById("lista-tarefas");
+  let ol = document.getElementById("lista-tarefas");
   let li = document.createElement("li");
   let input = document.getElementById("texto-tarefa").value;
-  li.classList = "texto";
   li.appendChild(document.createTextNode(input));
   ol.appendChild(li);
   document.getElementById("texto-tarefa").value = "";
-}
-adicionar();
-
-//document.getElementById("#lista-tarefas");
-/*document.getElementById("lista-tarefas").addEventListener('click' , escolherLista);
-function escolherLista(){  
-    let selecionado = document.querySelector(".selected");
-    if(selecionado === ""){
-      classList.add("selected");
-    }else{
-      selecionado.classList.remove("selected");
-      classList.add("selected");
+  itens = document.querySelectorAll("#lista-tarefas");
+  selecao = document.getElementById("lista-tarefas").addEventListener('click' , selecionar);
+  function selecionar(){
+    for(let i = 0; i < itens.length; i+=1){
+      if(itens.firstChild = itens[i]){
+        alert("Deu certo");
+      }    
     }
-}*/
+  }
+}
 
-//alert(document.getElementById("lista-tarefas").innerText)
+//let array = [];
+let inputText = document.getElementById("lista-tarefas");
+//itens = document.querySelectorAll("#lista-tarefas li");
+let array = [];
+
+for(let i = 0; i < itens.length; i+=1) {
+  tab.push(itens[i].innerHTML);
+}
+
+for(let i = 0; i < itens.length; i+=1) {
+  itens[i].onclick = function(){
+    index = tab.indexOf(this.innerHTML);
+    console.log(this.innerHTML + " INDEX = " + index);
+  };
+}
