8bf4d3c6f9972ec4aa21e66cd5a9f73e934a293c
lucas <peixotolucas516@gmail.com>
2021-03-18 10:03:56 -0300

    função criar tarefas concluída

---
+++ script.js
@@ -0,0 +1,32 @@
+document.getElementById('#lista-tarefa')
+
+
+function addNewTask() {
+  let getInputField = document.querySelector('#texto-tarefa');
+  let addInputButton = document.querySelector('#criar-tarefa');
+  let getTaskList = document.querySelector('#lista-tarefas');
+
+  addInputButton.addEventListener('click', function() {
+    if (getInputField.value.length > 0) {
+      let newLi = document.createElement('li');
+      newLi.innerText = getInputField.value;
+
+      getTaskList.appendChild(newLi);
+      getInputField.value = '';
+    } else {
+      alert('Error: Digite ao menos 1 caractere.');
+    }
+  })
+
+  getInputField.addEventListener('keyup', function(event) {
+    if (event.keyCode === 13 && getInputField.value.length > 0) {
+      let newLi = document.createElement('li');
+      newLi.innerText = getInputField.value;
+
+      getTaskList.appendChild(newLi);
+      getInputField.value = '';
+    }
+  });
+};
+
+addNewTask();
