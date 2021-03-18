2404d1fe0333091e0069811005f39874919cd9c6
Thiago da Paz <thiagopazjunior@gmail.com>
2021-03-18 13:09:59 -0300

    Adicionando o requisito 7.

--- script.js
+++ script.js
@@ -6,6 +6,14 @@ function adicionadoLista () {
     let itemLista = document.createElement('li');
     lista.appendChild(itemLista).innerHTML = conteudoItens.value;
     conteudoItens.value = '';
-  })
+  });
 }
 adicionadoLista();
+
+function listaColorida () {
+  let corLista = document.querySelector('#lista-tarefas');
+  corLista.addEventListener("click", function (evento){
+      evento.target.style.background = 'rgb(128, 128, 128)';
+  })
+}
+listaColorida(); 
