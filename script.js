1bb5f604d4479fa7c76db28f5ee308bea77f4181
Maximiliano Alvarenga <maximilianoabispo@gmail.com>
2021-03-18 00:55:51 -0400

    Requisito 07, adicionado mudança de cor ao clicar no item da lista gerada.

--- script.js
+++ script.js
@@ -1,28 +1,41 @@
-let btn = document.getElementById('criar-tarefa');
-let arrayListItens = [];
-const ordenedList = document.getElementById('lista-tarefas');
-
-btn.addEventListener('click', function () {
-  clearList();
-  arrayListItens.push(document.querySelector('#texto-tarefa').value);
-  createElementList();
-  document.querySelector('#texto-tarefa').value = "";
-});
-
 function clearList() {
   document.getElementById("lista-tarefas").innerHTML = "";
 }
 
-
+function insertText() {
+  arrayListItens.push(document.querySelector('#texto-tarefa').value);
+}
 
 function createElementList() {
   for (let indexCreate = 0; indexCreate < arrayListItens.length; indexCreate += 1) {
-    let textItem = arrayListItens[indexCreate];
-
-    let newLI = document.createElement('li');
+    const textItem = arrayListItens[indexCreate];
+    const newLI = document.createElement('li');
+    newLI.classList.add('li-tarefas');
     newLI.innerText = textItem;
-
     ordenedList.appendChild(newLI);
-
-  }
+  };
 }
+
+function switchBG() {
+  const bgItens = document.querySelectorAll('.li-tarefas');
+
+  for (let indexBGItens = 0; indexBGItens < bgItens.length; indexBGItens += 1) {
+    bgItens[indexBGItens].addEventListener('click', function () {
+      bgItens[indexBGItens].style.background = 'rgb(128, 128, 128)';
+    });
+  };
+}
+
+const btn = document.getElementById('criar-tarefa');
+const arrayListItens = [];
+const ordenedList = document.getElementById('lista-tarefas');
+
+btn.addEventListener('click', function () {
+  clearList();
+  insertText();
+  createElementList();
+
+  document.querySelector('#texto-tarefa').value = "";
+
+  switchBG();
+});
