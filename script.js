6f91ffcb3170ee6e0bd4d4c028c8c899b0069434
lucas <peixotolucas516@gmail.com>
2021-03-18 10:25:01 -0300

    add const no lugar de variaveis

--- script.js
+++ script.js
@@ -2,13 +2,13 @@ document.getElementById('#lista-tarefa')
 
 
 function addNewTask() {
-  let getInputField = document.querySelector('#texto-tarefa');
-  let addInputButton = document.querySelector('#criar-tarefa');
-  let getTaskList = document.querySelector('#lista-tarefas');
+  const getInputField = document.querySelector('#texto-tarefa');
+  const addInputButton = document.querySelector('#criar-tarefa');
+  const getTaskList = document.querySelector('#lista-tarefas');
 
   addInputButton.addEventListener('click', function() {
     if (getInputField.value.length > 0) {
-      let newLi = document.createElement('li');
+      const newLi = document.createElement('li');
       newLi.innerText = getInputField.value;
 
       getTaskList.appendChild(newLi);
