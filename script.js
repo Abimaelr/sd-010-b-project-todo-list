fee811e836a0cd475d50a42b77b0035bbf058033
fredmartins <fredweb22@gmail.com>
2021-03-18 12:09:26 -0300

    bgc para itens li  clicados

--- script.js
+++ script.js
@@ -1,24 +1,61 @@
-// criando elementos (Li)
-let toAdd = () => {
-    let addLi = document.createElement('li');
-    addLi.innerText = inputText.value;
-    document.getElementById('lista-tarefas').appendChild(addLi);
-    inputText.value = '';
-}
-// Inserindoo tarefa em lista de tarefas
-let createTask = document.getElementById('criar-tarefa');
-let inputText = document.getElementById('texto-tarefa');
+//https://www.w3schools.com/jsref/jsref_foreach.asp
+//https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Functions/Arrow_functions
+//https://www.w3schools.com/js/js_arrow_function.asp
 
-createTask.addEventListener('click', toAdd);
-
-
-let selectTaskWithBackgroundColor = (color) => {
-    let taskElementsList = document.querySelectorAll('.task');
-    let selectTask = null;
-    taskElementsList.forEach((task) => {
-        if (task.style.backgroundColor === color) {
-          selectTask = task;
+let createTask = (text) => {
+    let task = document.createElement('li');
+    task.className = 'task'; 
+    task.innerHTML = text; 
+    return task; 
+  }  
+  
+  let insertTask = () => {    
+    let input = document.getElementById('texto-tarefa');    
+    let lista = document.getElementById('lista-tarefas');
+  
+    if (input.value !== '') { 
+      let task = createTask(input.value); 
+      lista.appendChild(task); 
+      input.value = ''; 
     }
-});
-return selectTask; 
-}
+  }  
+  let selectTaskWithBackgroundColor = (color) => {    
+    let taskElementsList = document.querySelectorAll('.task');
+    let selectTask = null;     
+    taskElementsList.forEach((task) => {      
+      if (task.style.backgroundColor === color) {
+        selectTask = task; 
+      }
+    });
+  
+    return selectTask; 
+  }
+  
+ 
+  let changeTaskBackgroundColor = (event) => {
+    let task = event.target;  
+    
+    let currentTask = selectTaskWithBackgroundColor('rgb(128, 128, 128)');
+  
+    if (currentTask) {      
+      currentTask.style.backgroundColor = task.style.backgroundColor; 
+    }    
+    task.style.backgroundColor = 'rgb(128, 128, 128)';
+  } 
+  
+  let taskCompletedOrNot = (event) => {
+    let task = event.target; 
+    
+    task.classList.toggle('completed');
+  } 
+  
+  let setupEvents = () => {   
+    let button = document.querySelector('#criar-tarefa');    
+    button.addEventListener('click', insertTask);    
+    let taskList = document.querySelector('#lista-tarefas');    
+    taskList.addEventListener('click', changeTaskBackgroundColor);   
+    taskList.addEventListener('dblclick', taskCompletedOrNot);
+  }    
+    window.onload = function init() {
+    setupEvents(); 
+  };
