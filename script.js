3775e9f125720855dc28e0b48e143ebd67b24502
Thiago da Paz <thiagopazjunior@gmail.com>
2021-03-18 12:37:03 -0300

    Adicionando requisito 5.

---
+++ script.js
@@ -0,0 +1,11 @@
+function adicionadoLista () {
+  let butao = document.querySelector('#criar-tarefa');
+  let lista = document.querySelector('#lista-tarefas');
+  let conteudoItens = document.querySelector('#texto-tarefa');
+  butao.addEventListener('click', function () {
+    let itemLista = document.createElement('li');
+    lista.appendChild(itemLista).innerHTML = conteudoItens.value;
+    conteudoItens.value = '';
+  })
+}
+adicionadoLista();
