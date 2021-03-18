36115334fae45b439187112b20f39c765a96586e
Mariana Savoldi <marianasavodi123@gmail.com>
2021-03-18 01:19:19 -0300

    Requisito 8 teste

--- script.js
+++ script.js
@@ -14,9 +14,24 @@ function addTarefa(add) {
 
 criarTarefa.addEventListener('click', addTarefa);
 
-// requisito 7 
+// requisito 7
 function alteraCor(event) {
   event.target.style.backgroundColor = 'rgb(128, 128, 128)';
 }
 
 listaTarefa.addEventListener('click', alteraCor);
+
+// requisito 8
+const classTarefas = document.getElementsByTagName('li');
+classTarefas.className = 'task'
+function selecionaItem(troca) {
+  const todasTarefas = document.querySelectorAll('.task');
+  for (let i = 0; i < todasTarefas.length; i += 1) {
+    todasTarefas[i].className = 'task';
+  }
+  troca.target.classList.add('selected');
+  classTarefas.querySelector('.selected').classList.remove('selected');
+  this.classList.add('selected');
+}
+
+listaTarefa.addEventListener('click', selecionaItem);
