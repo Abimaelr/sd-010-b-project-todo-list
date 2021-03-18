10dc1fd746c32ab74d1a9ecc799f97e0084d852a
Thiago da Paz <thiagopazjunior@gmail.com>
2021-03-18 12:57:45 -0300

    Corrigindo requisito 5.

--- script.js
+++ script.js
@@ -9,22 +9,3 @@ function adicionadoLista () {
   })
 }
 adicionadoLista();
-
-// function criarTarefa(){
-//   let addtext = document.getElementById('texto-tarefa').value; 
-//   let list = document.getElementById('lista-tarefas');
-//   let li = document.createElement('li');
-//   list.appendChild(li).innerHTML = addtext; 
-//   limpaInput();
-// }
-
-// function botaoAdd(){
-//   let button = document.getElementById('criar-tarefa'); 
-//   button.addEventListener("click", criarTarefa);
-// }
-// botaoAdd(); 
-
-// function limpaInput(){
-//     document.getElementById('texto-tarefa').value = '';
-
-// }
